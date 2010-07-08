0ef34f9c98720715eaffad82c478bbe0a53e6a5c
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-07-08 13:48:04 +0300

    Improvement for label tag helper localization support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def label(*args)
+    # Using value by localize if found it
+    #
+    # Model key name
+    if I18n.t("activeldap.attributes").has_key?(args[0].to_sym) &&
+        # Attribute key name
+        I18n.t("activeldap.attributes.#{args[0]}").key?(args[1].to_sym)
+      # Using humanize method by String class If args[2] is nil
+      args[2] = I18n.t("activeldap.attributes.#{args[0]}.#{args[1]}")
+    end
+    super(*args)
+  end
 end
