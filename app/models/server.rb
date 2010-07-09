ef2b41bc3a9c6bf6411dc6923562fc003e4ac0c3
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-07-09 12:32:22 +0300

    puavoDevice attributes to Server form

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,13 +1,26 @@
-class Server < DeviceBase
+class Server < ActiveLdap::Base
   ldap_mapping( :dn_attribute => "puavoId",
-                :prefix => "ou=Hosts",
-                :classes => ['top', 'device', 'ipHost', 'puppetClient', 'puavoServer'] )
+                :prefix => "ou=Servers,ou=Hosts",
+                :classes => ['top', 'device', 'puppetClient', 'puavoServer'] )
 
   has_many( :automounts, :class_name => 'Automount',
             :primary_key => 'dn',
             :foreign_key => 'puavoServer' )
 
+  before_validation :set_puavo_id
+
   def full_hostname
-    "#{self.cn}.#{LdapBase.first.puavoDomain}"
+    "#{self.puavoHostname}.#{LdapBase.first.puavoDomain}"
+  end
+
+  def id
+    self.puavoId.to_s unless self.puavoId.nil?
+  end
+
+  private
+
+  def set_puavo_id
+    self.puavoId = IdPool.next_puavo_id if self.puavoId.nil?
+    self.cn = self.puavoHostname
   end
 end
