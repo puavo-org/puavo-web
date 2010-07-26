8932dc32d492b7db3167410fe30e05dd46b2ddee
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-07-26 12:54:46 +0300

    Puavo layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,41 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  helper_method :logged_in?, :current_user_displayName
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  def find_school
+    if params.has_key?(:school_id)
+      school_id = params[:school_id]
+    elsif controller_name == 'schools' && params.has_key?(:id)
+      school_id = params[:id]
+    end
+
+    unless school_id.nil?
+      begin
+        @school = School.find(school_id)
+      rescue Exception => e
+        logger.info "Incorrect school id (#{school_id})! Redirected..."
+        logger.debug "Exception: " + e
+        flash[:notice] = "Incorrect school id!"
+        #redirect_to devices_path
+      end
+    end
+  end
+
+  def current_user_displayName
+    if logged_in?
+      return current_user.displayName
+    end
+    ""
+  end
+
+  def logged_in?
+    if defined?(current_user) == "method"
+      current_user != nil
+    end
+  end
 end
