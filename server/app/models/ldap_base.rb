70122116d4976ae24d3b5606a6a9e9458a3bc8d3
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-08-05 15:28:33 +0300

    REST api refactoring

--- server/app/models/ldap_base.rb
+++ server/app/models/ldap_base.rb
@@ -1,3 +1,21 @@
 class LdapBase < ActiveLdap::Base
   include Puavo::Connection if defined?(Puavo::Connection)
+
+  # Activeldap object's to_json method return Array by default.
+  # E.g. @server.to_json -> [["puavoHostname", "puavoHostname 1"],["macAddress", "00-00-00-00-00-00-00-00"]]
+  # When we use @server.attributes.to_json method we get Hash value. This is better and
+  # following method make it automatically when we call to_json method.
+  def to_json(options = {})
+    method_values = { }
+    # Create Hash by :methods name if :methods options is set.
+    if options.has_key?(:methods)
+      method_values = Array(options[:methods]).inject({ }) do |result, method|
+        result.merge( { "#{method}" => self.send(method) } )
+      end
+      options.delete(:methods)
+    end
+    # Include method's values to the retur value
+    method_values.empty? ? self.attributes.to_json(options) :
+      self.attributes.merge( method_values ).to_json(options)
+  end
 end
