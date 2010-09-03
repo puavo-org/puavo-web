c82405bf0bd50659244b733a6a30102a919c0137
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-09-03 15:18:20 +0300

    Refactoring

--- init.rb
+++ init.rb
@@ -1,2 +1,12 @@
-# Include hook code here
-require 'puavo_authentication'
+%w{ models controllers }.each do  |dir|
+  path = File.join(File.dirname(__FILE__), 'app', dir)
+  $LOAD_PATH << path
+  ActiveSupport::Dependencies.load_paths << path
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
+end
+
+require 'puavo/authentication'
+require 'puavo/connection'
+require 'puavo_authentication/controllers/helpers'
+
+ActionController::Base.send :include, PuavoAuthentication::Controllers::Helpers
