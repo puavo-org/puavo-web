f20d1ac9afb5a886c0ca514fcdeb4c9956c457e6
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-09-07 09:54:13 +0300

    Changed session key name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_kilohaili_session',
+  :key         => '_puavo_session',
   :secret      => '57fd0fc97fd03b46e42cc7de38ff572a79ca946b08eb4a2606d1b21496bb5d016eeaf4b18691c40312a3cda13c1c9e1f940c00a0e8adc0cafde79f428ebbdf54'
 }
 
