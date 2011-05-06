d140c401615446f1b79ff96ab2b7bdcd0e9cc930
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-05-06 15:50:28 +0300

    Updated classes list on the SystemGroup model

--- app/models/system_group.rb
+++ app/models/system_group.rb
@@ -1,7 +1,7 @@
 class SystemGroup < LdapBase
   ldap_mapping( :dn_attribute => "cn",
                 :prefix => "ou=System Groups",
-                :classes => ["top", "puavoSystemGroup"] )
+                :classes => ["puavoSystemGroup"] )
 
   has_many :members, :class_name => "ExternalService", :wrap => "member", :primary_key => "dn"
 end
