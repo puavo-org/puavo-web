4414793d791957315402716f00a944cd0e3a4cca
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-05-09 10:42:33 +0300

    Remove useless resource

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -6,10 +6,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :devices, :path_prefix => ':school_id'
   map.revoke_certificate_device '/devices/:id/revoke_certificate', :controller => 'devices', :action => 'revoke_certificate', :conditions => { :method => :delete }, :path_prefix => ':school_id'
 
-  map.resources :terminals
-
-  map.resources :workstations
-
   map.resources :servers, :has_many => :automounts
   map.revoke_certificate_server '/servers/:id/revoke_certificate', :controller => 'servers', :action => 'revoke_certificate', :conditions => { :method => :delete }
 
