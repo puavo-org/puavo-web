ccb4131bd73a4b921cdd1ebdd4750db2b1140ca3
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-05-19 15:32:38 +0300

    Refactor Javascript code, use now jQuery instead of Prototype

--- server/public/javascripts/application.js
+++ server/public/javascripts/application.js
@@ -1,2 +1,7 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+    $('.clone_prev_input_element').click(function() {
+	clone_element = $(this).prev().find('input:first').clone()
+	clone_element.val('');
+	$(this).prev().append( clone_element );
+    });
+});
