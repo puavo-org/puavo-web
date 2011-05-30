3abd88530db93eec8ca34936cbbf1165b55f3c17
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-05-30 13:27:36 +0300

    Filtered binary attributes in json conversion

--- server/app/models/ldap_base.rb
+++ server/app/models/ldap_base.rb
@@ -6,6 +6,11 @@ class LdapBase < ActiveLdap::Base
   # When we use @server.attributes.to_json method we get Hash value. This is better and
   # following method make it automatically when we call to_json method.
   def to_json(options = {})
+    allowed_attributes = self.attributes
+    allowed_attributes.delete_if do |attribute, value|
+      !self.schema.attribute(attribute).syntax.human_readable?
+    end
+
     method_values = { }
     # Create Hash by :methods name if :methods options is set.
     if options.has_key?(:methods)
@@ -14,8 +19,8 @@ class LdapBase < ActiveLdap::Base
       end
       options.delete(:methods)
     end
-    # Include method's values to the return value
-    method_values.empty? ? self.attributes.to_json(options) :
-      self.attributes.merge( method_values ).to_json(options)
+    # Include method's values to the return value'
+    method_values.empty? ? allowed_attributes.to_json(options) :
+      allowed_attributes.merge( method_values ).to_json(options)
   end
 end
