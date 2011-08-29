69c2e6fde0fd1c523bfad05145e01d88cd3a8d2f
Janne <janne.juhani@gmail.com>
2011-08-29 08:41:24 +0300

    Change password -notifications fix. Flash notification fix to users_controller.rb-file.

--- javascripts/application.js
+++ javascripts/application.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     window.open(this.href);
 		return false;
 	});
-	if($("p.message_success") || $("p.message_notice")){
+	if($("p.message_success").length > 0 || $("p.message_notice").length > 0){
 		var notice = $("p[class^='message_']");
 		notice.delay(3000).slideUp(500).fadeOut(200, function() {
 			notice.remove();
