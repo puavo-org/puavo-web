38bcbfc7ae443986fb49c0c0912c81a21048f010
Janne <janne.juhani@gmail.com>
2011-09-13 08:58:27 +0300

    Little changes to locales and notice text-box javascript behaviour

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,9 +18,9 @@ $(document).ready(function() {
 	});
 	if($("p.message_success").length > 0 || $("p.message_notice").length > 0){
 		var notice = $("p[class^='message_']");
-		notice.delay(3000).slideUp(500).fadeOut(200, function() {
+		notice.delay(2000).fadeOut(500, function() {
 			notice.remove();
-		});		
+		});	
 	}
 
 });
