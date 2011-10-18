3ccc61f405a7524f60e60da2665bf0877d3334dd
Janne <janne.juhani@gmail.com>
2011-10-18 12:59:37 +0300

    Breathe theme update

--- public/javascripts/new_themes.js
+++ public/javascripts/new_themes.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     n ? c.css("margin-top",50) : c.css("margin-top",25);
     n ? $("#top-menu-right").css("float","left") : $("#top-menu-right").css("float","right");
   };
-  var container = $(".container");
+  var container = $(".container_wrap");
   var topNavContainer = $(".topNavContainer");
   var narrowcontainer = (topNavContainer.height()>=38) ? true : false;
   topMenuHandler(narrowcontainer, container);
