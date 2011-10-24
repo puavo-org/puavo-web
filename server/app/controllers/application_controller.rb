68f023e82e5ff8dffd882105a2569fa0d194f8e8
Janne <janne.juhani@gmail.com>
2011-10-24 13:12:56 +0300

    Puavo devices supports now custom themes. New theme to Puavo devices: Breathe. And some minor css-fixes.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -67,4 +67,7 @@ class ApplicationController < ActionController::Base
   def puavo_users?
     PUAVO_CONFIG["puavo_users"] == "enabled" ? true : false
   end
+  def theme
+    session[:organisation].value_by_key('theme') or "gray"
+  end
 end
