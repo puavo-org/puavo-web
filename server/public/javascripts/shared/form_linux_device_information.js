64706cc4cbe5b1865cdfbd2ce3140dcce1f16f68
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-11-15 15:17:07 +0200

    Optimize auto power off UI with breathe theme

--- server/public/javascripts/shared/form_linux_device_information.js
+++ server/public/javascripts/shared/form_linux_device_information.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.autopoweroff input:radio').change( function() {
+	$('#autopoweroff input:radio').change( function() {
 		if ( $(this).attr('value') != 'custom' ) {
 			$(this).closest('table').find('select').each( function() {
 				$(this).attr('value', '');
