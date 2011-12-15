7bb6c87636c5bce2a47fac13fd85765ff2f2cd51
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2011-12-15 08:12:49 +0200

    Remove useless file

--- app/models/school_roles.rb
+++
@@ -1,6 +0,0 @@
-class SchoolRole < BaseGroup
-  ldap_mapping( :dn_attribute => "puavoId",
-                :prefix => "ou=SchoolRoles,ou=Groups",
-                :classes => ['top',  'puavoSchoolRole', 'posixGroup', 'sambaGroupMapping'] )
-
-end
