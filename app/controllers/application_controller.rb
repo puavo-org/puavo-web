ebf832f0e7a91921fd5d70685e8e2cb416a2792b
Esa-Matti Suuronen <esa-matti.suuronen@opinsys.fi>
2011-12-27 17:21:20 +0200

    Default to breathe theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,6 +39,6 @@ class ApplicationController < ActionController::Base
   end
 
   def theme
-    session[:organisation].value_by_key('theme') or "gray"
+    session[:organisation].value_by_key('theme') or "breathe"
   end
 end
