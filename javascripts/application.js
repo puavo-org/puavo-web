f69c5c287812a0eee1d00850c98de462661db39d
Esa-Matti Suuronen <esa-matti.suuronen@opinsys.fi>
2011-12-29 12:49:37 +0200

    Remove CSS menus when touch device is in use. Also clean up topmenu.css

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,5 +1,7 @@
 (function($){
 
+  $.support.touch = "ontouchstart" in window;
+
   /*
    * Submenu jQuery plugin.
    *
@@ -11,28 +13,33 @@
     var subMenu = $(menuSelector);
 
     var open = false;
-    var hide = function(e) {
+
+    // For touch devices
+    $(document).bind("touchstart", function(e) {
       // Allow touching of the link elements
       if (e && e.target.tagName === "A") return;
-
       if (open) {
         subMenu.removeClass("open");
         open = false;
       }
-    };
+    });
 
-    // For touch devices
-    $(document).bind("touchstart", hide);
     subMenuButton.bind("touchstart", function() {
       subMenu.addClass("open");
+
       // Use small timeout so that close event won't fire at same time.
-      setTimeout(function() {
-        open = true;
-      }, 500);
+      if (!open) {
+        setTimeout(function() {
+          open = true;
+        }, 500);
+      }
+
     });
 
     return this;
   };
+
+  $.fn.opensMenu.css = ".open { display: block !important; }";
 
 
   $(document).ready(function() {
@@ -59,7 +66,18 @@
     });
 
 
-    $(".tool").opensMenu(".tools .tool ul");
+    if ($.support.touch) {
+
+      // Override mouse CSS so that it won't interfier.
+      $("style#menuHandling").text($.fn.opensMenu.css);
+
+      $(".organisation-menu-button").opensMenu(".organisation-menu");
+      $(".school-menu-button").opensMenu(".school-menu");
+
+      $(".tool").opensMenu(".tools .tool ul");
+
+    }
+
 
   });
 
