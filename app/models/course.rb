4abcaa84016c4a359f3cfb02cafd0ac468ac6b39
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2012-03-21 14:42:20 +0200

    Add webhooks to Course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,8 @@ class Course < LdapBase
 
   before_validation :set_puavoId
 
+  after_create :webhook_create
+
   def set_puavoId
     self.puavoId = IdPool.next_puavo_id if self.puavoId.nil?
   end
@@ -17,6 +19,39 @@ class Course < LdapBase
     { "name" => self.puavoCourseName,
       "course_id" => self.puavoCourseId,
       "description" => self.puavoCourseDescription,
-      "puavo_id" => self.puavoId }.to_json
+      "puavo_id" => self.puavoId.to_s }.to_json
+  end
+
+  private
+
+  def webhook_create
+    webhook("create")
+  end
+  def webhook_update
+    webhook("update")
+  end
+  def webhook_destroy
+    webhook("destroy")
+  end
+
+  def webhook(action)
+    organisation = LdapOrganisation.first.cn.to_s
+    config = Puavo::Organisation.find( organisation )
+    if webhook_config = config.value_by_key("webhooks")
+      if webhook_config.has_key?("course")
+        if webhook_config["course"]["actions"].to_a.include?(action)
+          payload = ({ :course => self,
+                       :action => action,
+                       :organisation => organisation }).to_json
+          hexdigest = HMAC::SHA1.hexdigest( webhook_config["private_api_key"],
+                                            payload )
+          RestClient.post( webhook_config["course"]["url"],
+                           { :payload => payload,
+                             :hmac => hexdigest },
+                           :content_type => :json,
+                           :accept => :json) 
+        end
+      end
+    end
   end
 end
