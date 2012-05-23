07c27388d99c28ee40e0de5442b95d6f57d41fce
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2012-05-23 15:22:07 +0300

    OAuthClient: encrypt password

--- app/models/oauth_client.rb
+++ app/models/oauth_client.rb
@@ -1,5 +1,10 @@
 class OauthClient < LdapBase
+  include Puavo::Security
+
   ldap_mapping( :dn_attribute => "puavoOAuthClientId",
                 :prefix => "ou=Clients,ou=OAuth",
                 :classes => ["simpleSecurityObject", "puavoOAuthClient"] )
+
+  before_save :encrypt_userPassword
+
 end
