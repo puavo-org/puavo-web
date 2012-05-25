e36f5d886b58ce311819d4075c16aacf360ab258
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-05-25 16:23:12 +0300

    Load OAuth config to Puavo::OAUTH_CONFIG required by puavo_authentication gem!

--- config/initializers/puavo.rb
+++ config/initializers/puavo.rb
@@ -1,5 +1,7 @@
 Puavo.available_languages = ['fi', 'en', 'sv']
 
+Puavo::OAUTH_CONFIG = YAML.load_file("#{ RAILS_ROOT }/config/oauth.yml")
+
 Puavo::DEVICE_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/puavo_devices.yml") rescue nil
 
 Puavo::EXTERNAL_LINKS = YAML.load_file("#{RAILS_ROOT}/config/puavo_external_links.yml") rescue nil
