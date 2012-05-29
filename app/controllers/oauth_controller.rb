db90bc93af367e439ce645d4df0d9ef76117a24d
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2012-05-29 09:32:04 +0300

    Don't overwrite oauth_params

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -8,7 +8,7 @@ class OauthController < ApplicationController
   def authorize
 
     # Save parameters given by the Client Service
-    session[:oauth_params] = params
+    session[:oauth_params] = params unless session[:oauth_params]
 
     # No need to show anything to user if the service is trusted
     return redirect_with_access_code if trusted_client_service?
