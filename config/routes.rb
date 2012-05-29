22f4bb566891a6830bac5b5126486ce70049c674
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-05-29 13:37:11 +0300

    Add ping method to OauthController

--- config/routes.rb
+++ config/routes.rb
@@ -154,5 +154,6 @@ ActionController::Routing::Routes.draw do |map|
     oauth.oauth_authorize "oauth/authorize", :action => 'authorize', :conditions => {:method => :get}
     oauth.oauth_access_token 'oauth/authorize', :action => 'code', :conditions => {:method => :post}
     oauth.oauth_refresh_access_token 'oauth/token', :action => 'refresh_token', :conditions => {:method => :post}
+    oauth.ping 'oauth/ping', :action => 'ping'
   end
 end
