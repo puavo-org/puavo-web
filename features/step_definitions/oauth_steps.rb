5de3c8d1d34a8d0c5edb619804826ff770f526ef
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-05-29 17:44:27 +0300

    Fix client_id in oauth cucumber steps

--- features/step_definitions/oauth_steps.rb
+++ features/step_definitions/oauth_steps.rb
@@ -2,7 +2,7 @@
 Given /^I have been redirected to (.*)$/ do |page_name|
   visit( url_for(:controller => :oauth,
                  :action => :authorize,
-                 :client_id => 'fXLDE4FKas42DFgsfhRTfdlizK7oEm',
+                 :client_id => 'fXLDE5FKas42DFgsfhRTfdlizK7oEm',
                  :scope => 'read:presonalInfo',
                  :redirect_uri => 'http://www.example2.com',
                  :state => '123456789',
@@ -16,8 +16,8 @@ Then /^I should get OAuth access token with access code$/ do
   params[:redirect_uri].should contain("http://www.example2.com")
   visit( oauth_access_token_path,
          :post, {
-           :client_id => 'fXLDE3FKas42DFgsfhRTfdlizK7oEm',
-           :client_secret => 'zK7oEm34gYk3hA54DKX8da4', 
+           :client_id => 'fXLDE5FKas42DFgsfhRTfdlizK7oEm',
+           :client_secret => 'zK7oEm34gYk3hA54DKX8da4',
            :grant_type => 'authorization_code',
            :code => params[:code], 
            :redirect_uri => 'http://www.example2.com',
