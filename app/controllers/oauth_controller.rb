998db0eaedb4137bd107274ceeed4ea881168da1
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-05-30 10:16:15 +0300

    Implement get Access Token resource

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -31,30 +31,47 @@ class OauthController < ApplicationController
   # This post comes from the client server
   # Here we exchange the code with the token
   def token
-    ac = AccessCode.find_by_access_code_and_client_id params[:code], params[:client_id]
     # NO need to authenticate here! Handled by puavo_authentication already
+    client_dn = authentication.dn
 
-    raise "TODO: build Access Code now!"
+    host = session[:organisation].ldap_host
+    base = session[:organisation].ldap_base
+    default_ldap_configuration = ActiveLdap::Base.ensure_configuration
+    authentication.configure_ldap_connection(
+      default_ldap_configuration["bind_dn"],
+      default_ldap_configuration["password"],
+      host,
+      base)
+
+    ac = AccessCode.find_by_access_code_and_client_id params[:code], params[:client_id]
 
     redirect_uri = params[:redirect_uri]
     grant_type = params[:grant_type]
 
     at = AccessToken.new
 
-    at.userPassword = "secret" # TODO: generate with uuid or something
-    at.puavoOAuthAccessTokenId = "342343" # TODO: geneate with uuid or something
-    at.puavoUser = current_user.dn
-    at.puavoOAuthClient = params[:client_id]
+    at.puavoOAuthAccessTokenId = UUID.new.generate
+    at.userPassword = UUID.new.generate
+    at.puavoOAuthEduPerson = ac.user_dn
+    at.puavoOAuthClient = client_dn
 
-    # NOTE: host and base are found from authenticate.host authenticate.base
+    at.save!
+
+    access_token = token_manager.encrypt(
+      at.dn.to_s,
+      at.userPassword,
+      authentication.host,
+      authentication.base
+    )
+
     token_hash = {
-         :access_token => "xxx", # TODO: use token_manager.encrypt(token_dn, token_pw, host, base)
+         :access_token => access_token,
          :token_type => "Bearer",
          :expires_in => 3600,
          :refresh_token => nil
     }
 
-    render :text, token_hash.to_json
+    render :json => token_hash.to_json
   end
 
   # POST /oauth/token
