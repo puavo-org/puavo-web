c02ec5a4e3c90ef456a2ff2daede731868ffbcdb
Markus Vuori <mvuori@opinsys.fi>
2012-05-30 16:11:11 +0300

    Refresh Token: encrypt user password

--- app/models/refresh_token.rb
+++ app/models/refresh_token.rb
@@ -1,8 +1,12 @@
 class RefreshToken < LdapBase
+  include Puavo::Security
 
-ldap_mapping( :dn_attribute => "puavoOAuthTokenId",
+
+  ldap_mapping( :dn_attribute => "puavoOAuthTokenId",
               :prefix => "ou=Tokens,ou=OAuth",
               :classes => ["simpleSecurityObject", "puavoOAuthRefreshToken"] )
+
+  before_save :encrypt_userPassword
 
 
 end
