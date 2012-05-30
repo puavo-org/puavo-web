7dcd83db57a697bb72aeb855bbe4a85fb5538ab8
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-05-30 13:50:33 +0300

    Remove OAuth Client server access. Moved to OAuthController.

--- lib/puavo_authentication/controllers/helpers.rb
+++ lib/puavo_authentication/controllers/helpers.rb
@@ -59,12 +59,6 @@ module PuavoAuthentication
           return User.uid_to_dn(uid), session[:password_plaintext], uid
         end
 
-        # OAuth Client server Access Token request
-        if request.method == "post" && request.path == "/oauth/authorize" # TODO: use variable
-          if oc = OauthClient.find(:first, :attribute => "puavoOAuthClientId", :value => params["client_id"])
-            return oc.dn,  params["client_secret"]
-          end
-        end
 
       end
 
