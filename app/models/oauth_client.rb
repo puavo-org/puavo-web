97130d3e6d6e5cc36a1ecf3c58a487b3f5b7e073
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2012-06-01 16:10:19 +0300

    Add puavoOAuthClientType attribute

--- app/models/oauth_client.rb
+++ app/models/oauth_client.rb
@@ -5,12 +5,16 @@ class OauthClient < LdapBase
                 :prefix => "ou=Clients,ou=OAuth",
                 :classes => ["simpleSecurityObject", "puavoOAuthClient"] )
 
-  before_validation  :generate_puavoOAuthClientId
+  before_validation  :generate_puavoOAuthClientId, :set_client_type
   before_save :encrypt_userPassword
 
   private
 
   def generate_puavoOAuthClientId
-    self.puavoOAuthClientId = "oauth_client_id/" + UUID.new.generate
+    self.puavoOAuthClientId = UUID.new.generate
+  end
+
+  def set_client_type
+    self.puavoOAuthClientType = 'confidential'
   end
 end
