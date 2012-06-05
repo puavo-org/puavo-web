9304c944fa7003499ea542d034ac796cd24092c0
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-06-05 17:41:23 +0300

    Add consume class method to AuthorizationCode

--- app/models/authorization_code.rb
+++ app/models/authorization_code.rb
@@ -2,9 +2,20 @@
 class AuthorizationCode < ActiveRecord::Base
   LIFETIME = 2.minutes
 
+  class Expired < UserError
+  end
+
   def expired?
     age = Time.now - created_at
     age > LIFETIME
+  end
+
+  def consume
+    if expired?
+      destroy
+      raise Expired
+    end
+    destroy
   end
 
 end
