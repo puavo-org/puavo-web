1155170cc9e9d9d8e52f6ce131a208279178ec42
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-06-06 16:48:21 +0300

    Generalize token validation & finding for Access&Refresh tokens

--- app/models/refresh_token.rb
+++ app/models/refresh_token.rb
@@ -10,32 +10,13 @@ class RefreshToken < LdapBase
 
   before_save :encrypt_userPassword
 
+
   class Expired < UserError
     attr_accessor :token
     def initialize(message, token)
       super message
       @token = token
     end
-  end
-
-  def self.validate(token)
-    if self.expired? token[:created]
-      rt = RefreshToken.find token[:dn]
-      rt.userPassword = RefreshToken.generate_nonsense
-      rt.save!
-      raise Expired.new "Reresh Token Expired", token
-    end
-    return true
-  end
-
-  def self.find_and_validate(token)
-    self.validate token
-    self.find token[:dn]
-  end
-
-  def self.expired?(created)
-    age = Time.now - created.to_time
-    return age > LIFETIME
   end
 
 
