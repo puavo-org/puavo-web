662d04a47263babf5e6605730acc30126ee4cda8
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-08-23 09:22:23 +0300

    Clean up user specs

--- spec/acl/user_spec.rb
+++ spec/acl/user_spec.rb
@@ -8,40 +8,25 @@ describe "ACL" do
 
 
   describe "password" do
+      before(:all) { env.reset }
 
-    before(:each) { env.reset }
+      it "should enforce basic password change permissions" do
 
-    it "should allow students to change their own passwords" do
-      env.student.can_set_password_for :student
-    end
+        env.student.can_set_password_for :student
+        env.reset
+        env.student.cannot_set_password_for :student2, LDAPTestEnvException
+        env.reset
+        env.admin.can_set_password_for :student
+        env.reset
+        env.admin.can_set_password_for :teacher
+        env.reset
+        env.teacher.can_set_password_for :student
+        env.reset
+        env.teacher.cannot_set_password_for :teacher2,  LDAPTestEnvException
+        env.reset
+        env.admin.can_set_password_for :other_school_student
 
-    it "should not allow students to change other's passwords" do
-      lambda {
-        env.student.can_set_password_for :student2
-      }.should raise_error LDAPException
-    end
-
-    it "should allow school admin to change student password" do
-      env.admin.can_set_password_for :student
-    end
-
-    it "should allow school admin to change teacher's passwords" do
-      env.admin.can_set_password_for :teacher
-    end
-
-    it "should allow teacher to change student password" do
-      env.teacher.can_set_password_for :student
-    end
-
-    it "should not allow teachers to change other teacher's paswords" do
-      lambda {
-        env.teacher.can_set_password_for :teacher2
-      }.should raise_error LDAPException
-    end
-
-    it "should allow school admins to change attributes of students in other schools" do
-      env.admin.can_set_password_for :other_school_student
-    end
+      end
 
   end
 
@@ -50,76 +35,32 @@ describe "ACL" do
 
     before(:all) { env.reset }
 
-    it "should allow students to read their own attributes" do
-      env.student.can_read :student, [:sn, :givenName]
-    end
+    it "should enforce basic permissions" do
 
-    it "should allow students to read other users" do
-      [:teacher, :student2, :admin].each do |user|
-        env.student.can_read user, [:sn, :givenName, :uid]
-      end
-    end
+      env.student.can_read :student,        [:sn,       :givenName,  :uid]
+      env.student.can_read :student2,       [:sn,       :givenName,  :uid]
+      env.student.can_read :teacher,        [:sn,       :givenName,  :uid]
+      env.student.can_read :admin,          [:sn,       :givenName,  :uid]
 
-    it "should not allow teachers to modify students" do
-      lambda {
-        env.teacher.can_modify :student, [:replace, :givenName, ["newname"]]
-      }.should raise_error InsufficientAccessRights
-    end
+      env.student.cannot_modify :student,   [:replace,  :givenName,  ["bad"]],              InsufficientAccessRights
+      env.student.cannot_modify :student2,  [:replace,  :givenName,  ["newname"]],          InsufficientAccessRights
+      env.student.cannot_modify :student2,  [:replace,  :mail,       ["bad@example.com"]],  InsufficientAccessRights
 
-    it "should allow student to modify its own email" do
-      env.student.can_modify :student, [:replace, :mail, ["foo@example.com"]]
-    end
+      env.teacher.cannot_modify :student,   [:replace,  :givenName,  ["newname"]],          InsufficientAccessRights
 
-    it "should not allow student to modify its own name" do
-      lambda {
-        env.student.can_modify :student, [:replace, :givenName, ["bad"]]
-      }.should raise_error InsufficientAccessRights
-    end
+      env.admin.can_modify :student,        [:replace,  :givenName,  ["newname"]]
+      env.admin.can_modify :teacher,        [:replace,  :givenName,  ["newname"]]
 
-    it "should not allow users to have same email addresses" do
-
-      env.student.can_modify :student, [:replace, :mail, ["foo@example.com"]]
-
-      lambda {
-          env.student.can_modify :student2, [:replace, :mail, ["foo@example.com"]]
-      }.should raise_error ConstraintViolation
+      env.teacher.cannot_modify :admin,     [:replace,  :givenName,  ["newname"]],          InsufficientAccessRights
 
     end
 
-    it "should not allow students to modify other students" do
-        lambda {
-          env.student.can_modify :student2, [:replace, :givenName, ["newname"]]
-        }.should raise_error InsufficientAccessRights
-
-        lambda {
-          env.student.can_modify :student2, [:replace, :mail, ["bad@example.com"]]
-        }.should raise_error InsufficientAccessRights
+    it "should not allow same email for two students" do
+      env.student.can_modify :student,       [:replace,  :mail,  ["foo@example.com"]]
+      env.student2.cannot_modify :student2,  [:replace,  :mail,  ["foo@example.com"]],  ConstraintViolation
     end
 
-    it "should allow admins to modify students" do
-        env.admin.can_modify :student, [:replace, :givenName, ["newname"]]
-    end
-
-    it "should allow admins to modify teachers" do
-        env.admin.can_modify :teacher, [:replace, :givenName, ["newname"]]
-    end
-
-    it "should not allow teachers to change admin attributes" do
-      lambda {
-        env.teacher.can_modify :admin, [:replace, :givenName, ["newname"]]
-      }.should raise_error InsufficientAccessRights
-    end
 
   end
-
-
-  # it "should not allow teachers to change admin passwords" do
-  # end
-
-
-
-  # it "should allow only organisation owner to change organisation attributes" do
-  # end
-
 
 end
