0a0d558953745f729bd59834c7424e5e2d708be6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-08-24 13:14:45 +0300

    Clean rest of the tests

--- acl/test/samba_domain_acl_test.rb
+++ acl/test/samba_domain_acl_test.rb
@@ -2,11 +2,10 @@
 env = LDAPTestEnv.new
 
 env.validate "Samba Domain" do
-  env.student
-  env.admin.can_read       :domain_users,   [:memberUid]
-  env.admin.can_modify     :domain_users,   [:replace,    :memberUid,  ["testexampleuid"]]
-  env.admin.cannot_read    :domain_admins,  [:memberUid],                                    InsufficientAccessRights
-  env.admin.cannot_modify  :domain_admins,  [:replace,    :memberUid,  ["testexampleuid"]],  InsufficientAccessRights
-  env.owner.can_read       :domain_admins,  [:memberUid]
-  env.owner.can_modify     :domain_admins,  [:replace,    :memberUid,  ["testexampleuid"]]
+  admin.can_read       domain_users,   [:memberUid]
+  admin.can_modify     domain_users,   [:replace,    :memberUid,  ["testexampleuid"]]
+  admin.cannot_read    domain_admins,  [:memberUid],                                    InsufficientAccessRights
+  admin.cannot_modify  domain_admins,  [:replace,    :memberUid,  ["testexampleuid"]],  InsufficientAccessRights
+  owner.can_read       domain_admins,  [:memberUid]
+  owner.can_modify     domain_admins,  [:replace,    :memberUid,  ["testexampleuid"]]
 end
