98306fc5d94b76400d9f772527be3c4d8341000d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-08-24 09:37:37 +0300

    Use with_data in group spec

--- spec/acl/group_spec.rb
+++ spec/acl/group_spec.rb
@@ -7,21 +7,20 @@ describe "ACL" do
 
   env = LDAPTestEnv.new
 
-  before(:all) { env.reset }
 
-  describe "groups" do
-    it "should enforce basic group permissions" do
-      env.owner.can_read :group, [:displayName, :puavoSchool]
-      env.admin.can_read :group, [:displayName, :puavoSchool]
-      env.teacher.can_read :group, [:displayName, :puavoSchool]
-      env.student.can_read :group, [:displayName, :puavoSchool]
+    describe "groups" do
+      it "should enforce basic group permissions" do
+        env.with_data do
 
-      env.student.cannot_modify :group, [ :replace, :displayName, ["newname"] ], InsufficientAccessRights
-      env.teacher.cannot_modify :group, [ :replace, :displayName, ["newname"] ], InsufficientAccessRights
-    end
+          owner.can_read group, [:displayName, :puavoSchool]
+          admin.can_read group, [:displayName, :puavoSchool]
+          teacher.can_read group, [:displayName, :puavoSchool]
+          student.can_read group, [:displayName, :puavoSchool]
 
+          student.cannot_modify group, [ :replace, :displayName, ["newname"] ], InsufficientAccessRights
+          teacher.cannot_modify group, [ :replace, :displayName, ["newname"] ], InsufficientAccessRights
 
-
+        end
+      end
   end
-
 end
