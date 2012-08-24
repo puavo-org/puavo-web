1f0ae72a38edba5250d0e532d44110514a97eb69
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-08-24 12:55:46 +0300

    Fix test organisation acl test order

--- spec/acl/ldap_organisation_spec.rb
+++ spec/acl/ldap_organisation_spec.rb
@@ -20,6 +20,26 @@ describe "ACL" do
       env.owner.cannot_modify :organisation, [ :replace, :puavoRemoteDesktopPrivateKey,  ["dfsadfowieroasdfasodf"]       ],  InsufficientAccessRights
       env.owner.cannot_modify :organisation, [ :add,     :owner,                         [env.teacher.dn]                ],  InsufficientAccessRights
 
+
+      # Owner can modify following attributes
+      env.owner.can_modify :organisation, [ :replace, :description,                  ["Example description of example2 organisation"] ]
+      env.owner.can_modify :organisation, [ :replace, :eduOrgHomePageURI,            ["http://www.example.com"]                       ]
+      env.owner.can_modify :organisation, [ :replace, :facsimileTelephoneNumber,     ["+358017123456789"]                             ]
+      env.owner.can_modify :organisation, [ :replace, :l,                            ["Example city"]                                 ]
+      env.owner.can_modify :organisation, [ :replace, :o,                            ["Example2"]                                     ]
+      env.owner.can_modify :organisation, [ :replace, :postOfficeBox,                ["PL 12345"]                                     ]
+      env.owner.can_modify :organisation, [ :replace, :postalAddress,                ["Examplestreet 55"]                             ]
+      env.owner.can_modify :organisation, [ :replace, :postalCode,                   ["1234567"]                                      ]
+      env.owner.can_modify :organisation, [ :replace, :preferredLanguage,            ["English"]                                      ]
+      env.owner.can_modify :organisation, [ :replace, :puavoDeviceAutoPowerOffMode,  ["custom"]                                       ]
+      env.owner.can_modify :organisation, [ :replace, :puavoDeviceOffHour,           ["17"]                                           ]
+      env.owner.can_modify :organisation, [ :replace, :puavoDeviceOnHour,            ["07"]                                           ]
+      env.owner.can_modify :organisation, [ :replace, :puavoEduOrgAbbreviation,      ["example2"]                                     ]
+      env.owner.can_modify :organisation, [ :replace, :st,                           ["Example state"]                                ]
+      env.owner.can_modify :organisation, [ :replace, :street,                       ["Examplestreet 55"]                             ]
+      env.owner.can_modify :organisation, [ :replace, :telephoneNumber,              ["+358017123456789"]                             ]
+      env.owner.can_modify :organisation, [ :replace, :eduOrgLegalName,              ["example2"]                                     ]
+
       # Owner can read following attributes
       env.owner.can_read :organisation, [ :cn,
                                           :description,
@@ -46,26 +66,6 @@ describe "ACL" do
                                           :st,
                                           :street,
                                           :telephoneNumber ]
-
-
-      # Owner can modify following attributes
-      env.owner.can_modify :organisation, [ :replace, :description,                  ["Example description of example2 organisation"] ]
-      env.owner.can_modify :organisation, [ :replace, :eduOrgHomePageURI,            ["http://www.example.com"]                       ]
-      env.owner.can_modify :organisation, [ :replace, :facsimileTelephoneNumber,     ["+358017123456789"]                             ]
-      env.owner.can_modify :organisation, [ :replace, :l,                            ["Example city"]                                 ]
-      env.owner.can_modify :organisation, [ :replace, :o,                            ["Example2"]                                     ]
-      env.owner.can_modify :organisation, [ :replace, :postOfficeBox,                ["PL 12345"]                                     ]
-      env.owner.can_modify :organisation, [ :replace, :postalAddress,                ["Examplestreet 55"]                             ]
-      env.owner.can_modify :organisation, [ :replace, :postalCode,                   ["1234567"]                                      ]
-      env.owner.can_modify :organisation, [ :replace, :preferredLanguage,            ["English"]                                      ]
-      env.owner.can_modify :organisation, [ :replace, :puavoDeviceAutoPowerOffMode,  ["custom"]                                       ]
-      env.owner.can_modify :organisation, [ :replace, :puavoDeviceOffHour,           ["17"]                                           ]
-      env.owner.can_modify :organisation, [ :replace, :puavoDeviceOnHour,            ["07"]                                           ]
-      env.owner.can_modify :organisation, [ :replace, :puavoEduOrgAbbreviation,      ["example2"]                                     ]
-      env.owner.can_modify :organisation, [ :replace, :st,                           ["Example state"]                                ]
-      env.owner.can_modify :organisation, [ :replace, :street,                       ["Examplestreet 55"]                             ]
-      env.owner.can_modify :organisation, [ :replace, :telephoneNumber,              ["+358017123456789"]                             ]
-      env.owner.can_modify :organisation, [ :replace, :eduOrgLegalName,              ["example2"]                                     ]
 
     end
   end
