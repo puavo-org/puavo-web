64945e0c03f5c8df18b70137059d227afab9a059
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-08-27 16:12:01 +0300

    Add some base for OAuth ACL tests

---
+++ acl/test/oauth_acl_test.rb
@@ -0,0 +1,38 @@
+
+env = LDAPTestEnv.new
+# No working implementation yet
+env.disable
+
+env.validate "OAuth Access Token creation" do
+
+  student.can_add :authorization_code, client_server.id
+  client_server.can_add :access_token, client_server.id
+
+end
+
+env.validate "OAuth Access Token cannot be added without Authorization Code" do
+  client_server.cannot_add :authorization_code, client_server.id
+end
+
+
+env.validate "OAuth Access Token" do
+
+  student_access_token.can_read student,        [:sn,       :givenName,  :uid]
+  student_access_token.can_read student2,       [:sn,       :givenName,  :uid]
+  student_access_token.can_read teacher,        [:sn,       :givenName,  :uid]
+  student_access_token.can_read admin,          [:sn,       :givenName,  :uid]
+
+  student_access_token.cannot_modify student,   [:replace,  :givenName,  ["bad"]],              InsufficientAccessRights
+  student_access_token.cannot_modify student2,  [:replace,  :givenName,  ["newname"]],          InsufficientAccessRights
+  student_access_token.cannot_modify student2,  [:replace,  :mail,       ["bad@example.com"]],  InsufficientAccessRights
+
+  teacher_access_token.cannot_modify student,   [:replace,  :givenName,  ["newname"]],          InsufficientAccessRights
+
+  admin_access_token.can_modify student,        [:replace,  :givenName,  ["newname"]]
+  admin_access_token.can_modify teacher,        [:replace,  :givenName,  ["newname"]]
+
+  teacher_access_token.cannot_modify admin,     [:replace,  :givenName,  ["newname"]],          InsufficientAccessRights
+
+end
+
+
