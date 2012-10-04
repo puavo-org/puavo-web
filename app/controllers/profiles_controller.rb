f81a3ddc067c26f0320a25c75e1da3319de5f34c
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2012-10-04 09:14:29 +0300

    Profile updating

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,4 +10,20 @@ class ProfilesController < ApplicationController
       format.html
     end
   end
+
+  # PUT /profile
+  def update
+    
+    @user = User.find( session[:dn] )
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = t('flash.profile.updated')
+        format.html { render :action => "edit" }
+      else
+        flash[:alert] = t('flash.profile.save_failed')
+        format.html { render :action => "edit" }
+      end
+    end
+  end
 end
