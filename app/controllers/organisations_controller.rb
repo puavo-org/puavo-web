991257213c0c31e3616988ae34ed538abbc9cbae
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2012-11-29 14:05:30 +0200

    Add base to organisation json data

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -9,9 +9,11 @@ class OrganisationsController < ApplicationController
       format.json do
         json = JSON.parse @organisation.to_json
 
+        # FIXME: following ldap host is not specified organisation host
         json[:ldap_host] = LdapBase.ensure_configuration["host"]
         json[:kerberos_realm] = @organisation.puavoKerberosRealm
         json[:puavo_domain] = @organisation.puavoDomain
+        json[:base] = @organisation.base.to_s
 
         render :json => json
       end
