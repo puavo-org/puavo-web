7e05b63112151a5ea6d859ff3f48f09848349ebc
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-01-11 13:12:13 +0200

    Environments: Remove duplicate configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,6 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 config.cache_store = :memory_store
 
-config.cache_store = :memory_store
-
 if File.exists?(File.join(RAILS_ROOT,'tmp', 'debug.txt'))
   require 'ruby-debug'
   Debugger.wait_connection = true
