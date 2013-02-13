6a5bb06dd514b562b31887ed07eeb0193e2951b8
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-02-13 14:31:16 +0200

    Refactor hash building of device

--- server/app/models/device.rb
+++ server/app/models/device.rb
@@ -23,103 +23,12 @@ class Device < DeviceBase
     super(args)
   end
 
-  # Building hash for to_json method with better name of attributes
-  #  * data argument may be Device or Hash
-  def self.build_hash_for_to_json(data)
-    new_device_hash = {}
+  def self.json_attributes
     # Note: value of attribute may be raw ldap value eg. { puavoHostname => ["thinclient-01"] }
-    device_attributes = [
-       { :original_attribute_name => "description",
-         :new_attribute_name => "description",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "ipHostNumber",
-         :new_attribute_name => "ip_address",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "jpegPhoto",
-         :new_attribute_name => "image",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "macAddress",
-         :new_attribute_name => "mac_address",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDefaultPrinter",
-         :new_attribute_name => "default_printer",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceAutoPowerOffMode",
-         :new_attribute_name => "auto_power_mode",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceBootMode",
-         :new_attribute_name => "boot_mode",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceManufacturer",
-         :new_attribute_name => "manufacturer",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceModel",
-         :new_attribute_name => "model",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoLatitude",
-         :new_attribute_name => "latitude",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoLocationName",
-         :new_attribute_name => "location_name",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoLongitude",
-         :new_attribute_name => "longitude",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoPurchaseDate",
-         :new_attribute_name => "purchase_date",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoPurchaseLocation",
-         :new_attribute_name => "purchase_location",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoPurchaseURL",
-         :new_attribute_name => "purchase_url",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoSupportContract",
-         :new_attribute_name => "support_contract",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoTag",
-         :new_attribute_name => "tags",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoWarrantyEndDate",
-         :new_attribute_name => "warranty_end_date",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "serialNumber",
-         :new_attribute_name => "serialnumber",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoHostname",
-         :new_attribute_name => "hostname",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoSchool",
-         :new_attribute_name => "school_id",
-         :value_block => lambda{ |value| value.to_s.match(/puavoId=([^, ]+)/)[1].to_i } },
-       { :original_attribute_name => "puavoId",
-         :new_attribute_name => "puavo_id",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceKernelVersion",
-         :new_attribute_name => "kernel_version",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceKernelArguments",
-         :new_attribute_name => "kernel_arguments",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceXrandr",
-         :new_attribute_name => "xrandr",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceXrandrDisable",
-         :new_attribute_name => "xrandr_disable",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceType",
-         :new_attribute_name => "device_type",
-         :value_block => lambda{ |value| Array(value).first } },
-       { :original_attribute_name => "puavoDeviceImage",
-         :new_attribute_name => "device_image",
-         :value_block => lambda{ |value| Array(value).first } } ]
-
-    device_attributes.each do |attr|
-      attribute_value = data.class == Hash ? data[attr[:original_attribute_name]] : data.send(attr[:original_attribute_name])
-      new_device_hash[attr[:new_attribute_name]] = attr[:value_block].call(attribute_value)
-    end
-    return new_device_hash
-  end
+    DeviceBase.json_attributes.push( { :original_attribute_name => "puavoSchool",
+                                       :new_attribute_name => "school_id",
+                                       :value_block => lambda{ |value| value.to_s.match(/puavoId=([^, ]+)/)[1].to_i } } )
+end
 
   private
 
--- server/app/models/device_base.rb
+++ server/app/models/device_base.rb
@@ -297,4 +297,104 @@ class DeviceBase < LdapBase
       self.jpegPhoto = image_orig.resize_to_fit(220,220).to_blob
     end
   end
+
+  # Building hash for to_json method with better name of attributes
+  #  * data argument may be Device or Hash
+  def self.build_hash_for_to_json(data)
+    new_device_hash = {}
+
+    device_attributes = self.json_attributes
+
+    device_attributes.each do |attr|
+      attribute_value = data.class == Hash ? data[attr[:original_attribute_name]] : data.send(attr[:original_attribute_name])
+      new_device_hash[attr[:new_attribute_name]] = attr[:value_block].call(attribute_value)
+    end
+    return new_device_hash
+  end
+
+  def self.json_attributes
+    # Note: value of attribute may be raw ldap value eg. { puavoHostname => ["thinclient-01"] }
+    [
+     { :original_attribute_name => "description",
+       :new_attribute_name => "description",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "ipHostNumber",
+       :new_attribute_name => "ip_address",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "jpegPhoto",
+       :new_attribute_name => "image",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "macAddress",
+       :new_attribute_name => "mac_address",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDefaultPrinter",
+       :new_attribute_name => "default_printer",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceAutoPowerOffMode",
+       :new_attribute_name => "auto_power_mode",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceBootMode",
+       :new_attribute_name => "boot_mode",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceManufacturer",
+       :new_attribute_name => "manufacturer",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceModel",
+       :new_attribute_name => "model",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoLatitude",
+       :new_attribute_name => "latitude",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoLocationName",
+       :new_attribute_name => "location_name",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoLongitude",
+       :new_attribute_name => "longitude",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoPurchaseDate",
+       :new_attribute_name => "purchase_date",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoPurchaseLocation",
+       :new_attribute_name => "purchase_location",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoPurchaseURL",
+       :new_attribute_name => "purchase_url",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoSupportContract",
+       :new_attribute_name => "support_contract",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoTag",
+       :new_attribute_name => "tags",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoWarrantyEndDate",
+       :new_attribute_name => "warranty_end_date",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "serialNumber",
+       :new_attribute_name => "serialnumber",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoHostname",
+       :new_attribute_name => "hostname",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoId",
+       :new_attribute_name => "puavo_id",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceKernelVersion",
+       :new_attribute_name => "kernel_version",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceKernelArguments",
+       :new_attribute_name => "kernel_arguments",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceXrandr",
+       :new_attribute_name => "xrandr",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceXrandrDisable",
+       :new_attribute_name => "xrandr_disable",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceType",
+       :new_attribute_name => "device_type",
+       :value_block => lambda{ |value| Array(value).first } },
+     { :original_attribute_name => "puavoDeviceImage",
+       :new_attribute_name => "device_image",
+       :value_block => lambda{ |value| Array(value).first } } ]
+  end
 end
