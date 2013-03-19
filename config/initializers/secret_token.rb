bac56c882b97fcbc8c23f568c2ad72691bc6bd55
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-03-19 14:52:17 +0000

    Add rails 3 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuavoUsers::Application.config.secret_token = '4b1f14b0c0ee9a4cb6525fbc3a77ce159e7286d0dac6495a40c350ad81be682ccf459b0a1bf3ffa9435fcccb29a172e8a448b4586aeafc5fa784d7736cba46c9'
