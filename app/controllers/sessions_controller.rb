6b92127cde566aed38a92cf9e2d0ea25cd4a2e18
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-03-20 13:53:53 +0200

    Fix redirecting to root path. See: http://rack.rubyforge.org/doc/SPEC.html

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     session[:uid] = params[:user][:uid]
     session[:password_plaintext] = params[:user][:password]
     session[:login_flash] = t('flash.session.login_successful')
-    redirect_back_or_default root_path
+    redirect_back_or_default  ENV["PATH_INFO"] || "/"
   end
 
   def auth
