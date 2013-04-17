3ec3ad66f46228322d5b1f0f462535e8fa5f3507
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-17 14:41:26 +0000

    Change session key Make sure that it won't collide with existing Rails2 session ids when Puavo is updated to production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PuavoUsers::Application.config.session_store :active_record_store
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Rails3::Application.config.session_store :active_record_store
+PuavoUsers::Application.config.session_store(
+    :active_record_store,
+    :key => "_puavo2_sessions_id"
+)
