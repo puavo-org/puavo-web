49ede062214eb79631e80098188a333db26430fd
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-29 12:11:26 +0000

    specs: Setup ldap connection in spec_helper.rb

--- spec/models/external_file_spec.rb
+++ spec/models/external_file_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
-test_organisation = Puavo::Organisation.find('example')
-default_ldap_configuration = ActiveLdap::Base.ensure_configuration
-# Setting up ldap configuration
-LdapBase.ldap_setup_connection( test_organisation.ldap_host,
-                                test_organisation.ldap_base,
-                                default_ldap_configuration["bind_dn"],
-                                default_ldap_configuration["password"] )
-
-@owner_dn = User.find(:first, :attribute => "uid", :value => test_organisation.owner).dn.to_s
-@owner_password = test_organisation.owner_pw
-
-LdapBase.ldap_setup_connection( test_organisation.ldap_host,
-                                test_organisation.ldap_base,
-                                @owner_dn,
-                                @owner_password )
 
 describe ExternalFile do
   it "can save " do
