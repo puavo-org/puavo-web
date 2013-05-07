7ac5dcdf117add2d707a6e19bfb4d80a75eaf44c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-05-07 13:42:36 +0000

    Document base

--- resources/base.rb
+++ resources/base.rb
@@ -10,8 +10,74 @@ class LdapModel
   end
 
   # Escape unsafe user input for safe LDAP filter use
+  #
+  # @see https://github.com/ruby-ldap/ruby-net-ldap/blob/8ddb2d7c8476c3a2b2ad9fcd367ca0d36edaa611/lib/net/ldap/filter.rb#L247-L264
   def self.escape(s)
     # TODO
+    # https://github.com/ruby-ldap/ruby-net-ldap/blob/master/lib/net/ldap/filter.rb
+    s
+  end
+
+  # Define conversion between LDAP attribute and the JSON attribute
+  # @param ldap_name [Symbol] LDAP attribute to convert
+  # @param json_name [Symbol] Value conversion block. Default: Get first array item
+  # @param convert [Block] Use block to 
+  # @see convert
+  def self.ldap_attr_conversion(ldap_name, json_name, &convert)
+    @@ldap2json[ldap_name.to_s] = {
+      :attr => json_name.to_s,
+      :convert => convert || lambda { |v| Array(v).first }
+    }
+  end
+
+  # Return LDAP attributes that will be converted
+  def self.ldap_attrs
+    @@ldap2json.keys
+  end
+
+  # Covert LDAP entry Hash to JSON style Hash
+  # @param entry [Hash] Ruby Hash from #filter
+  # @param all [Boolean] Include also those attributes that have no attribute
+  # conversion
+  # @see ldap_attr_conversion
+  # @see #filter
+  def self.convert(entry, all=false)
+    h = {}
+
+    entry.each do |k,v|
+      if ob = @@ldap2json[k.to_s]
+        h[ob[:attr]] = ob[:convert].call(v)
+      elsif all
+        h[k] = v
+      end
+    end
+
+    return h
+  end
+
+  # LDAP base for this model. Must be implemented by subclasses
+  def ldap_base
+    raise "not implemented"
+  end
+
+  # LDAP::LDAP_SCOPE_SUBTREE filter search for #ldap_base
+  # @param filter [String] LDAP filter
+  # @param attributes [Array] Limit search results to these attributes
+  # @see #ldap_base
+  # @see http://ruby-ldap.sourceforge.net/rdoc/classes/LDAP/Conn.html#M000025
+  def filter(filter, attributes=nil)
+    res = []
+
+    @ldap_conn.search(
+      ldap_base,
+      LDAP::LDAP_SCOPE_SUBTREE,
+      filter,
+      attributes
+    ) do |entry|
+      res.push(entry.to_hash)
+    end
+
+    return res
   end
 
 end
