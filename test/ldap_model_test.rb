5750a58a8062ca752a504a58771f2d565a7f0ebc
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-05-08 07:06:42 +0000

    Add tests for attribute conversions

--- test/ldap_model_test.rb
+++ test/ldap_model_test.rb
@@ -1,10 +1,27 @@
 require "./test/helper"
 
-describe PuavoRest::Root do
+describe PuavoRest::LdapModel do
 
-  it "should respond hello on root" do
-    get "/"
-    assert_equal last_response.body, "hello"
+  class CustomModel < PuavoRest::LdapModel
+    ldap_attr_conversion :cn, :name
+    ldap_attr_conversion(:foo, :bar) { |v| v.to_i }
+  end
+
+  it "can convert atribute names" do
+    json = CustomModel.convert({
+      "cn" => ["foo"]
+    })
+    assert_equal "foo", json["name"]
+  end
+
+
+  it "can use custom value conversion blocks" do
+    json = CustomModel.convert({
+      "foo" => "2"
+    })
+    assert_equal 2, json["bar"]
   end
 
 end
+
+
