702b49ea541f5bcc319dfd453bdd824bd43a8991
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-06-05 11:48:20 +0300

    typo

--- rest/local_store.rb
+++ rest/local_store.rb
@@ -2,7 +2,7 @@ require "pstore"
 
 module PuavoRest
 
-# Writeable local store. Sense ldap connection is read only on boot servers we
+# Writeable local store. Since ldap connection is read only on boot servers we
 # need some simple store we can write to.. Here's simple Pstore wrapper.
 class LocalStore
 
