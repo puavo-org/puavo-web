e532cd95579535f2da3828dce3baa0a2b46e423d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-06-05 10:19:26 +0300

    rest. document default organisation

--- rest/ldap_model.rb
+++ rest/ldap_model.rb
@@ -31,9 +31,12 @@ class LdapModel
 
       if organisation_entry = puavo_ldap.organisation
         organisation = Puavo::Client::Base.new_by_ldap_entry( organisation_entry )
+
+        # Default organisation is the organisation of the bootserver
         if PUAVO_ETC.domain == organisation.domain
           organisations_by_domain["*"] = organisation.data
         end
+
         organisations_by_domain[organisation.domain] = organisation.data
       end
 
