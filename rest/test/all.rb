1e8cc651b5805386d02dbdf18976f83c7e9fe39a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-06-06 16:53:27 +0300

    rest: run all tests from all.rb :)

--- rest/test/all.rb
+++ rest/test/all.rb
@@ -1,3 +1,5 @@
-require_relative "./ltsp_servers_test"
-require_relative "./ldap_model_test"
-require_relative "./sessions_test"
+require_relative "./devices_test.rb"
+require_relative "./ldap_hash_test.rb"
+require_relative "./ldap_model_test.rb"
+require_relative "./ltsp_servers_test.rb"
+require_relative "./sessions_test.rb"
