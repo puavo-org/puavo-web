4e98316ee3c8bab963d7ed3512bd41f71fb87dd1
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-06-13 14:42:03 +0300

    Add puavoAllowGuest and puavoPersonalDevice attributes to device page

---
+++ app/models/boolean_attributes.rb
@@ -0,0 +1,26 @@
+module BooleanAttributes
+  
+  def puavoAllowGuest=(boolean)
+    if boolean == true || boolean == "true" || boolean == "TRUE"
+      boolean = "TRUE"
+    elsif boolean == false || boolean == "false" || boolean == "FALSE"
+      boolean = "FALSE"
+    else
+      boolean = nil
+    end
+    set_attribute("puavoAllowGuest", boolean)
+  end
+
+  def puavoPersonalDevice=(boolean)
+    if boolean == true || boolean == "true" || boolean == "TRUE"
+      boolean = "TRUE"
+    elsif boolean == false || boolean == "false" || boolean == "FALSE"
+      boolean = "FALSE"
+    else
+      boolean = nil
+    end
+    set_attribute("puavoPersonalDevice", boolean)
+  end
+
+
+end
