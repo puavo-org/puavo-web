d0610384c4427e82267782102c6c1c240899f674
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-06-14 16:24:32 +0300

    tests: better error message if test org is missing

--- rest/test/helper.rb
+++ rest/test/helper.rb
@@ -41,7 +41,11 @@ def setup_connection
                                   default_ldap_configuration["bind_dn"],
                                   default_ldap_configuration["password"] )
 
-  @owner_dn = User.find(:first, :attribute => "uid", :value => test_organisation.owner).dn.to_s
+  owner = User.find(:first, :attribute => "uid", :value => test_organisation.owner)
+  if owner.nil?
+    raise "Cannot find organisation owner for 'example'. Organisation not created?"
+  end
+  @owner_dn = owner.dn.to_s
   @owner_password = test_organisation.owner_pw
 
   LdapBase.ldap_setup_connection( test_organisation.ldap_host,
