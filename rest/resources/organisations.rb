4d0f6a1718efc0c3f479e9b41b4894e0b47cb60b
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-06-17 15:53:05 +0300

    rest: refactor wlan_networks parsing

--- rest/resources/organisations.rb
+++ rest/resources/organisations.rb
@@ -3,7 +3,7 @@ require 'puavo/ldap'
 module PuavoRest
 class Organisation < LdapHash
   ldap_map :puavoDeviceImage, :preferred_image
-  ldap_map :puavoWlanSSID, :wlan_networks, &School.method(:parse_wlan)
+  ldap_map :puavoWlanSSID, :wlan_networks, &LdapConverters.parse_wlan
   ldap_map :puavoAllowGuest, :allow_guest, &LdapConverters.string_boolean
   ldap_map :puavoPersonalDevice, :personal_device, &LdapConverters.string_boolean
 
--- rest/resources/schools.rb
+++ rest/resources/schools.rb
@@ -1,20 +1,9 @@
 module PuavoRest
 class School < LdapHash
 
-  # Parse wlan data from puavoWlanSSID attribute
-  def self.parse_wlan(networks)
-    networks.map do |n|
-      begin
-        JSON.parse(n)
-      rescue JSON::ParserError
-        # Legacy data is not JSON. Just ignore...
-      end
-    end.compact
-  end
-
   ldap_map :dn, :dn
   ldap_map :puavoDeviceImage, :preferred_image
-  ldap_map :puavoWlanSSID, :wlan_networks, &method(:parse_wlan)
+  ldap_map :puavoWlanSSID, :wlan_networks, &LdapConverters.parse_wlan
   ldap_map :puavoAllowGuest, :allow_guest, &LdapConverters.string_boolean
   ldap_map :puavoPersonalDevice, :personal_device, &LdapConverters.string_boolean
 
