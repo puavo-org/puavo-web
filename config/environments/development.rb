c915c2b9dd00c980a2d6a838ab1c81a681d6dda6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-06-18 13:13:05 +0300

    serve static assets in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ PuavoUsers::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
