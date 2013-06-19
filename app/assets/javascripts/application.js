21f40eacc7641ae1ff825c898aecb2ac8a929ec5
Janne Saarela <janne.saarela@opinsys.fi>
2013-06-19 15:26:18 +0300

    Change search-input definition to .search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,16 @@
 /* global jQuery */
-// This is a manifest file that'll be compiled into application.js, which will include all the files 
-// listed below.                                                                                     
-//                                                                                                   
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascript
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.    
-//                                                                                                   
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the    
-// the compiled file.                                                                                
-//                                                                                                   
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD      
-// GO AFTER THE REQUIRES BELOW.                                                                      
-//                                                                                                   
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
 //= require jquery
 //= require jquery_ujs
 //= require jquery.opensmenu
@@ -30,7 +30,7 @@ jQuery(document).ready(function($) {
     $(this).prev().append( clone_element );
   });
 
-  $('#search').liveSearch({
+  $('.search').liveSearch({
     minLength: 2,
     url: window.SEARCH_URLS,
     duration: 400,
