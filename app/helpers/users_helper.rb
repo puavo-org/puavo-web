30609b534c2ad831ae18cd505e07d299e93ee5aa
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-06-26 18:00:26 +0300

    add profile_image_path helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def profile_image_path(user=nil)
+    user ||= current_user
+    if user.jpegPhoto
+      image_user_path(user.school.id, user.id)
+    else
+      "anonymous.png"
+    end
+  end
 end
