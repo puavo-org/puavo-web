3ba01792a4e08d0f7ab05ebde99515a28bdc50b7
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-28 14:11:03 +0300

    sso: add puavoServicePathPrefix

--- script/add_external_application.rb
+++ script/add_external_application.rb
@@ -1,25 +1,30 @@
 # Usage: bundle exec rails runner script/add_external_application.rb
 
-def ask(question, default=nil)
+def ask(question, opts={})
   new_value = nil
   while true
-    print "#{question} [#{ default }]> "
+    print "#{question} [#{ opts[:default] }]"
+    print "(optional)" if opts[:optional]
+    print "> "
     new_value = STDIN.gets.strip
 
     # Use default or previous value if user did not give anything
-    new_value = default if new_value.to_s.empty?
+    new_value =  opts[:default] if new_value.to_s.empty?
 
-    # Break only when we have a value
+    # Break if we have value
     break if not new_value.to_s.empty?
+
+    # Allow empty value in optional
+    break if opts[:optional]
   end
   new_value
 end
 
 LdapBase.ldap_setup_connection(
-  ask("LDAP master", PUAVO_ETC.get(:ldap_master)),
+  ask("LDAP master", :default => PUAVO_ETC.get(:ldap_master)),
   "o=Puavo",
-  ask("LDAP admin dn", "uid=admin,o=puavo"),
-  ask("LDAP admin pw", PUAVO_ETC.get(:ldap_password))
+  ask("LDAP admin dn", :default => "uid=admin,o=puavo"),
+  ask("LDAP admin pw", :default => PUAVO_ETC.get(:ldap_password))
 )
 
 app = ExternalApplication.new
@@ -27,16 +32,16 @@ app.classes = ["top", "puavoJWTService"]
 
 app.cn = ask "Name"
 app.puavoServiceDomain = ask "Domain"
+app.puavoServicePathPrefix = ask "Path prefix", :optional => true
 app.puavoServiceSecret = ask "Shared secret"
 app.description = ask "Description"
 app.mail = ask "Maintainer email"
-app.puavoServiceDescriptionURL = ask "Description url"
+app.puavoServiceDescriptionURL = ask "Description url", :optional => true
 
-puts app
-
-if ask("Ok?", "y") == "y"
+puts
+if ask("Ok?", :default => "y") == "y"
   app.save!
-  puts app.inspect
+  puts "saved"
 else
   puts "abort"
 end
