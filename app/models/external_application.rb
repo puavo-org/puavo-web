1d2defa14d98ab208883f91150071efc13bd6326
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-28 14:06:37 +0300

    SSO: use puavoId as dn attr

--- app/models/external_application.rb
+++ app/models/external_application.rb
@@ -1,6 +1,16 @@
 class ExternalApplication < LdapBase
   ldap_mapping(
-    :dn_attribute => "puavoServiceDomain",
+    :dn_attribute => "puavoId",
     :prefix => "ou=Services"
   )
+
+  before_validation :set_puavo_id
+
+  def set_puavo_id
+    self.puavoId = IdPool.next_puavo_id if self.puavoId.nil?
+  end
+
+
 end
+
+
