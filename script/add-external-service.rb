56f317e26f182fece9719f8ce207f19d6ac716f2
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-09-02 14:55:30 +0300

    Ask whether the external service is trusted

--- script/add-external-service.rb
+++ script/add-external-service.rb
@@ -43,6 +43,7 @@ app.puavoServiceSecret = ask "Shared secret"
 app.description = ask "Description"
 app.mail = ask "Maintainer email"
 app.puavoServiceDescriptionURL = ask "Description url", :optional => true
+app.puavoServiceTrusted = ask("Trusted y/n?", :default => "n") == "y"
 
 puts
 if ask("Ok?", :default => "y") == "y"
