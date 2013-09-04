38f8e7c76b9d605843c4b0e41e306b50d611137e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-09-04 10:32:53 +0300

    ExternalService: Ensure slash in prefix and use prefix to detect duplicates

--- app/models/external_service.rb
+++ app/models/external_service.rb
@@ -6,24 +6,35 @@ class ExternalService < ActiveLdap::Base
     :prefix => "ou=Services"
   )
 
-  before_validation :set_puavo_id
-  validate :unique_domain
+  before_validation :set_puavo_id, :ensure_slash_in_prefix
+  validate :unique_domain_and_prefix
 
   def set_puavo_id
     self.puavoId = IdPool.next_puavo_id if self.puavoId.nil?
   end
 
-  def unique_domain
-    used_domains = self.class.all.select do |s|
+  def ensure_slash_in_prefix
+    if self.puavoServicePathPrefix && self.puavoServicePathPrefix[0] != "/"
+      self.puavoServicePathPrefix = "/#{ self.puavoServicePathPrefix }"
+    end
+  end
+
+  def domain_with_prefix
+    "#{ self.puavoServiceDomain }#{ self.puavoServicePathPrefix }"
+  end
+
+  def unique_domain_and_prefix
+
+    used = self.class.all.select do |s|
       self.new_entry? || s.dn != self.dn
     end.map do |s|
-      s.puavoServiceDomain
+      s.domain_with_prefix
     end
 
-    if used_domains.include?(self.puavoServiceDomain)
+    if used.include?(self.domain_with_prefix)
       errors.add(
         :puavoServiceDomain,
-        "Domain #{ self.puavoServiceDomain } is not unique"
+        "Domain #{ self.domain_with_prefix } is not unique"
       )
     end
   end
