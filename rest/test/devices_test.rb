7b20b4a1e2daa53ee051327fddf23f3e0ed3d52b
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-10-15 13:35:12 +0300

    rest: relocate test code

--- rest/test/devices_test.rb
+++ rest/test/devices_test.rb
@@ -178,39 +178,6 @@ describe PuavoRest::Devices do
 
   end
 
-  describe "device" do
-
-    before(:each) do
-      create_device(
-        :puavoHostname => "athin",
-        :macAddress => "bf:9a:8c:1b:e0:6a",
-        :puavoPreferredServer => @server1.dn,
-        :puavoSchool => @school.dn
-      )
-      get "/v3/bf:9a:8c:1b:e0:6a/boot_configuration", {}, {
-        "HTTP_AUTHORIZATION" => "Bootserver"
-      }
-      assert_200
-      @data = last_response.body
-    end
-
-    it "has following boot configuration" do
-      configuration =<<EOF
-default ltsp-NBD
-ontimeout ltsp-NBD
-
-
-label ltsp-NBD
-  menu label LTSP, using NBD
-  menu default
-  kernel ltsp/schoolprefimage/vmlinuz
-  append ro initrd=ltsp/schoolprefimage/initrd.img init=/sbin/init-puavo puavo.hosttype=thinclient root=/dev/nbd0 nbdroot=:schoolprefimage 
-  ipappend 2
-EOF
-      assert_equal configuration, @data
-    end
-  end
-
   describe "device boot configuration" do
     before(:each) do
       @thinclient01 = create_device(
