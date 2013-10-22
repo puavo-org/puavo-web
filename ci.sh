573e58c14d842bbad0eb7150b1e439427ccffc73
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-10-22 17:12:42 +0300

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,9 +3,12 @@
 set -x
 set -eu
 
+debbox_url="$1"
+version="$(sed -rne 's/^.*\"([^\"]+)\".*$/\1/p' config/version.rb)"
+
 sudo apt-get update
 
-sudo apt-get install -y puavo-standalone ruby1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev libsqlite3-dev libmagickwand-dev ldap-utils libpq-dev libssl-dev build-essential libopenssl-ruby xpdf-utils git libreadline6-dev libxml2-dev libxslt1-dev libpq-dev libmagickwand-dev libsqlite3-dev ruby-bundler puavo-client nodejs-bundle puavo-ca-rails redis-server
+sudo apt-get install -y puavo-standalone ruby1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev libsqlite3-dev libmagickwand-dev ldap-utils libpq-dev libssl-dev build-essential libopenssl-ruby xpdf-utils git libreadline6-dev libxml2-dev libxslt1-dev libpq-dev libmagickwand-dev libsqlite3-dev ruby-bundler puavo-client nodejs-bundle puavo-ca-rails redis-server puavo-devscripts
 
 sudo puavo-init-standalone --unsafe-passwords opinsys.net
 
@@ -13,17 +16,8 @@ sudo puavo-add-new-organisation --yes hogwarts --username albus --password albus
 sudo puavo-add-new-organisation --yes example --username cucumber --password cucumber --given-name cucumber --surname cucumber
 sudo puavo-add-new-organisation --yes anotherorg --username admin --password admin --given-name Admin --surname Administrator
 
-make
-bundle exec rake puavo:configuration
-bundle exec rake db:migrate
-
-cd rest
-make dev-install
-make test
-cd ..
-
-RAILS_ENV=test bundle exec rake db:migrate
-
-bundle exec cucumber --tags @start_test_server
-bundle exec cucumber --tags ~@start_test_server
+puavo-build-debian-dir
+puavo-dch $version
+puavo-debuild
+puavo-deb-upload "$debbox_url" ../*.deb
 
