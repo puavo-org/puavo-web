2639a873420846f0b0a7e9e864bb838d3b7c9980
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-10-24 09:50:11 +0300

    Api::V2::HostsController: add error handling. Return 404 status if device not found.

--- app/controllers/api/v2/hosts_controller.rb
+++ app/controllers/api/v2/hosts_controller.rb
@@ -1,22 +1,36 @@
 class Api::V2::HostsController < ApplicationController
 
+  class DeviceNotFound < StandardError
+  end
+
   # POST /hosts/:hostname/sign_certificate
   def sign_certificate
-    @host = Server.find(:first, :attribute => "puavoHostname", :value => params[:hostname])
 
-    if @host.nil?
-      @host = Device.find(:first, :attribute => "puavoHostname", :value => params[:hostname])
-    end
+    allowed_device_type = ["ltspserver", "bootserver", "infotv"]
 
+    begin
+      @host = Server.find(:first, :attribute => "puavoHostname", :value => params[:hostname])
 
-    @host.revoke_certificate( session[:organisation].organisation_key,
+      if @host.nil?
+        @host = Device.find(:first, :attribute => "puavoHostname", :value => params[:hostname])
+      end
+
+      if @host.nil?
+        raise DeviceNotFound
+      end
+
+      @host.revoke_certificate( session[:organisation].organisation_key,
+                                @authentication.dn,
+                                @authentication.password )
+      @host.host_certificate_request = params[:host_certificate_request]
+      @host.sign_certificate( session[:organisation].organisation_key,
                               @authentication.dn,
                               @authentication.password )
-    @host.host_certificate_request = params[:host_certificate_request]
-    @host.sign_certificate( session[:organisation].organisation_key,
-                            @authentication.dn,
-                            @authentication.password )
 
-    render :json => @host
+      render :json => @host
+
+    rescue DeviceNotFound
+      render :status => 404, :json => { :error => "Device not found (#{ params[:hostname] })" }
+    end
   end
 end
