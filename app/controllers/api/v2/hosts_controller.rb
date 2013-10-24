333ef31050a343791b5a890df7bf2c2874e1f4e7
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-10-24 09:48:23 +0300

    Api::V2::HostsController: revoke and sign certificate

--- app/controllers/api/v2/hosts_controller.rb
+++ app/controllers/api/v2/hosts_controller.rb
@@ -2,18 +2,21 @@ class Api::V2::HostsController < ApplicationController
 
   # POST /hosts/:hostname/sign_certificate
   def sign_certificate
-    # @server = Server.build_hash_for_to_json( Server.find(params[:id]).attributes )
     @host = Server.find(:first, :attribute => "puavoHostname", :value => params[:hostname])
 
-    unless @host
-      @host = Device.(:first, :attribute => "puavoHostname", :value => params[:hostname])
+    if @host.nil?
+      @host = Device.find(:first, :attribute => "puavoHostname", :value => params[:hostname])
     end
 
-    # @host.reveko_certificate
-    # @host.sign_certificate
-    
-    respond_to do |format|
-      format.json  { render :json => @host }
-    end
+
+    @host.revoke_certificate( session[:organisation].organisation_key,
+                              @authentication.dn,
+                              @authentication.password )
+    @host.host_certificate_request = params[:host_certificate_request]
+    @host.sign_certificate( session[:organisation].organisation_key,
+                            @authentication.dn,
+                            @authentication.password )
+
+    render :json => @host
   end
 end
