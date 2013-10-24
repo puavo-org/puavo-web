7568a905709bee8a310b346dac0c0667c73161e2
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-10-24 22:04:55 +0300

    Add org test

---
+++ rest/test/organisations_test.rb
@@ -0,0 +1,50 @@
+require_relative "./helper"
+
+describe PuavoRest::Organisations do
+
+  before(:each) do
+    Puavo::Test.clean_up_ldap
+    FileUtils.rm_rf CONFIG["ltsp_server_data_dir"]
+    @school = School.create(
+      :cn => "gryffindor",
+      :displayName => "Gryffindor"
+    )
+
+    @user = User.new(
+      :givenName => "Bob",
+      :sn  => "Brown",
+      :uid => "bob",
+      :puavoEduPersonAffiliation => "student",
+      :preferredLanguage => "en",
+      :mail => "bob@example.com"
+    )
+    @user.set_password "secret"
+    @user.puavoSchool = @school.dn
+    @user.role_ids = [
+      Role.find(:first, :attribute => "displayName", :value => "Maintenance").puavoId
+    ]
+    @user.save!
+  end
+
+  it "can be fetched by admin" do
+    basic_authorize "cucumber", "cucumber"
+    get "/v3/organisations"
+    assert_200
+    data = JSON.parse(last_response.body)
+    assert(
+      !data.select{ |o| o["domain"] == "www.example.net" }.empty?,
+      "Has www.example.net"
+    )
+  end
+
+  it "cannot be fetched by normal user" do
+    basic_authorize "bob", "secret"
+    get "/v3/organisations"
+    data = JSON.parse(last_response.body)
+    assert_equal 401, last_response.status
+    assert data["error"]
+    assert_equal data["error"]["code"], "Unauthorized"
+  end
+
+
+end
