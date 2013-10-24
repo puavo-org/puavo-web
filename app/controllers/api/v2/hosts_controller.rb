7f83300afd643f156abe443a2d77725fe683c7a1
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2013-10-24 09:56:50 +0300

    Api::V2::HostsController: return 406 if device type is not allowed

--- app/controllers/api/v2/hosts_controller.rb
+++ app/controllers/api/v2/hosts_controller.rb
@@ -3,6 +3,9 @@ class Api::V2::HostsController < ApplicationController
   class DeviceNotFound < StandardError
   end
 
+  class InvalidDeviceType < StandardError
+  end
+
   # POST /hosts/:hostname/sign_certificate
   def sign_certificate
 
@@ -19,6 +22,11 @@ class Api::V2::HostsController < ApplicationController
         raise DeviceNotFound
       end
 
+      # Allowe device type
+      unless allowed_device_type.include?(@host.puavoDeviceType)
+        raise InvalidDeviceType
+      end
+
       @host.revoke_certificate( session[:organisation].organisation_key,
                                 @authentication.dn,
                                 @authentication.password )
@@ -31,6 +39,9 @@ class Api::V2::HostsController < ApplicationController
 
     rescue DeviceNotFound
       render :status => 404, :json => { :error => "Device not found (#{ params[:hostname] })" }
+
+    rescue InvalidDeviceType
+      render :status => 406, :json => { :error => "Invalid device type (#{ @host.puavoDeviceType })" }
     end
   end
 end
