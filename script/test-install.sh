af8284435ace2a49aa6f343a172edd656437fffc
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-10-31 10:29:10 +0200

    add fails

--- script/test-install.sh
+++ script/test-install.sh
@@ -20,8 +20,8 @@ RAILS_ENV=production bundle exec rake db:migrate
 # Wait for puavo-web to start. It's sloooooow.
 sleep 10
 
-curl -v --noproxy "*"  http://localhost:9292/v3/about
+curl -v --fail --noproxy "*"  http://localhost:9292/v3/about
 echo "puavo-rest .deb package OK!"
 
-curl -v --noproxy "*"  http://localhost:8081/users/login > /dev/null
+curl -v --fail --noproxy "*"  http://localhost:8081/users/login > /dev/null
 echo "puavo-web .deb package OK!"
