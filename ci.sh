56f64f16aae6db7ff273e293e97d431dea1147ac
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-11-12 17:06:14 +0200

    Stop fluent in ci.sh

--- ci.sh
+++ ci.sh
@@ -11,6 +11,10 @@ sudo apt-get update
 
 sudo apt-get install -y --force-yes puavo-standalone ruby1.9.1 ruby1.9.1-dev libxml2-dev libxslt-dev libsqlite3-dev libmagickwand-dev ldap-utils libpq-dev libssl-dev build-essential libopenssl-ruby xpdf-utils git libreadline6-dev libxml2-dev libxslt1-dev libpq-dev libmagickwand-dev libsqlite3-dev ruby-bundler puavo-client nodejs-bundle puavo-ca-rails redis-server td-agent aptirepo-upload puavo-devscripts
 
+# fluentd does not work on standalone installations yet because it's not a
+# puavo managed installation. It also might cause log forwarding loops.
+sudo stop fluentd || true
+
 sudo puavo-init-standalone --unsafe-passwords opinsys.net
 
 sudo puavo-add-new-organisation --yes hogwarts --username albus --password albus --given-name Albus --surname Dumbledore
