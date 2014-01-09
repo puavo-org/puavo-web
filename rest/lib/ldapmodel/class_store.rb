e97cd015937cd4e32f6fe62e4ff56a3c9f65e615
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-01-09 15:14:56 +0200

    rest: Documen class_store

--- rest/lib/ldapmodel/class_store.rb
+++ rest/lib/ldapmodel/class_store.rb
@@ -5,6 +5,21 @@ class LdapModel
     @@_class_store[self] ||= {}
   end
 
+  # Like double at sign attributes (@@foo) but they are not shared between
+  # subclasses
+  #
+  # class Foo
+  #   class_store :bar
+  #   def get_baz
+  #     bar[:baz]
+  #   end
+  # end
+  #
+  # Foo.bar[:baz] = 1
+  # assert Foo.new.get_baz == 1
+  #
+  # @param [Symbol] accessor name
+  # @param [Block] default value creator
   def self.class_store(name, &create_default)
     define_method(name) do
       default = create_default.call if create_default
