adc0b5f7632ee54b4b282cfa41b1ad27a83f26df
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-01-14 14:50:14 +0200

    puavo-web: add path, mountpoint and options attribute to Mountpoint

--- app/models/mountpoint.rb
+++ app/models/mountpoint.rb
@@ -1,11 +1,32 @@
 module Mountpoint
-  attr_accessor :fs
+  attr_accessor :fs, :path, :mountpoint, :options
 
   def fs
     if @fs.nil?
       @fs = parse_mountpoint("fs")
     end
     @fs
+  end
+
+  def path
+    if @path.nil?
+      @path = parse_mountpoint("path")
+    end
+    @path
+  end
+
+  def mountpoint
+    if @mountpoint.nil?
+      @mountpoint = parse_mountpoint("mountpoint")
+    end
+    @mountpoint
+  end
+
+  def options
+    if @options.nil?
+      @options = parse_mountpoint("options")
+    end
+    @options
   end
 
   private
@@ -19,7 +40,11 @@ module Mountpoint
 
   def set_puavo_mountpoint
     self.fs.each_index do |index|
-      self.puavoMountpoint = Array(self.puavoMountpoint) + [{ "fs" => fs[index] }.to_json]
+      self.puavoMountpoint = Array(self.puavoMountpoint) +
+        [{ "fs" => fs[index],
+           "path" => path[index],
+           "mountpoint" => mountpoint[index],
+           "options" => options[index] }.to_json]
     end
   end
 end
--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -20,8 +20,14 @@ describe Device do
     d.puavoDeviceType = "fatclient"
     d.puavoSchool = @school.dn
     d.fs = ["nfs3"]
+    d.path = ["10.0.0.1/share"]
+    d.mountpoint = ["/home/share"]
+    d.options = []
     d.save!
 
     assert_equal "nfs3", Device.first.fs[0]
+    assert_equal "10.0.0.1/share", Device.first.path[0]
+    assert_equal "/home/share", Device.first.mountpoint[0]
+    assert_equal nil, Device.first.options[0]
   end
 end
