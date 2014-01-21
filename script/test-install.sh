c3ec0cee1b1efe7356ac15083be7ac94ab7ae9e9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-01-21 17:57:23 +0200

    fix typo

--- script/test-install.sh
+++ script/test-install.sh
@@ -5,7 +5,7 @@ set -x
 echo "hogwarts.opinsys.net" > /etc/puavo/domain
 
 # Generate "deployment" workers keys because they were removed during install
-make wworker-keys
+make worker-keys
 cp config/resque_worker_*_key /etc/puavo-web
 
 dpkg -i ../*deb
