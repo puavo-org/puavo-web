2bbc5c79c6cc71eb35ba3e2bffd2c098db97008f
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-01-23 14:05:42 +0200

    web: remove old and unused ActiveRecord models

--- app/models/group_user_ref.rb
+++
@@ -1,4 +0,0 @@
-class GroupUserRef < ActiveRecord::Base
-  belongs_to :group
-  belongs_to :user
-end
--- app/models/school_group_ref.rb
+++
@@ -1,4 +0,0 @@
-class SchoolGroupRef < ActiveRecord::Base
-  belongs_to :school
-  belongs_to :group
-end
