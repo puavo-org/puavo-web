ebac8d12da4706b842fbbd827d34b554d96c19e3
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-05-22 12:40:03 +0300

    Add german locale to Puavo

--- config/initializers/puavo.rb
+++ config/initializers/puavo.rb
@@ -1,6 +1,6 @@
 require 'puavo/authentication'
 
-Puavo.available_languages = ['fi', 'en', 'sv']
+Puavo.available_languages = ['fi', 'en', 'sv', 'de']
 
 Puavo::DEVICE_CONFIG = YAML.load_file("#{Rails.root}/config/puavo_devices.yml") rescue nil
 
