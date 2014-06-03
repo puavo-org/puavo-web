95499bcf9ef1255cad9e6360634f84c8630b6b6c
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-06-03 15:28:04 +0300

    Set preferred language only if locale is not empty

--- app/models/locale.rb
+++ app/models/locale.rb
@@ -1,7 +1,7 @@
 module Locale
 
   def set_preferred_language
-    if self.puavoLocale
+    if self.puavoLocale && !self.puavoLocale.empty?
       self.preferredLanguage = self.puavoLocale.match(/^[a-z]{2}/)[0]
     end
   end
