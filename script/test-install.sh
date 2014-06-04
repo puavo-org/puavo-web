bd5f10d3fde50e4c6d31d4941ad38c0d8fff0ec2
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-06-04 15:05:14 +0300

    Set timeout for curl test

--- script/test-install.sh
+++ script/test-install.sh
@@ -22,8 +22,8 @@ cd /var/app/puavo-web
 # Wait for puavo-web to start. It's sloooooow.
 sleep 10
 
-curl -v --fail --noproxy "*"  http://localhost:9292/v3/about
+curl -v --max-time 60 --fail --noproxy "*"  http://localhost:9292/v3/about
 echo "puavo-rest .deb package OK!"
 
-curl -v --fail --noproxy "*"  http://localhost:8081/users/login > /dev/null
+curl -v --max-time 60  --fail --noproxy "*"  http://localhost:8081/users/login > /dev/null
 echo "puavo-web .deb package OK!"
