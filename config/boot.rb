330c192de4e66242e5b106518f6ae80f5d7c81a0
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-30 14:57:17 +0000

    Use port 8081 in devmode too

--- config/boot.rb
+++ config/boot.rb
@@ -7,3 +7,14 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# http://stackoverflow.com/a/6539193/153718
+require 'rails/commands/server'
+module Rails
+  class Server
+    alias :default_options_alias :default_options
+    def default_options
+      default_options_alias.merge!(:Port => 8081)
+    end
+  end
+end
