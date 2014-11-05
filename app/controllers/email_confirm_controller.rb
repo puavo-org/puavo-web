c180c9245f5bd93ec3fad9976e17be23b537b99a
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-11-05 10:22:45 +0200

    web: add new email address for user

--- app/controllers/email_confirm_controller.rb
+++ app/controllers/email_confirm_controller.rb
@@ -13,12 +13,31 @@ class EmailConfirmController < ApplicationController
 
   # PUT /users/email_confirm
   def confirm
-    # jwt
-    # password
+    begin
 
+      jwt_data = JWT.decode(params[:jwt], Puavo::CONFIG["email_confirm_secret"])
 
-    respond_to do |format|
-      format.html { redirect_to( successfully_email_confirm_path ) }
+      perform_login( :uid => jwt_data["username"],
+                     :organisation_key => organisation_key_from_host(jwt_data["organisation_domain"]),
+                     :password => params[:email_confirm][:password] )
+
+      User.ldap_modify_operation( current_user.dn,
+                                  :add, [{ "mail" => [jwt_data["email"]] }] )
+
+      respond_to do |format|
+        format.html { redirect_to( successfully_email_confirm_path ) }
+      end
+
+    rescue Puavo::AuthenticationFailed
+      # invalid password, redirect?
+      # FIXME redirect to the main page. Show "Invalid password" message
+      render :error
+    rescue ActiveLdap::LdapError::TypeOrValueExists
+      # email address already exists
+      render :error
+    rescue JWT::DecodeError
+      # invalid jwt token
+      render :error
     end
   end
 
