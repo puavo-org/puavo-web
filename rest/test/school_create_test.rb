cbef15959d169b5ce6739dafe991b0751539af12
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-07-17 06:52:44 +0000

    rest: test school creation

--- rest/test/school_create_test.rb
+++ rest/test/school_create_test.rb
@@ -32,5 +32,23 @@ describe LdapModel do
       assert @school.dn, "model got dn"
     end
 
+    it "has Fixnum gid_number" do
+        assert_equal Fixnum, @school.gid_number.class
+    end
+
+    it "has name" do
+      assert_equal "Test School 1", @school.name
+    end
+
+    it "has internal samba attributes" do
+      assert_equal ["2"], @school.get_raw(:sambaGroupType)
+
+      samba_sid = @school.get_raw(:sambaSID)
+      assert samba_sid
+      assert samba_sid.first
+      assert_equal "S", samba_sid.first.first
+
+    end
+
   end
 end
