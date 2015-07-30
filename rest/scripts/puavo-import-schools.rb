d60cdeb2689177dbda3054d03f87556b975d7486
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-07-30 08:11:59 +0000

    rest: import script for schools

---
+++ rest/scripts/puavo-import-schools.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/ruby1.9.1
+
+require 'optparse'
+
+options = {}
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: puavo-import-schools [options]
+
+Import schools to Puavo
+
+"
+
+  opts.on("--organisation-domain DOMAIN", "Domain of organisation") do |o|
+    options[:organisation_domain] = o
+  end
+
+  opts.on("--csv-file FILE", "csv file with schools") do |o|
+    options[:csv_file] = o
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    STDERR.puts opts
+    Process.exit()
+  end
+end
+parser.parse!
+
+if options.keys.count != 2
+  STDERR.puts("Invalid arguments")
+  STDERR.puts(parser)
+  Process.exit(1)
+end
+
+require 'bundler/setup'
+require_relative "../puavo-rest"
+
+LdapModel.setup(
+  :credentials => CONFIG["server"]
+)
+
+LdapModel.setup(
+  :organisation => PuavoRest::Organisation.by_domain!(options[:organisation_domain])
+)
+
+PuavoRest::School.all.each do |school|
+  puts school.name
+end
+
+# 
