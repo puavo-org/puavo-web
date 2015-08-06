a02f37a73307730dfd88c2342d345bab756de7e0
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-08-06 11:23:46 +0000

    Test add_member method for Group

--- rest/test/groups_create_test.rb
+++ rest/test/groups_create_test.rb
@@ -36,4 +36,37 @@ describe LdapModel do
     end
 
   end
+
+  describe "group updating" do
+
+    before(:each) do
+      @school = School.create(
+        :cn => "gryffindor",
+        :displayName => "Gryffindor",
+        :puavoSchoolHomePageURL => "schoolhomepage.example"
+      )
+      @user = PuavoRest::User.new(
+        :first_name => "Heli",
+        :last_name => "Kopteri",
+        :username => "heli",
+        :roles => ["staff"],
+        :email => "heli.kopteri@example.com",
+        :school_dns => [@school.dn.to_s],
+        :password => "userpwlonger"
+      )
+      @user.save!
+
+    end
+
+    it "add new members" do
+
+      @group.add_member(@user)
+      @group.save!
+
+      reloaded_group = PuavoRest::Group.by_dn!(@group.dn)
+
+      assert_equal reloaded_group.member_usernames.include?(@user.username), true
+      assert_equal reloaded_group.member_dns.include?(@user.dn), true
+    end
+  end
 end
