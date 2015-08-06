426a8b133dc94714248933ff84b7cb67cb3dd8e1
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-08-06 10:51:26 +0000

    Run before hook for all describe

--- rest/test/groups_create_test.rb
+++ rest/test/groups_create_test.rb
@@ -3,33 +3,33 @@ require_relative "../lib/ldapmodel"
 
 describe LdapModel do
 
+  before(:each) do
+    Puavo::Test.clean_up_ldap
+
+    LdapModel.setup(
+      :organisation => PuavoRest::Organisation.default_organisation_domain!,
+      :rest_root => "http://" + CONFIG["default_organisation_domain"],
+      :credentials => {
+        :dn => PUAVO_ETC.ldap_dn,
+        :password => PUAVO_ETC.ldap_password }
+    )
+
+    @school = PuavoRest::School.new(
+      :name => "Test School 1",
+      :abbreviation => "testschool1"
+    )
+    @school.save!
+
+    @group = PuavoRest::Group.new(
+      :name => "Test group 1",
+      :abbreviation => "testgroup1",
+      :school_dn => @school.dn
+    )
+    @group.save!
+
+  end
+
   describe "group creation" do
-
-    before(:each) do
-      Puavo::Test.clean_up_ldap
-
-      LdapModel.setup(
-        :organisation => PuavoRest::Organisation.default_organisation_domain!,
-        :rest_root => "http://" + CONFIG["default_organisation_domain"],
-        :credentials => {
-          :dn => PUAVO_ETC.ldap_dn,
-          :password => PUAVO_ETC.ldap_password }
-      )
-
-      @school = PuavoRest::School.new(
-        :name => "Test School 1",
-        :abbreviation => "testschool1"
-      )
-      @school.save!
-
-      @group = PuavoRest::Group.new(
-        :name => "Test group 1",
-        :abbreviation => "testgroup1",
-        :school_dn => @school.dn
-      )
-      @group.save!
-
-    end
 
     it "has Fixnum id" do
       assert_equal Fixnum, @group.id.class
