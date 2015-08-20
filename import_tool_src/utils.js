00daa154fed386a6b3c9118fda3a6f3c3869e78d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-08-20 18:15:23 +0000

    import-tool: Fix getCellValue order

--- import_tool_src/utils.js
+++ import_tool_src/utils.js
@@ -4,7 +4,7 @@ import R from "ramda";
 const trimmedProp = R.compose(R.trim, String, R.or(R.__, ""), R.prop);
 
 export const getCellValue = R.compose(
-    R.either(trimmedProp("originalValue"), trimmedProp("customValue")),
+    R.either(trimmedProp("customValue"), trimmedProp("originalValue")),
     R.or(R.__, {})
 );
 
