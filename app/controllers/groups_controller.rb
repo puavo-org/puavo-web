e84b5ba5c91f2be775558d6cfe63365df90444b1
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-08-26 08:27:51 +0000

    web: users search on the group page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -148,4 +148,29 @@ class GroupsController < ApplicationController
       end
     end
   end
+
+  def user_search
+    @group = Group.find(params[:id])
+
+    @users = User.words_search_and_sort_by_name(
+      ["sn", "givenName", "uid"],
+      lambda{ |v| "#{v['sn'].first} #{v['givenName'].first}" },
+      lambda { |w| "(|(givenName=*#{w}*)(sn=*#{w}*)(uid=*#{w}*))" },
+      params[:words] )
+
+    @schools = Hash.new
+    School.search_as_utf8( :scope => :one,
+                   :attributes => ["puavoId", "displayName"] ).map do |dn, v|
+      @schools[v["puavoId"].first] = v["displayName"].first
+    end
+
+    respond_to do |format|
+      if @users.length == 0
+        format.html { render :inline => '' }
+      else
+        format.html { render :user_search, :layout => false }
+      end
+    end
+
+  end
 end
