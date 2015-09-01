32c6c8010e105b236d4465cfeb7e26f9c976ff00
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-09-01 13:09:04 +0000

    import-tool: Use webpack.ExtendedAPIPlugin
    To get __webpack_hash__ var

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
+/*eslint comma-dangle:0*/
+var webpack = require("webpack");
+
 module.exports = {
     entry: "./import-tool/index.js",
     output: {
         path: __dirname + "/public",
         filename: "import_tool.js"
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     module: {
         loaders: [
-            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}
+            {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
-    }
+    },
+    plugins: [
+        new webpack.ExtendedAPIPlugin()
+    ]
 };
