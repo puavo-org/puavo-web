014e5be7d5f2dd5c46c0ea828149c9628386bf77
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-09-03 09:30:13 +0000

    Download resource for List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,4 +10,80 @@ class ListsController < ApplicationController
     end
   end
 
+  # POST /users/:school_id/lists/:id
+  def download
+    @list = List.by_id(params[:id])
+
+    logger.debug @list.inspect
+
+    @users_by_group = {}
+
+    logger.debug  @list.users.inspect
+
+    @list.users.each do |user_id|
+      user = User.find(user_id)
+      logger.debug user.inspect
+      if params[:password_to_all]
+        user.new_password = params[:new_password]
+      else
+        user.set_generated_password
+      end
+
+      user.save!
+
+      # FIXME Use Group if new_group_management? is true
+      group = user.roles.first
+
+      @users_by_group[group.displayName] ||= []
+      @users_by_group[group.displayName].push(user)
+    end
+
+    pdf = Prawn::Document.new( :skip_page_creation => true, :page_size => 'A4')
+
+    pdf.start_new_page
+    pdf.font "Times-Roman"
+    pdf.font_size = 12
+    start_page_number = pdf.page_number
+
+    @users_by_group.each do |group_name, users|
+      # Sort users by sn + givenName
+      users = users.sort{|a,b| a.sn + a.givenName <=> b.sn + a.givenName }
+
+      pdf.text "\n"
+
+      users_of_page_count = 0
+      users.each do |user|
+        pdf.indent(300) do
+          pdf.text "#{t('activeldap.attributes.user.displayName')}: #{user.displayName}"
+          pdf.text "#{t('activeldap.attributes.user.uid')}: #{user.uid}"
+          pdf.text "#{t('activeldap.attributes.user.password')}: #{user.new_password}\n\n\n"
+
+          users_of_page_count += 1
+          if users_of_page_count > 10 && user != users.last
+            users_of_page_count = 0
+            pdf.start_new_page
+          end
+        end
+        pdf.repeat start_page_number..pdf.page_number do
+          pdf.draw_text "#{ current_organisation.name }, #{ @school.displayName }, #{ group_name }",
+          :at => pdf.bounds.top_left
+        end
+      end
+
+    end
+
+    filename = current_organisation.organisation_key + "_" +
+      @school.cn + "_" + Time.now.strftime("%Y%m%d") + ".pdf"
+
+    respond_to do |format|
+      format.pdf do
+        send_data(
+                  pdf.render,
+                  :filename => filename,
+                  :type => 'application/pdf',
+                  :disposition => 'inline' )
+      end
+
+    end
+  end
 end
