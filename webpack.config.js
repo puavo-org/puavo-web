95461b4eaa2f2e053e126fa4785bd8649226d3a8
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-09-15 13:36:15 +0000

    Use cheap-module-eval-source-map during development

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 /*eslint comma-dangle:0*/
 var webpack = require("webpack");
 
+var sourceMap = "cheap-module-eval-source-map";
+
+if (process.env.NODE_ENV === "production") {
+    sourceMap = "source-map";
+}
+
 module.exports = {
     entry: "./import-tool/index.js",
     output: {
         path: __dirname + "/public",
         filename: "import_tool.js"
     },
-    devtool: "source-map",
+    devtool: sourceMap,
     module: {
         loaders: [
             {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"}
