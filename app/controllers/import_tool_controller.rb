c5f01fef365e9a8a5290d987f007507e5253ba5c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-11-19 10:28:22 +0200

    import-tool: Do not use groups yet

--- app/controllers/import_tool_controller.rb
+++ app/controllers/import_tool_controller.rb
@@ -3,7 +3,7 @@ class ImportToolController < ApplicationController
     school = School.find(params["school_id"])
     @import_tool_options = {
       "containerId" => "import-tool", # the html element id
-      "useGroupsOnly" => true,
+      "useGroupsOnly" => false,
       "school" => {
         "dn" => school.dn.to_s,
         "id" => school.puavo_id
