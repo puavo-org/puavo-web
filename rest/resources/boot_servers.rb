70e23c25bc65e67fa75440a45f7d2c28b4190f7b
Tuomas Räsänen <tuomasjjrasanen@tjjr.fi>
2016-02-04 09:51:31 +0200

    rest: fix indentation

--- rest/resources/boot_servers.rb
+++ rest/resources/boot_servers.rb
@@ -69,11 +69,11 @@ class BootServer < LdapModel
   end
 
   def image_series_source_urls
-     if get_own(:image_series_source_urls).nil?
-       organisation.image_series_source_urls
-     else
-       get_own(:image_series_source_urls)
-     end
+    if get_own(:image_series_source_urls).nil?
+      organisation.image_series_source_urls
+    else
+      get_own(:image_series_source_urls)
+    end
   end
 
 end
