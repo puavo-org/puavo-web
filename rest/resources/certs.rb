e6baecb8470f69d7f5cfef672435fabffeec86c7
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2016-03-23 11:12:13 +0000

    rest: refactor

--- rest/resources/certs.rb
+++ rest/resources/certs.rb
@@ -10,11 +10,14 @@ module PuavoRest
       org = Host.organisation
       org_key = org.domain.split("." + PUAVO_ETC.topdomain).first
 
+      fqdn = host.hostname + "." + org.domain
+
       res = HTTP.basic_auth(:user => LdapModel.settings[:credentials][:dn],
                             :pass => LdapModel.settings[:credentials][:password])
         .post(CONFIG["puavo_ca"] + "/certificates.json",
-              :json => { "certificate" => {
-                  "fqdn" => host.hostname + "." + org.domain,
+              :json => {
+                "certificate" => {
+                  "fqdn" => fqdn,
                   "host_certificate_request" => json_params["certificate_request"] } } )
 
       if !res.code.to_s.match(/^2/)
