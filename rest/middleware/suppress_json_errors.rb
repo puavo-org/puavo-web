6201295dd60a418d3b48ab7fddd5808158da4a7f
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-09-06 14:27:17 +0300

    In SuppressJSONError suppress these errors so totally that do not separately log them.  This is because these are logged at root.rb anyway, through flog() which is how it is supposed to be.

--- rest/middleware/suppress_json_errors.rb
+++ rest/middleware/suppress_json_errors.rb
@@ -11,13 +11,6 @@ class SuppressJSONError
     begin
       @app.call(env)
     rescue JSONError => err
-      message = "ERROR: #{ err.message }"
-      if STDIN.tty?
-	STDERR.puts message.colorize(:red)
-      else
-	STDERR.puts message
-      end
-
       [err.http_code, err.headers, [err.to_json]]
     end
   end
