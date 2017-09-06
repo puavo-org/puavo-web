09efc13aa10a900199b8bd165a5c90fdb29d3df9
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-09-06 07:36:37 +0300

    Do not output backtrace for JSONError errors.  Most of these errors are normal, such as "Unauthorized" when trying to fetch puavo-session information with kerberos, and do not merit a backtrace filling up the logs.

--- rest/middleware/suppress_json_errors.rb
+++ rest/middleware/suppress_json_errors.rb
@@ -11,14 +11,11 @@ class SuppressJSONError
     begin
       @app.call(env)
     rescue JSONError => err
-      if ENV["RACK_ENV"] != "test"
-
-        message = "#{ err }\n#{ err.backtrace.join("\n") }"
-        if STDIN.tty?
-          STDERR.puts message.colorize(:red)
-        else
-          STDERR.puts message
-        end
+      message = "ERROR: #{ err.message }"
+      if STDIN.tty?
+	STDERR.puts message.colorize(:red)
+      else
+	STDERR.puts message
       end
 
       [err.http_code, err.headers, [err.to_json]]
