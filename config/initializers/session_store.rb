7966e77b7689fbebc56c7cc7553372813da05563
Jarmo Pietiläinen <jarmo@opinsys.fi>
2017-09-12 13:18:27 +0300

    Update some of the Rails' configuration files. These changes are fairly small and they've been in use for over 12 hours without problems.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-PuavoUsers::Application.config.session_store :cookie_store, key: '_puavo3_session'
+PuavoUsers::Application.config.session_store :cookie_store, key: '_puavo_users_session'
