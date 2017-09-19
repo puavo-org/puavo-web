29e83ba82b4e9cf72d0a3c9678419ed9eb124fd9
Jarmo Pietiläinen <jarmo@opinsys.fi>
2017-09-19 09:52:03 +0300

    More migration test updates. These have been in use for two days without problems.

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'puavo'
-require 'redcarpet/compat'
+#require 'redcarpet/compat'
 
-# http://stackoverflow.com/a/2212867/153718
-require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
-#require "active_resource/railtie"
 require "rails/test_unit/railtie"
 require "sprockets/railtie"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,8 @@ PuavoUsers::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-end
 
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+
+end
