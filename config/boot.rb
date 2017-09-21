1ef5e9db958c6860a4c4070ad8ac65575b89e790
Jarmo Pietiläinen <jarmo@opinsys.fi>
2017-09-21 13:00:40 +0300

    The Rails migration script wants to do this, seems to work fine

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
 # http://stackoverflow.com/a/6539193/153718
 require 'rails/commands/server'
