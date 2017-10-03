d3007a93d5d6b6f7c011fadf0ec2b96e332d8b4a
Jarmo Pietiläinen <jarmo@opinsys.fi>
2017-10-03 13:14:58 +0300

    Don't crash if the group name contains no number that could be incremented. This is potentially a very stupid fix as I don't know how group renaming actually works.

--- app/helpers/rename_groups_helper.rb
+++ app/helpers/rename_groups_helper.rb
@@ -2,6 +2,7 @@ module RenameGroupsHelper
 
   def increase_numeric_value_of_string(value)
     match_data = value.match(/\d+/)
+    return value + "1" if match_data.nil?
     number_length = match_data[0].length
     number = match_data[0].to_i + 1
     return value.sub(/\d+/, ("%0#{number_length}d" % number))
