a8380290d837e42395576d97b63e684dd5e56673
Jarmo Pietiläinen <jarmo@opinsys.fi>
2017-10-06 12:22:45 +0300

    Verify and update monkey patching.

--- monkeypatches.rb
+++ monkeypatches.rb
@@ -1,4 +1,4 @@
-puts "Monkey patching LDAP schema caches to activeldap. Expecting version 3.2.2"
+puts "Monkey patching LDAP schema caching. Assuming the activeldap gem version is 5.1.0."
 
 # Because we do set the ldap connection manually for each request to the
 # current users the defaul schema caching in ActiveLdap does not work because
@@ -12,7 +12,7 @@ puts "Monkey patching LDAP schema caches to activeldap. Expecting version 3.2.2"
 $active_ldap_schema_cache = {}
 module ActiveLdap
   class Schema
-    # https://github.com/activeldap/activeldap/blob/3.2.2/lib/active_ldap/schema.rb#L167-L170
+    # https://github.com/activeldap/activeldap/blob/master/lib/active_ldap/schema.rb#L168-L171
     private
     def cache(key)
       ($active_ldap_schema_cache[key] ||= [yield])[0]
