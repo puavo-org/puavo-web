24f06fa18eff4eeecdce6cf14af962d5c7f2829e
Jarmo Pietiläinen <jarmo@opinsys.fi>
2017-10-19 11:11:21 +0300

    Prevent school deletion if it still contains devices.
    Previously, if the school was deleted, its devices were left intact. This
    caused all sorts of problems later on.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -111,7 +111,9 @@ class SchoolsController < ApplicationController
     @school = School.find(params[:id])
 
     respond_to do |format|
-      if @school.members.count > 0 || @school.roles.count > 0 || @school.groups.count > 0
+      if @school.members.count > 0 || @school.roles.count > 0 || @school.groups.count > 0 ||
+        Device.find(:all, :attribute => "puavoSchool", :value => @school.dn).count > 0
+
         flash[:alert] = t('flash.school.destroyed_failed')
         format.html { redirect_to(school_path(@school)) }
         format.xml  { render :xml => @school.errors, :status => :unprocessable_entity }
