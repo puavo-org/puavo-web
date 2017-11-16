f588e48d1667bf2c07da4aca0d436b1d4a33776b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2017-11-16 07:54:04 +0200

    Add a new rest-interface, "/v3/devices/:hostname/wlan_networks_with_certs", that will include all types of wlan networks and not only "open" and "psk".  We have a new wlan network type coming, "eap-tls", and netboot devices should not need to know it for any purpose, thus we'll try to keep it more secret and device credentials are needed to fetch the certificates needed by such networks.

--- rest/resources/wlan_networks.rb
+++ rest/resources/wlan_networks.rb
@@ -6,17 +6,35 @@ class WlanNetworks < PuavoSinatra
   def networks
     device = Device.by_hostname(params["hostname"])
 
+    if device.nil? then
+      status 404
+      return json({ :status => "failed",
+                    :error  => "Cannot find device by hostname" })
+    end
+
     org_networks = Array(device.organisation["wlan_networks"])
     school_networks = Array(device.school["wlan_networks"])
 
-    school_networks_ssids = school_networks.map{ |w| w["ssid"] }
-    org_networks.delete_if{ |w| school_networks_ssids.include?(w["ssid"]) }
+    school_networks_ssids = school_networks.map { |w| w["ssid"] }
+    org_networks.delete_if { |w| school_networks_ssids.include?(w["ssid"]) }
 
     return org_networks + school_networks
   end
 
   get "/v3/devices/:hostname/wlan_networks" do
     auth :basic_auth, :server_auth, :legacy_server_auth
+
+    only_open_and_psk_networks \
+      = networks.select { |s| %w(open psk).include?(s['type']) }
+
+    json only_open_and_psk_networks
+  end
+
+  get "/v3/devices/:hostname/wlan_networks_with_certs" do
+    # Networks might include "eap-tls" and other networks that require
+    # certificates, so keep this information more secret than other network
+    # secrets.  Netboot devices should have no need for this information.
+    auth :basic_auth
 
     json networks
   end
