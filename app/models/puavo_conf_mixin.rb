386cb0cfbfe60c06771bfad0a1f8a46ceefb6e1c
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2018-01-26 14:28:01 +0200

    Use the dual "puavoConfString"- + "puavoconf"-methods were not a success, because changing the database state did not work through that (or at least I could not get that working).  Use a simpler model, hopefully.  Adapt in views and use partials to render puavo-conf settings.

--- app/models/puavo_conf_mixin.rb
+++ app/models/puavo_conf_mixin.rb
@@ -1,28 +1,8 @@
 module PuavoConfMixin
-  def puavoConfString
-    Array(get_attribute('puavoConf')).first
-  end
-
-  def puavoConfString=(puavoconf_string)
+  def puavoConf=(puavoconf_string)
     puavoconf_data = parse_as_json(puavoconf_string)
     return if puavoconf_data.nil?
 
-    puavoConf = puavoconf_data
-  end
-
-  def puavoConf
-    puavoconf_data = parse_as_json(puavoConfString)
-    return {} if puavoconf_data.nil?
-
-    if !validate_puavoconf_data(puavoconf_data) then
-      puavoconf_data = {}
-      logger.warn 'database contains invalid puavoconf-data'
-    end
-
-    puavoconf_data
-  end
-
-  def puavoConf=(puavoconf_data)
     if !validate_puavoconf_data(puavoconf_data) then
       logger.warn 'not storing invalid puavoconf-data'
       return
@@ -42,10 +22,10 @@ module PuavoConfMixin
     data
   end
 
-  def validate_puavoconf_data(puavoconf)
-    return false unless puavoconf.kind_of?(Hash)
+  def validate_puavoconf_data(puavoconf_data)
+    return false unless puavoconf_data.kind_of?(Hash)
 
-    puavoconf.all? do |k,v|
+    puavoconf_data.all? do |k,v|
       k.kind_of?(String) \
         && (v.kind_of?(String) || v.kind_of?(Integer) \
               || v == false || v == true)
