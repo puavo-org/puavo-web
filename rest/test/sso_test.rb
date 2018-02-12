8eef09bb748eaa88c181e0795a75e707225c5567
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-02-12 13:57:11 +0200

    Remove an obsolete test

--- rest/test/sso_test.rb
+++ rest/test/sso_test.rb
@@ -299,15 +299,6 @@ describe PuavoRest::SSO do
         el.attributes["value"].value if el
       end
 
-      it "is added from hostname" do
-        get "/v3/sso", {
-          "return_to" => "http://test-client-service.example.com/path",
-        }, {
-            "HTTP_HOST" => "anotherorg.opinsys.net"
-        }
-        assert_equal "anotherorg.opinsys.net", hidden_organisation_field
-      end
-
       it "is overridden from query string" do
         get "/v3/sso", {
           "organisation" => "anotherorg.opinsys.net",
