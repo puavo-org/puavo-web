6f1a286eab0580aaf92b48f474c0dd7c5768de9c
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-08-14 12:37:24 +0300

    Add checkboxes to role/group mass rename page to select which types (roles/groups) the user wants to rename.
    This commit also thoroughly revamps the page layout with fieldsets
    and reindents it nicely.

--- app/controllers/rename_groups_controller.rb
+++ app/controllers/rename_groups_controller.rb
@@ -4,11 +4,17 @@ class RenameGroupsController < ApplicationController
 
   def new
     begin
+      @do_only_groups = new_group_management?(@school)
+
       # Detect class range
-      @roles = @school.roles.select do |r|
-        r.displayName.match(/\d+/) && !r.displayName.match(/poistuvat/)
-      end.sort do |a,b|
-        a.displayName <=> b.displayName
+      @roles = []
+
+      if !@do_only_groups
+        @roles = @school.roles.select do |r|
+          r.displayName.match(/\d+/) && !r.displayName.match(/poistuvat/)
+        end.sort do |a,b|
+          a.displayName <=> b.displayName
+        end
       end
 
       unless @roles.empty?
@@ -53,37 +59,60 @@ class RenameGroupsController < ApplicationController
   end
 
   def create
-    params[:role_display_name]&.each_index do |index|
-      unless params[:role_display_name][index].empty?
-        role = Role.find(params[:role_puavo_id][index])
-        role.displayName = params[:role_display_name][index]
-        role.save!
+    @do_only_groups = new_group_management?(@school)
+
+    if !@do_only_groups && !params[:do_roles] && !params[:do_groups]
+      flash[:notice] = "Et valinnut rooleja ja/tai ryhmiä mitä käsitellä"
+      redirect_to new_rename_groups_path(@school)
+      return
+    end
+
+    num_roles_renamed = 0
+    num_groups_renamed = 0
+
+    if params[:do_roles] && !@do_only_groups
+      params[:role_display_name]&.each_index do |index|
+        unless params[:role_display_name][index].empty?
+          role = Role.find(params[:role_puavo_id][index])
+          role.displayName = params[:role_display_name][index]
+          role.save!
+          num_roles_renamed += 1
+        end
+      end
+
+      params[:new_roles]&.each do |role_name|
+        unless role_name.empty?
+          r = Role.new(:displayName => role_name,
+                       :puavoSchool => @school.dn )
+          r.save!
+        end
       end
     end
 
-    params[:new_roles]&.each do |role_name|
-      unless role_name.empty?
-        r = Role.new(:displayName => role_name,
-                     :puavoSchool => @school.dn )
-        r.save!
+    if params[:do_groups] || @do_only_groups
+      params[:group_display_name]&.each_index do |index|
+        unless params[:group_display_name][index].empty?
+          group = Group.find(params[:group_puavo_id][index])
+          group.displayName = params[:group_display_name][index]
+          group.save!
+          num_groups_renamed += 1
+        end
+      end
+
+      params[:new_groups_cn]&.each_index do |index|
+        if !params[:new_groups_display_name][index].empty? && !params[:new_groups_cn][index].empty?
+          g = Group.new(:displayName => params[:new_groups_display_name][index],
+                        :cn => params[:new_groups_cn][index],
+                        :puavoSchool => @school.dn )
+          g.save!
+        end
       end
     end
 
-    params[:group_display_name]&.each_index do |index|
-      unless params[:group_display_name][index].empty?
-        group = Group.find(params[:group_puavo_id][index])
-        group.displayName = params[:group_display_name][index]
-        group.save!
-      end
-    end
-
-    params[:new_groups_cn]&.each_index do |index|
-      if !params[:new_groups_display_name][index].empty? && !params[:new_groups_cn][index].empty?
-        g = Group.new(:displayName => params[:new_groups_display_name][index],
-                      :cn => params[:new_groups_cn][index],
-                      :puavoSchool => @school.dn )
-        g.save!
-      end
+    if @do_only_groups
+      flash[:notice] = "Nimetty #{num_groups_renamed} ryhmä(ä) uudelleen."
+    else
+      flash[:notice] = "Nimetty #{num_roles_renamed} rooli(a) ja #{num_groups_renamed} ryhmä(ä) uudelleen."
     end
 
     respond_to do |format|
