d461e6a40dc1e9e2980a340a3b458429446cddc4
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-10-30 10:53:30 +0200

    Add a new group type (course groups) for Primus imports All tests pass.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -98,7 +98,7 @@ module ApplicationHelper
 
   def group_types_for_select
     [[t("default_select_value"), ""]] +
-    (['teaching group', 'year class', 'administrative group', 'archive users', 'other groups']).map do |type|
+    (['teaching group', 'course group', 'year class', 'administrative group', 'archive users', 'other groups']).map do |type|
       [humanize_group_type(type), type]
     end
   end
