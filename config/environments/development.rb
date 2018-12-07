ab1cec2cb2e8436843510cf00ba2aef3a5733ef6
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-12-07 13:57:17 +0200

    Reduce the size of the generated CSS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Disable useless and excessive debug commentary in stylesheets
+  config.sass.line_comments = false
 end
