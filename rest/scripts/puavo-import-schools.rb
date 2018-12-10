4079204b511c24de10ae5ab5554d0d3e4f93809b
Jarmo Pietiläinen <jarmo@opinsys.fi>
2018-12-10 12:47:45 +0200

    Create the two required administrative groups automatically

--- rest/scripts/puavo-import-schools.rb
+++ rest/scripts/puavo-import-schools.rb
@@ -43,6 +43,37 @@ mode = options[:mode] || "default"
 
 schools = PuavoImport::School.all
 
+def ensure_administrative_group_exists(abbreviation, name,  school)
+  group = PuavoRest::Group.by_attr(:abbreviation, abbreviation)
+
+  if group
+    # The group's name can be changed freely so we ignore it, but make sure
+    # the type is correct.
+    if group.type != "administrative group"
+      puts brown("Updating the type of the group \"#{abbreviation}\"")
+
+      begin
+        group.type = "administrative group"
+        group.save!
+      rescue StandardError => e
+        puts red("ERROR: Could not update the group: #{e}")
+      end
+    end
+  else
+    # Create a new group
+    puts green("Creating new group \"#{name}\" (abbreviation \"#{abbreviation}\")")
+
+    begin
+      PuavoRest::Group.new(:name => name,
+                           :abbreviation => abbreviation,
+                           :type => "administrative group",
+                           :school_dn => school.dn).save!
+    rescue StandardError => e
+      puts red("ERROR: Could not create a new group: #{e}")
+    end
+  end
+end
+
 case mode
 when "diff"
   puts "Compare current data for import data\n\n"
@@ -57,6 +88,7 @@ when "diff"
 
     unless puavo_rest_school
       puts green("Add new school: #{ school.to_s }")
+      puts green("  Add the 'teachers' and 'staff' administrative groups")
       next
     end
 
@@ -109,6 +141,7 @@ when "import"
     end
 
     puavo_rest_school = PuavoRest::School.by_attr(:external_id, school.external_id)
+
     if puavo_rest_school
       if school.need_update?(puavo_rest_school)
         puts "#{ school.to_s }: update school information"
@@ -121,10 +154,20 @@ when "import"
       end
     else
       puts "#{ school.to_s }: add school to Puavo"
-      PuavoRest::School.new(:name => school.name,
+      puavo_rest_school = PuavoRest::School.new(:name => school.name,
                             :external_id => school.external_id,
-                            :abbreviation => school.abbreviation).save!
+                                                :abbreviation => school.abbreviation)
+      puavo_rest_school.save!
     end
+
+    # Create/update the two required administrative groups
+    ensure_administrative_group_exists("#{school.abbreviation}-henkilokunta",
+                                       'Henkilökunta',
+                                       puavo_rest_school)
+
+    ensure_administrative_group_exists("#{school.abbreviation}-opettajat",
+                                       'Opettajat',
+                                       puavo_rest_school)
   end
 end
 
