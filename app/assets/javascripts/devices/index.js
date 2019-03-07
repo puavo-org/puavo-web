6c6b36480e62de3a5adad511d73e6b2b04600b71
Jarmo Pietiläinen <jarmo@opinsys.fi>
2019-03-07 15:23:22 +0200

    Add a comment, so I won't accidentally delete this file.

--- app/assets/javascripts/devices/index.js
+++ app/assets/javascripts/devices/index.js
@@ -1,8 +1,12 @@
-$(document).ready(function() {
+/* Used on devices index page to hide the form submit button
+   and make the the combo box submit instantly when something
+   is selected. */
+
+$(document).ready(function(){
     $('#device_submit').hide();
-    
+
     $('#device_type').change( function() {
-	$('#new_device').submit();
+        $('#new_device').submit();
     });
 
     $('#device_type').val('');
