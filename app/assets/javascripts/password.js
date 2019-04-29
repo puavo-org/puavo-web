0c8c02724ecd9c777b073eed371d657aa5978c7f
Jarmo Pietiläinen <jarmo@opinsys.fi>
2019-04-29 11:38:13 +0300

    This file appears to be unused.

--- app/assets/javascripts/password.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	if($("p.message_success").length > 0 || $("p.message_notice").length > 0){
-		var notice = $("p[class^='message_']");
-		notice.delay(3000).slideUp(500).fadeOut(200, function() {
-			notice.remove();
-		});		
-	}
-});
