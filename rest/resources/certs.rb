d131ae894e825c33c1e3e6b496e6e55b400000d8
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-09-13 10:44:25 +0300

    Unify /v3/hosts/certs/sign_with_version and /v3/hosts/certs/sign so we have only one API to maintain.  Now /v3/hosts/certs/sign can accept certificate chain version, but also functions without it.

--- rest/resources/certs.rb
+++ rest/resources/certs.rb
@@ -3,58 +3,13 @@ require 'http'
 module PuavoRest
 
   class Certs < PuavoSinatra
-
-    # Deprecated, but should be supported for a long time
-    # for all Ubuntu Trusty / Debian Stretch versions of puavo-os.
-    post "/v3/hosts/certs/sign" do
-      auth :basic_auth
-
-      host = Host.by_dn(User.current.dn)
-      unless host then
-        status 404
-        return json({ 'error' => 'could not find the connecting host' })
-      end
-
-      org = Host.organisation
-      org_key = org.domain.split("." + PUAVO_ETC.topdomain).first
-
-      fqdn = host.hostname + "." + org.domain
-
-      res = HTTP.basic_auth(:user => LdapModel.settings[:credentials][:dn],
-                            :pass => LdapModel.settings[:credentials][:password])
-        .delete(CONFIG["puavo_ca"] + "/certificates/revoke.json",
-                :json => { "fqdn" => fqdn } )
-
-      if res.code != 200 && res.code != 404
-        raise InternalError, "Unable to revoke certificate"
-      end
-
-      res = HTTP.basic_auth(:user => LdapModel.settings[:credentials][:dn],
-                            :pass => LdapModel.settings[:credentials][:password])
-        .post(CONFIG["puavo_ca"] + "/certificates.json",
-              :json => {
-                "org" => org_key,
-                "certificate" => {
-                  "fqdn" => fqdn,
-                  "host_certificate_request" => json_params["certificate_request"] } } )
-
-      if !res.code.to_s.match(/^2/)
-        raise InternalError, "Unable to sign certificate"
-      end
-
-      json res.parse["certificate"]
-    end
-
-    post '/v3/hosts/certs/sign_with_version' do
+    post '/v3/hosts/certs/sign' do
       auth :basic_auth
 
       certificate_request = json_params['certificate_request']
       halt(400, json(:error => 'no certificate request parameter')) \
         unless certificate_request.kind_of?(String) \
                  && !certificate_request.empty?
-      version = json_params['version']
-      halt(400, json(:error => 'no version parameter')) \
-        unless version.kind_of?(String) && !version.empty?
 
       host = Host.by_dn( LdapModel.settings[:credentials][:dn] )
       unless host then
@@ -66,33 +21,42 @@ module PuavoRest
       org_key = org.domain.gsub(".#{ PUAVO_ETC.topdomain }", '')
 
       fqdn = "#{ host.hostname }.#{ org.domain }"
+      revoke_params = { 'fqdn' => fqdn }
+      revoke_params[:version] = json_params[:version] \
+        unless json_params[:version].nil?
 
       res = puavo_ca_request.delete(
               "#{ CONFIG['puavo_ca'] }/certificates/revoke.json",
-              :json => { 'fqdn' => fqdn, 'version' => version })
-      raise InternalError, 'Unable to revoke certificate' \
-        unless res.code == 200 || res.code == 404
+              :json => revoke_params)
+      unless res.code == 200 || res.code == 404 then
+        # XXX we should get a proper (detailed) error message to log
+        raise InternalError, "Unable to revoke certificate: #{ res.parse }"
+      end
+
+      certificate_params = {
+        :fqdn                     => fqdn,
+        :host_certificate_request => certificate_request,
+        :organisation             => org_key,
+      }
+      certificate_params[:version] \
+        = json_params[:version] unless json_params[:version].nil?
 
       res = puavo_ca_request.post("#{ CONFIG['puavo_ca'] }/certificates.json",
-              :json => {
-                'org' => org_key,
-                'certificate' => {
-                  'fqdn'                     => fqdn,
-                  'host_certificate_request' => certificate_request,
-                  'version'                  => version }})
+              :json => { 'certificate' => certificate_params })
 
-      raise InternalError, "Unable to sign certificate" \
+      # XXX we should get a proper (detailed) error message to log
+      raise InternalError, 'Unable to sign certificate' \
         unless res.code.to_s.match(/^2/)
 
       parsed_response = res.parse
       raise 'no certificate structure returned by puavo-ca' \
         unless parsed_response.kind_of?(Hash)
       raise 'no host certificate returned by puavo-ca' \
-        unless parsed_response['host_certificate'].kind_of?(Hash)
+        unless parsed_response['certificate'].kind_of?(String)
       raise 'no organisation ca certificate bundle returned by puavo-ca' \
-        unless parsed_response['org_ca_certificate_bundle'].kind_of?(Hash)
+        unless parsed_response['org_ca_certificate_bundle'].kind_of?(String)
       raise 'no root ca certificate returned by puavo-ca' \
-        unless parsed_response['root_ca_certificate'].kind_of?(Hash)
+        unless parsed_response['root_ca_certificate'].kind_of?(String)
 
       return json(parsed_response)
     end
