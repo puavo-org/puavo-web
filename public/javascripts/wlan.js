b6bd617b82d7ed27f120cc811ff8589e456dbb3a
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-12-31 14:21:28 +0200

    Support wlan networks of type "EAP-TTLS".
    Also make certificates optional in "EAP-*" type of networks.
    Tests need to be updated.

--- public/javascripts/wlan.js
+++ public/javascripts/wlan.js
@@ -2,15 +2,19 @@ $(document).ready(function() {
   function update_wlan_fields_visibility(index, select_wlan_type) {
     var wlan_type = select_wlan_type.val();
 
-    var tr_wlan_ap       = $('tr.wlan_ap_' + index);
-    var tr_wlan_certs    = $('tr.wlan_certs_' + index);
-    var tr_wlan_identity = $('tr.wlan_identity_' + index);
-    var tr_wlan_password = $('tr.wlan_password_' + index);
+    var tr_wlan_ap             = $('tr.wlan_ap_' + index);
+    var tr_wlan_certs          = $('tr.wlan_certs_' + index);
+    var tr_wlan_identity       = $('tr.wlan_identity_' + index);
+    var tr_wlan_password       = $('tr.wlan_password_' + index);
+    var tr_wlan_phase2_autheap = $('tr.wlan_phase2_autheap_' + index);
 
-    var show_wlan_ap       = (wlan_type !== 'eap-tls');
-    var show_wlan_certs    = (wlan_type === 'eap-tls');
-    var show_wlan_identity = (wlan_type === 'eap-tls');
-    var show_wlan_password = (wlan_type === 'psk');
+    var show_wlan_ap       = [ 'open', 'psk' ].includes(wlan_type);
+    var show_wlan_certs    = [ 'eap-tls', 'eap-ttls' ].includes(wlan_type);
+    var show_wlan_identity = [ 'eap-tls', 'eap-ttls' ].includes(wlan_type);
+    var show_wlan_password = [ 'eap-tls', 'eap-ttls', 'psk' ]
+		               .includes(wlan_type);
+    var show_wlan_phase2_autheap = [ 'eap-tls', 'eap-ttls' ]
+		                     .includes(wlan_type);
 
     for (var i = 0; i < tr_wlan_ap.length; i++) {
       tr_wlan_ap[i].style.display = show_wlan_ap ? 'table-row' : 'none';
@@ -28,6 +32,11 @@ $(document).ready(function() {
     for (var i = 0; i < tr_wlan_password.length; i++) {
       tr_wlan_password[i].style.display
         = show_wlan_password ? 'table-row' : 'none';
+    }
+
+    for (var i = 0; i < tr_wlan_password.length; i++) {
+      tr_wlan_phase2_autheap[i].style.display
+        = show_wlan_phase2_autheap ? 'table-row' : 'none';
     }
   }
 
