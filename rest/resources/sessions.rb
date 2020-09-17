682dcff43adcfc60bb4623272ef2d8328e503b01
Jarmo Pietiläinen <jarmo@opinsys.fi>
2020-09-17 11:33:53 +0300

    Do not call User.to_hash when generating sessions
    It wastes memory and CPU time and poses a (small) privacy problem,
    because your profile JSON will, for example, list all the groups and
    schools you're in, with their username lists. You don't need that
    information.
    
    This commit has a potential to wreak havoc on epic scale. The data
    returned from a "POST /v3/sessions" call is used in, among other
    places, when logging in (by the puavo-login script). If the data
    isn't correct, you won't log in to your desktop. I've verified every
    piece of data, but it's possible (read: very likely) that something
    has slipped through and is now missing.
    
    We'll see how long it takes for this commit to be reverted.

--- rest/resources/sessions.rb
+++ rest/resources/sessions.rb
@@ -175,24 +175,70 @@ class Sessions < PuavoSinatra
 
     if User.current && !User.current.server_user?
       user = User.current
-      session["user"] = user.to_hash
-      groups = user.groups
-      session["user"]["groups"] = groups
-      session["user"]["groups"] += user.schools.map do |s|
-        {
-          "id" => s.id,
-          "name" => s.name,
-          "dn" => s.dn,
-          "gid_number" => s.gid_number,
-          "abbreviation" => s.abbreviation,
-          "object_model" => s.object_model
+
+      session['user'] = {
+        # Generic useful information (mostly for puavo-login)
+        'username' => user.username,
+        'first_name' => user.first_name,
+        'last_name' => user.last_name,
+        'user_type' => user.user_type,
+        'gid_number' => user.gid_number,
+        'uid_number' => user.uid_number,
+
+        # check-if-account-is-locked
+        'locked' => user.locked,
+
+        # 41puavo-set-locale
+        'locale' => user.locale,
+        'preferred_language' => user.preferred_language,
+
+        # 42puavo-set-browser-homepage
+        'homepage' => user.homepage,
+
+        # puavo-handle-user-password-expiration
+        'password_last_set' => user.password_last_set,
+
+        # various bits and pieces of info that isn't currently required,
+        # but that can be useful
+        'id' => user.id.to_i,
+        'profile_image_link' => user.profile_image_link,
+        'edu_person_principal_name' => user.edu_person_principal_name,
+        'primary_school_id' => user.school.id.to_i,
+      }
+
+      # puavo-login needs groups and their GID numbers. Convert schools into groups,
+      # because from our system's POV a school *is* a group.
+      raw_groups = user.schools
+
+      user.list_supplementary_schools.each do |school_id, _|
+        s = School.by_id(school_id)
+        raw_groups << s if s
+      end
+
+      raw_groups += user.groups
+
+      groups = []
+
+      # This works because schools and groups have the same members. And Ruby does not care
+      # if an array contains different types of objects.
+      raw_groups.each do |group|
+        groups << {
+          'id' => group.id.to_i,                            # currently unused
+          'name' => group.name,                             # currently unused
+          'abbreviation' => group.abbreviation,
+          'gid_number' => group.gid_number,
+          'printer_queue_dns' => group.printer_queue_dns,   # currently unused?
         }
       end
 
+      session['user']['groups'] = groups
+
+      # Needed by 41puavo-set-locale
       session["preferred_language"] = user.preferred_language
       session["locale"] = user.locale
 
-      groups.each do |group|
+      # Needed by 90puavo-setup-printers
+      user.groups.each do |group|
         session["printer_queues"] += group.printer_queues
       end
     end
