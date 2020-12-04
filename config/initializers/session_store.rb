d3b15b27601989783deb9df10dd1668db8bdea46
Jarmo Pietiläinen <jarmo@opinsys.fi>
2020-12-04 12:40:17 +0200

    Enable secure SameSite cookies in development and production
    Every single test will fail with this because the test system
    cannot login in. I don't know why, but it only affects tests
    so it's easy to fix.
    
    Whether this change actually fixes the core issue, I don't know.
    But the warnings in browsers go away and logins still work,
    so maybe it's good?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_puavo_users_session'
+if ENV['RAILS_ENV'] == 'test'
+  # Only works in the test environment
+  Rails.application.config.session_store :cookie_store, key: '_puavo_users_session'
+else
+  # This works in development and production
+  Rails.application.config.session_store :cookie_store, {
+    :key => '_puavo_users_session',
+    :same_site => :strict,
+    :secure => :true,
+  }
+end
