73e140f15a6d812582d7a84ea24bd5898ed25c3e
Jarmo Pietiläinen <jarmo@opinsys.fi>
2021-02-26 10:24:38 +0200

    Change how strict same-site cookies are enabled
    "Production mode" really requires HTTPS. And puavo-standalone does
    not have HTTPS. This only affects the actual production servers.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,16 +1,17 @@
 # Be sure to restart your server when you modify this file.
 
-if ENV['RAILS_ENV'] == 'test' || ENV['RAILS_ENV'] == 'development'
-  # Testing and development environments aren't exactly secure, but they don't have to be
-  Rails.application.config.session_store :cookie_store, {
-    :key => '_puavo_users_session',
-    :same_site => :lax,
-  }
-else
-  # Production only, with SSL and everything
+if ENV.include?('STRICT_SAMESITE_COOKIES')
+  # Actual production mode, with HTTPS and everything
   Rails.application.config.session_store :cookie_store, {
     :key => '_puavo_users_session',
     :same_site => :strict,
     :secure => :true,
+  }
+else
+  # Testing and development environments aren't exactly secure, but they don't have to be.
+  # This is also used on puavo-standalone.
+  Rails.application.config.session_store :cookie_store, {
+    :key => '_puavo_users_session',
+    :same_site => :lax,
   }
 end
