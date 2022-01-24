f9c6a3f0e545d93fa948bf88310e5917a2aca385
Jarmo Pietiläinen <jarmo@opinsys.fi>
2022-01-24 13:57:15 +0200

    A JavaScript version of the password validator, for edit forms
    Performs live-validation of entered passwords and tells the user
    why the new password won't be accepted.
    
    The password "instructions" are also now part of the rulesets, so
    they can be removed form the public repository.

--- app/helpers/password_helper.rb
+++ app/helpers/password_helper.rb
@@ -1,87 +1,33 @@
 module PasswordHelper
   # Renders the Javascript template at the bottom of the page that sets up
   # password field validation.
-  def setup_password_validator(organisation_name, school_id,
-                               password_id='user_new_password',
-                               confirm_id='user_new_password_confirmation')
+  def setup_password_validator(organisation_name, school_id, password_field_id, confirm_field_id, callback=nil)
+    ruleset_name = get_school_password_requirements(organisation_name, school_id)
 
-    requirements = get_school_password_requirements(organisation_name, school_id)
+    logger.info("setup_password_validator(): password validation ruleset for school #{school_id} " \
+                "in organisation \"#{organisation_name}\" is \"#{ruleset_name}\"")
 
-    logger.info("setup_password_validator(): requirements for school #{school_id} " \
-                "in organisation \"#{organisation_name}\" are \"#{requirements}\"")
+    return unless ruleset_name
 
-    return unless requirements
+    rules = Puavo::PASSWORD_RULESETS[ruleset_name][:rules]
 
-    # defaults
-    template = 'password/password_length_only'
-
-    locals = {
-      password_id: password_id,
-      confirm_id: confirm_id,
-
-      # Translate strings and pass them to the Javascript code. ERB templates
-      # can be used with Javascript, but it's hairy, apparently uses different
-      # rules and is primarily meant for AJAX calls. Also some of these strings
-      # are dynamic. I wasted four hours trying to come up with a better
-      # solution, but none of them worked. :-( I hate this kind of code.
-      strings: {
-        ok: t('password.validator_ok'),
-        ascii_only: t('password.validator_ascii_only'),
-        no_whitespace: t('password.validator_no_whitespace'),
-        too_short: t('password.validator_too_short'),
-        too_short_with_count: t('password.validator_too_short_with_count'),
-        passwords_match: t('password.validator_passwords_match'),
-        passwords_dont_match: t('password.validator_passwords_dont_match'),
-      }
+    render partial: 'password/password_validator', locals: {
+      password_field_id: password_field_id,
+      confirm_field_id: confirm_field_id,
+      rules: rules,
+      callback: callback,
     }
-
-    case requirements
-      when 'Google'
-        template = 'password/password_gsuite'
-
-      # TODO: Create a more flexible system for specifying password requirements
-      when 'oulu_ad'
-        locals[:min_length] = 8
-        template = 'password/password_oulu_ad'
-
-      when 'SixCharsMin'
-        locals[:min_length] = 6
-
-      when 'SevenCharsMin'
-        locals[:min_length] = 7
-    else
-      logger.warn "setup_password_validator(): unknown requirements \"#{requirements}\", validator template not rendered"
-      return
-    end
-
-    render partial: template, locals: locals
   end
 
   # Creates the small help text that explains the password requirements for this organisation/school
   def show_password_requirements(organisation_name, school_id)
-    requirements = get_school_password_requirements(organisation_name, school_id)
-    return unless requirements
+    ruleset_name = get_school_password_requirements(organisation_name, school_id)
+    return unless ruleset_name
 
-    msg = ''
+    descriptions = Puavo::PASSWORD_RULESETS[ruleset_name][:descriptions]
+    lang = I18n.locale.to_s
+    return unless descriptions.include?(lang)
 
-    case requirements
-      when 'Google'
-        msg = t('password.gsuite_integration_enabled')
-
-      when 'oulu_ad'
-        # this... this is not the proper way of doing it
-        msg = t('password.oulu_ad_integration_enabled')
-
-      when 'SixCharsMin'
-        msg = t('password.six_chars_min')
-
-      when 'SevenCharsMin'
-        msg = t('password.seven_chars_min')
-
-    else
-      return
-    end
-
-    "<p class=\"passwordNotice\">#{msg}</p>".html_safe
+    "<p class=\"passwordNotice\">#{descriptions[lang]}</p>".html_safe
   end
 end
--- public/javascripts/password_validator.js
+++ public/javascripts/password_validator.js
@@ -1,95 +1,171 @@
 // =============================================================================
-// Extremely ugly password field validator
-
+// Extremely ugly password field validator, v2.0
 // Does not prevent the form from being submitted, but it lets the
 // user know that the password will be rejected if they submit it
 // =============================================================================
 
-// Return one these from the validator function
-const PASSWORD_OK = 0,
-      PASSWORD_ASCII_ONLY = 1,      // for GSuite at the moment
-      PASSWORD_NO_WHITESPACE = 2,   // for GSuite at the moment
-      PASSWORD_TOO_SHORT = 3;
-
-var passwordField = null,
-    passwordFieldStatus = null,
-    confirmField = null,
-    confirmFieldStatus = null,
-    validatorFunction = null,       // user-supplied function that validates the password fields
-    minPasswordLength = -1,
-    strings = {};                   // localized status messages, generated server-side and copied here
-
-function validatePasswords(event)
+// Returns an array of message IDs detailing why the password doesn't meet the requirements,
+// or empty array if the password is valid.
+function validatePassword(password, rules)
 {
-  try {
+    let errors = [];
+
+    // The rules have been validated server-side, so all error checking has been omitted
+    for (const rule of rules) {
+        let match = null;
+
+        switch (rule.type) {
+            case "length":
+            {
+                const length = (password === null || password === undefined) ? 0 : password.length;
+
+                switch (rule.operator) {
+                    case "=":
+                        match = (length == rule.value);
+                        break;
+
+                    case "!=":
+                        match = (length != rule.value);
+                        break;
+
+                    case "<":
+                        match = (length < rule.value);
+                        break;
+
+                    case "<=":
+                        match = (length <= rule.value);
+                        break;
+
+                    case ">":
+                        match = (length > rule.value);
+                        break;
+
+                    case ">=":
+                        match = (length >= rule.value);
+                        break;
+                }
+
+                break;
+            }
+
+            case "regexp":
+                match = (rule.value.exec(password) ? "=" : "!=") == rule.operator;
+                break;
+
+            default:
+                break;
+        }
+
+        if (!match)
+            errors.push(rule.message);
+    }
+
+    return errors;
+}
+
+// UI element handles
+let passwordField = null,
+    passwordStatus = null,
+    confirmField = null,
+    confirmStatus = null;
+
+// Optional function that is called whenever the input changes and the password is revalidated.
+// It receives two parameters: one true/false that indicates if the password is okay, and
+// another true/false that indicates if the password and the confirmation fields are identical.
+// Because the validator is placed on the page through an ERB template, we can't pass a JavaScript
+// function as a parameter, we can only pass its name.
+let updateCallbackName = null;
+
+function callCallback(ok, identical)
+{
+    if (updateCallbackName)
+        window[updateCallbackName](ok, identical);
+}
+
+function findTD(e)
+{
+    while (e && e.tagName != "TD")
+        e = e.parentNode;
+
+    return e;
+}
+
+// There's no escape, I mean, unescape from Ruby's string escaping wrath. If you use t(),
+// it *WILL* escape ' and " with HTML entities, period. No way around it. But JavaScript
+// inserts the strings on the page as-is, so these escaped entities will not display
+// correctly.
+function unEsacapeHTML(s)
+{
+    return s.replaceAll("&#39;", "'")
+            .replaceAll("&quot;", "\"");
+}
+
+function onPasswordInput()
+{
     const password = passwordField.value,
           confirmation = confirmField.value;
 
     if (password.length == 0 && confirmation.length == 0) {
-      // both fields are empty, nothing to validate
-      passwordFieldStatus.innerHTML = "";
-      confirmFieldStatus.innerHTML = "";
-      return;
+        passwordStatus.innerText = "";
+        confirmStatus.innerText = "";
+        callCallback(true, true);
+        return;
     }
 
-    switch (validatorFunction(password)) {
-      case PASSWORD_OK:
-        passwordFieldStatus.innerHTML = strings["ok"];
-        break;
+    // PASSWORD_RULES is defined in the inline <script> block on the page
+    const errors = validatePassword(password, PASSWORD_RULES);
 
-      case PASSWORD_ASCII_ONLY:
-        passwordFieldStatus.innerHTML = strings["ascii_only"];
-        break;
+    passwordStatus.innerText = (errors.length == 0) ? "" : errors.map(e => unEsacapeHTML(e)).join("\n");
+    confirmStatus.innerText = (confirmation == password) ? "" : unEsacapeHTML(CONFIRM_MISMATCH);
 
-      case PASSWORD_NO_WHITESPACE:
-        passwordFieldStatus.innerHTML = strings["no_whitespace"];
-        break;
-
-      case PASSWORD_TOO_SHORT:
-        if (minPasswordLength == -1)
-          passwordFieldStatus.innerHTML = strings["too_short"];
-        else passwordFieldStatus.innerHTML = strings["too_short_with_count"].replace("%{minlength}", minPasswordLength);
-
-        break;
-
-      default:
-        passwordFieldStatus.innerHTML = "?";
-        break;
-    }
-
-    if (password == confirmation)
-      confirmFieldStatus.innerHTML = strings["passwords_match"];
-    else confirmFieldStatus.innerHTML = strings["passwords_dont_match"];
-  } catch (e) {
-    // TODO: what now?
-  }
+    callCallback(errors.length == 0, (confirmation == password));
 }
 
-function initPasswordValidator(passwordFieldID, confirmFieldID, validator, minLength, localizedStrings)
+function initializePasswordValidator(passwordFieldID, confirmFieldID, callback=null)
 {
-  try {
-    console.log("initPasswordValidator(): passwordFieldID=\"" + passwordFieldID +
-                "\", confirmFieldID=\"" + confirmFieldID +
-                "\", minLength: " + minLength + "\n");
+    try {
+        console.log(`Initializing the password validator, have ${PASSWORD_RULES.length} rule(s)`);
 
-    passwordField = document.getElementById(passwordFieldID);
-    confirmField = document.getElementById(confirmFieldID);
-    validatorFunction = validator;
-    minPasswordLength = minLength;
-    strings = localizedStrings;
+        passwordField = document.getElementById(passwordFieldID);
+        confirmField = document.getElementById(confirmFieldID);
 
-    passwordFieldStatus = document.createElement("div");
-    confirmFieldStatus = document.createElement("div");
+        if (!passwordField) {
+            console.error(`Could not find the password input field by ID "${passwordFieldID}"`);
+            return;
+        }
 
-    passwordField.parentNode.appendChild(passwordFieldStatus);
-    confirmField.parentNode.appendChild(confirmFieldStatus);
+        if (!confirmField) {
+            console.error(`Could not find the confirm input field by ID "${confirmFieldID}"`);
+            return;
+        }
 
-    passwordField.addEventListener("input", validatePasswords);
-    confirmField.addEventListener("input", validatePasswords);
+        // Reuse the existing field error elements. Must find the actual TD element
+        // because if the field is invalid, it's wrapped in a DIV, but not otherwise.
+        passwordStatus = findTD(passwordField).querySelector("span.field_error");
+        confirmStatus = findTD(confirmField).querySelector("span.field_error");
 
-    console.log("initPasswordValidator(): done");
-  } catch (e) {
-    console.log("Password field validator initialization failed!\n");
-    console.log(e)
-  }
+        // Except if the form is the publicly accessible password form, then the
+        // field error spans don't exist and we must create them.
+        if (!passwordStatus) {
+            passwordStatus = document.createElement("span");
+            passwordStatus.classList.add("field_error");
+            findTD(passwordField).appendChild(passwordStatus);
+        }
+
+        if (!confirmStatus) {
+            confirmStatus = document.createElement("span");
+            confirmStatus.classList.add("field_error");
+            findTD(confirmField).appendChild(confirmStatus);
+        }
+
+        updateCallbackName = callback;
+
+        console.log(updateCallbackName);
+
+        passwordField.addEventListener("input", () => onPasswordInput());
+        confirmField.addEventListener("input", () => onPasswordInput());
+    } catch (e) {
+        console.error("Password field validator initialization failed!");
+        console.error(e)
+    }
 }
