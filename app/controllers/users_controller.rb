4d0b67f82e138e29bdb7e8d81ef7cbf8ea5c974c
Jarmo Pietiläinen <jarmo@opinsys.fi>
2022-02-09 10:30:39 +0200

    Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -935,26 +935,7 @@ class UsersController < ApplicationController
         return
       end
 
-      schools = Array(@user.puavoSchool.dup)
-      schools.reject!{ |s| s.to_s == @target.dn.to_s }
-      @user.puavoSchool = (schools.count == 1) ? schools[0] : schools
-
-      # Remove school admin associations if needed
-      Array(@user.puavoAdminOfSchool).each do |dn|
-        if dn.to_s == @target.dn.to_s
-          @user.puavoAdminOfSchool = Array(@user.puavoAdminOfSchool).reject{ |dn| dn.to_s == @target.dn.to_s }
-          @target.puavoSchoolAdmin = Array(@target.puavoSchoolAdmin).reject{ |dn| dn.to_s == @user.dn.to_s }
-          @target.save!
-          break
-        end
-      end
-
-      @user.save!
-
-      # The system appears to automatically add the user's UID and DN to the relevant arrays,
-      # but it won't *remove* them
-      LdapBase.ldap_modify_operation(@target.dn, :delete, [{ "member" => [@user.dn.to_s] }])
-      LdapBase.ldap_modify_operation(@target.dn, :delete, [{ "memberUid" => [@user.uid.to_s] }])
+      _remove_user_from_school(@user, @target)
 
       flash[:notice] = t('flash.user.removed_from_school', :name => @target.displayName)
     rescue StandardError => e
@@ -1190,7 +1171,6 @@ class UsersController < ApplicationController
 
   end
 
-  private
     def user_params
       u = params.require(:user).permit(
           :givenName,
@@ -1297,5 +1277,31 @@ class UsersController < ApplicationController
 
       logger.info('Synchronous action(s) completed without errors, proceeding with user deletion')
       return true, nil
+    end
+
+    # Removes the user from the target school. Assumes you've done the prerequisite verifications
+    # (the user is in the school and it's not the user's primary school).
+    def _remove_user_from_school(user, school)
+      schools = Array(user.puavoSchool.dup)
+      schools.reject!{ |s| s.to_s == school.dn.to_s }
+      user.puavoSchool = (schools.count == 1) ? schools[0] : schools
+
+      # Remove school admin associations if needed
+      Array(user.puavoAdminOfSchool).each do |dn|
+        if dn.to_s == school.dn.to_s
+          user.puavoAdminOfSchool = Array(user.puavoAdminOfSchool).reject { |dn| dn.to_s == school.dn.to_s }
+          school.puavoSchoolAdmin = Array(school.puavoSchoolAdmin).reject { |dn| dn.to_s == user.dn.to_s }
+          school.save!
+          break
+        end
+      end
+
+      # This must be done first, otherwise ldap_modify_operation() below will fail
+      user.save!
+
+      # The system appears to automatically add the user's UID and DN to the relevant arrays,
+      # but it won't *remove* them
+      LdapBase.ldap_modify_operation(school.dn, :delete, [{ "member" => [user.dn.to_s] }])
+      LdapBase.ldap_modify_operation(school.dn, :delete, [{ "memberUid" => [user.uid.to_s] }])
     end
 end
