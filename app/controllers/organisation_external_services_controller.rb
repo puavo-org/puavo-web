225be709ae2b30b2d9c4f2806143c3ddf8da1489
Jarmo Pietiläinen <jarmo@opinsys.fi>
2022-11-30 08:00:21 +0200

    Don't use case-sensitive DN comparisons in external services listings
    I spent... way too much time with this, trying to track down strange
    bugs I saw.
    
    Some LDAP searches return all-downcase DNs, which then cause serious
    problems down the road, because by default, string comparisons are
    case-sensitive. So change all comparisons to use downcased comparisons,
    but don't store downcased DNs.
    
    Yes, this is a hack.

--- app/controllers/organisation_external_services_controller.rb
+++ app/controllers/organisation_external_services_controller.rb
@@ -3,8 +3,8 @@ class OrganisationExternalServicesController < ExternalServicesBase
     return if redirected_nonowner_user?
 
     # Don't show services that aren't active in this organisation
-    activated = Array(LdapOrganisation.current.puavoActiveService).map { |dn| dn.to_s }.to_set
-    @external_services.reject! { |e| !activated.include?(e[:dn]) }
+    activated = Array(LdapOrganisation.current.puavoActiveService).map { |dn| dn.to_s.downcase }.to_set
+    @external_services.reject! { |e| !activated.include?(e[:dn].downcase) }
 
     respond_to do |format|
       format.html # show.html.erb
--- app/controllers/schools/external_services_controller.rb
+++ app/controllers/schools/external_services_controller.rb
@@ -2,15 +2,15 @@ class Schools::ExternalServicesController < ExternalServicesBase
   def show
     # Don't show services that aren't active in this school (and aren't
     # activated on organisation-level)
-    organisation_activated = Array(LdapOrganisation.current.puavoActiveService).map { |dn| dn.to_s }.to_set
+    organisation_activated = Array(LdapOrganisation.current.puavoActiveService).map { |dn| dn.to_s.downcase }.to_set
 
-    activated = Array(@school.puavoActiveService).map { |dn| dn.to_s }.to_set
+    activated = Array(@school.puavoActiveService).map { |dn| dn.to_s.downcase }.to_set
     activated += organisation_activated
 
-    @external_services.reject! { |e| !activated.include?(e[:dn]) }
+    @external_services.reject! { |e| !activated.include?(e[:dn].downcase) }
 
     @external_services.each do |e|
-      e[:org_level] = organisation_activated.include?(e[:dn])
+      e[:org_level] = organisation_activated.include?(e[:dn].downcase)
     end
 
     @is_school = true
