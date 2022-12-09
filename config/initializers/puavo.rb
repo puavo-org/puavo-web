617856a931c8d2181821225737a77892c69303f6
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2022-12-09 13:25:29 +0200

    Revert the code that looked up puavo-web configurations from a different place depending on Rails environment.

--- config/initializers/puavo.rb
+++ config/initializers/puavo.rb
@@ -1,16 +1,16 @@
 require 'puavo/authentication'
 
-Puavo::EXTERNAL_FILES = YAML.load_file( PuavoUsers::config_file('puavo_external_files.yml') ) rescue nil
-Puavo::EXTERNAL_LINKS = YAML.load_file( PuavoUsers::config_file('puavo_external_links.yml') ) rescue nil
+Puavo::EXTERNAL_FILES = YAML.load_file("#{Rails.root}/config/puavo_external_files.yml") rescue nil
+Puavo::EXTERNAL_LINKS = YAML.load_file("#{Rails.root}/config/puavo_external_links.yml") rescue nil
 
-Puavo::CONFIG = YAML.load_file( PuavoUsers::config_file('puavo_web.yml') )
+Puavo::CONFIG = YAML.load_file("#{Rails.root}/config/puavo_web.yml")
 
 begin
-  Puavo::SERVICES = YAML.load_file( PuavoUsers::config_file('services.yml') )
+  Puavo::SERVICES = YAML.load_file("#{Rails.root}/config/services.yml")
 rescue Errno::ENOENT => e
   Puavo::SERVICES = nil
   puts "WARNING: " + e.to_s
 end
 
-REDIS_CONFIG = PuavoUsers::config_file('redis.yml')
+REDIS_CONFIG = File.join(Rails.root, "config", "redis.yml")
 REDIS_CONNECTION = Redis.new YAML.load_file(REDIS_CONFIG).symbolize_keys
