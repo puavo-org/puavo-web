3f265c458d55571ee37f0fee267bf26c761cdbba
Jarmo Pietiläinen <jarmo@opinsys.fi>
2024-01-04 12:17:07 +0200

    Fix deprecation warnings in cookie setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,16 +2,16 @@
 
 if ENV.include?('STRICT_SAMESITE_COOKIES')
   # Actual production mode, with HTTPS and everything
-  Rails.application.config.session_store :cookie_store, {
-    :key => '_puavo_users_session',
-    :same_site => :strict,
-    :secure => :true,
-  }
+  Rails.application.config.session_store(:cookie_store,
+    key: '_puavo_users_session',
+    same_site: :strict,
+    secure: :true
+  )
 else
   # Testing and development environments aren't exactly secure, but they don't have to be.
   # This is also used on puavo-standalone.
-  Rails.application.config.session_store :cookie_store, {
-    :key => '_puavo_users_session',
-    :same_site => :lax,
-  }
+  Rails.application.config.session_store(:cookie_store,
+    key: '_puavo_users_session',
+    same_site: :lax
+  )
 end
