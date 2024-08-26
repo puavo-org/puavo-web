5f3e458ecdbebd0682758431fcb8ebb18faad406
Jarmo Pietiläinen <jarmo@opinsys.fi>
2024-08-26 09:22:05 +0300

    Implement preliminary scopes support
    The list of known scopes WILL change.

--- rest/resources/openidconnect.rb
+++ rest/resources/openidconnect.rb
@@ -6,6 +6,21 @@ require_relative '../lib/login/utility'
 
 module PuavoRest
 
+# Known built-in scopes
+BUILTIN_SCOPES = %w(
+  openid
+  profile
+  email
+  phone
+  roles
+  organisation
+  schools
+  groups
+  admin
+  ldap
+  security
+).to_set.freeze
+
 class OpenIDConnect < PuavoSinatra
   register Sinatra::R18n
 
@@ -90,7 +105,7 @@ class OpenIDConnect < PuavoSinatra
     # ----------------------------------------------------------------------------------------------
     # Verify the scopes
 
-    scopes = params.fetch('scope', '').split(' ')
+    scopes = params.fetch('scope', '').split(' ').to_set
 
     unless scopes.include?('openid')
       $rest_log.error("[#{request_id}] No 'openid' found in scopes (#{scopes.inspect})")
@@ -98,7 +113,33 @@ class OpenIDConnect < PuavoSinatra
       return
     end
 
-    # TODO: This is incomplete, but we don't have enough data yet to fully validate the scopes
+    # Build a set of all scopes this client is allowed to access. If the list contains scopes
+    # that aren't built-in or scope aliases, the operation stops, because this list MUST contain
+    # only valid scopes.
+    scope_aliases = oidc_config.fetch('scope_aliases', {}).freeze
+    client_allowed = ['openid']
+
+    client_config.fetch('allowed_scopes', []).each do |scope|
+      if BUILTIN_SCOPES.include?(scope)
+        client_allowed << scope
+        next
+      end
+
+      if scope_aliases.include?(scope)
+        client_allowed << scope_aliases[scope]
+        next
+      end
+
+      $rest_log.error("[#{request_id}] Client \"#{client_id}\" has an invalid allowed scope \"#{scope}\"; it is neither a built-in scope nor a scope alias.")
+      status 400
+      return
+    end
+
+    client_allowed = client_allowed.flatten.to_set & BUILTIN_SCOPES
+
+    # Remove scopes that aren't allowed. Because aliases have been expanded, their names can be
+    # safely removed from the list.
+    scopes = (scopes & client_allowed).to_a
 
     # ----------------------------------------------------------------------------------------------
     # Build Redis data
@@ -288,13 +329,12 @@ class OpenIDConnect < PuavoSinatra
       payload['nonce'] = oidc_state['nonce']
     end
 
-    # Append sample data (as if the "profile" scope was used)
-    payload.merge!({
-      'name' => 'Testi Käyttäjä',
-      'given_name' => 'Testi',
-      'family_name' => 'Käyttäjä',
-      'preferred_username' => 'testi.kayttaja',
-    })
+    # Collect the user data and append it to the payload
+    organisation = Organisation.by_domain(oidc_state['organisation']['domain'])
+    LdapModel.setup(organisation: organisation, credentials: CONFIG['server'])
+    user = PuavoRest::User.by_dn(oidc_state['user']['dn'])
+
+    payload.merge!(gather_user_data(request_id, oidc_state['scopes'], organisation, user))
 
     # The client has to supply this token in future requests to other OIDC endpoints
     access_token = SecureRandom.hex(16)
@@ -322,6 +362,146 @@ private
     LdapModel.setup(credentials: CONFIG['server']) do
       PuavoRest::ExternalService.by_dn(dn)
     end
+  end
+
+  # School searches are slow, so cache them
+  def get_school(dn, cache)
+    unless cache.include?(dn)
+      cache[dn] = School.find(dn)
+    end
+
+    cache[dn]
+  end
+
+  def gather_user_data(request_id, scopes, organisation, user)
+    out = {}
+    school_cache = {}
+
+    if scopes.include?('profile')
+      # Try to extract the modification timestamp from the LDAP operational attributes
+      begin
+        extra = User.raw_filter("ou=People,#{organisation['base']}", "(puavoId=#{user.id})", ['modifyTimestamp'])
+        updated_at = Time.parse(extra[0]['modifyTimestamp'][0]).to_i
+      rescue StandardError => e
+        $rest_log.warn("[#{request_id}] Cannot determine the user's last modification time: #{e}")
+        updated_at = nil
+      end
+    end
+
+    # Include LDAP DNs in the response?
+    has_ldap = scopes.include?('ldap')
+
+    if scopes.include?('profile')
+      out['given_name'] = user.first_name
+      out['family_name'] = user.last_name
+      out['name'] = "#{user.first_name} #{user.last_name}"
+      out['preferred_username'] = user.username
+      out['uuid'] = user.uuid
+      out['puavoid'] = user.puavo_id.to_i
+      out['ldap_dn'] = user.dn if has_ldap
+      out['external_id'] = user.external_id if user.external_id
+      out['learner_id'] = user.learner_id if user.learner_id
+      out['roles'] = user.roles
+      out['updated_at'] = updated_at unless updated_at.nil?
+      out['locale'] = user.locale
+      out['timezone'] = user.timezone
+    end
+
+    if scopes.include?('email')
+      # Prefer the primary email address if possible
+      unless user.primary_email.nil?
+        out['email'] = user.primary_email
+        out['email_verified'] = user.primary_email
+      else
+        unless user.verified_email.empty?
+          # This should not really happen, as the first verified email is
+          # also the primary email
+          out['email'] = user.verified_email[0]
+          out['email_verified'] = user.verified_email[0]
+        else
+          if user.email
+            out['email'] = user.email[0]
+          end
+        end
+      end
+    end
+
+    if scopes.include?('phone')
+      out['phone_number'] = user.telephone_number[0] unless user.telephone_number.empty?
+    end
+
+    if scopes.include?('schools')
+      schools = []
+
+      user.schools.each do |s|
+        school_cache[s.dn] = s
+
+        school = {
+          'name' => s.name,
+          'abbreviation' => s.abbreviation,
+          'puavoid' => s.puavo_id.to_i,
+          'external_id' => s.external_id,
+          'school_code' => s.school_code,
+          'oid' => s.school_oid,
+        }
+
+        school['ldap_dn'] = s.dn if has_ldap
+        school['primary'] = true if user.primary_school_dn == s.dn
+
+        schools << school
+      end
+
+      out['schools'] = schools
+    end
+
+    if scopes.include?('groups')
+      have_schools = scopes.include?('schools')
+      groups = []
+
+      user.groups.each do |g|
+        group = {
+          'name' => g.name,
+          'abbreviation' => g.abbreviation,
+          'puavoid' => g.id.to_i,
+          'external_id' => g.external_id,
+          'type' => g.type,
+        }
+
+        group['ldap_dn'] = g.dn if has_ldap
+        group['school_id'] = get_school(g.school_dn, school_cache).puavo_id.to_i if have_schools
+
+        groups << group
+      end
+
+      out['groups'] = groups
+    end
+
+    if scopes.include?('organisation')
+      org = {
+        'name' => organisation.name,
+        'domain' => organisation.domain,
+      }
+
+      org['ldap_dn'] = organisation.dn if has_ldap
+
+      out['organisation'] = org
+    end
+
+    if scopes.include?('admins')
+      out['is_organisation_owner'] = organisation.owner.include?(user.dn)
+
+      out['admin_in_schools'] = user.admin_of_school_dns.collect do |dn|
+        get_school(dn, school_cache).abbreviation
+      end
+    end
+
+    if scopes.include?('security')
+      out['mfa_enabled'] = user.mfa_enabled == true
+      out['opinsys_admin'] = false    # TODO: Future placeholder (for now)
+    end
+
+    school_cache = nil
+    out
   end
 
 end   # class OpenIDConnect
