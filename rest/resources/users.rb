45de9bf40e3c83a9454254a99852a8bac998cfef
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2025-02-11 12:47:43 +0200

    Fix setting of teacher permissions when default teacher permissions are empty.

--- rest/resources/users.rb
+++ rest/resources/users.rb
@@ -287,7 +287,8 @@ class User < LdapModel
     self.uuid = SecureRandom.uuid
 
     if self.roles.include?('teacher') then
-      self.teacher_permissions = Organisation.current.default_teacher_permissions
+      default_perms = Organisation.current.default_teacher_permissions
+      self.teacher_permissions = default_perms unless default_perms.empty?
     end
 
     validate_unique(:uid_number)
