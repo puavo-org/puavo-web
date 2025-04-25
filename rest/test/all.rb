1887750f1d032f6ba7429bb207b54f31102a23f4
Jarmo Pietiläinen <jarmo@opinsys.fi>
2025-04-25 08:41:17 +0300

    Silence the spammy "not verifying SSL hostname" warning during tests
    Incessant repetitive pointless warnings achieve nothing except fill
    logs with useless noise.

--- rest/test/all.rb
+++ rest/test/all.rb
@@ -1,3 +1,15 @@
+# Warnings are good, unless they become spammy. Monkeypatch the warning message
+# printer to suppress the annoying "not verifying SSL hostname of LDAPS server"
+# warning, because it accomplishes nothing in the test environment, except it
+# drowns out other warnings that might actually be serious.
+module Warning
+  def warn(msg)
+    return unless msg
+    return if msg.start_with?('not verifying SSL hostname')
+    super
+  end
+end
+
 Dir.glob(File.expand_path(File.dirname(__FILE__)) + "/*_test.rb").each do |p|
     puts "TEST #{ p }"
     require_relative p
