d39e8adaedc29f15c7edbaafc382b8cc2e1ba598
Jarmo Pietiläinen <jarmo@opinsys.fi>
2025-04-30 09:12:12 +0300

    It's a boolean, not string
    All tests pass.

--- rest/resources/users.rb
+++ rest/resources/users.rb
@@ -39,7 +39,7 @@ class User < LdapModel
   ldap_map :puavoRemovalRequestTime, :removal_request_time,
            LdapConverters::TimeStamp
   ldap_map :eduPersonPrincipalName, :edu_person_principal_name
-  ldap_map :puavoDoNotDelete, :do_not_delete
+  ldap_map :puavoDoNotDelete, :do_not_delete, LdapConverters::StringBoolean
   ldap_map :sambaPwdLastSet, :password_last_set, LdapConverters::Number
   ldap_map :puavoAdminPermissions, :admin_permissions, LdapConverters::ArrayValue
   ldap_map :puavoTeacherPermissions, :teacher_permissions, LdapConverters::ArrayValue
