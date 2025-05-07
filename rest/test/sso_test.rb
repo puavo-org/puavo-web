bf05621c50292693500172882fbbc378fb385307
Jarmo Pietiläinen <jarmo@opinsys.fi>
2025-05-07 13:46:42 +0300

    SSO multi-factor authentication tests
    Extensively tests all MFA-related functionality in the SSO form.
    The MFA secrets server is not installed in puavo-standalone,
    so we have to stub some of the relevant requests with WebMock.
    
    The result of three days of work.

--- rest/test/sso_test.rb
+++ rest/test/sso_test.rb
@@ -885,4 +885,460 @@ describe PuavoRest::SSO do
       assert jwt['username'] == 'verified' && jwt['email'] == 'verified@example.com'
     end
   end
+
+  describe 'MFA tests' do
+    before(:each) do
+      @mfa_user = PuavoRest::User.new(
+        first_name: 'Bob',
+        last_name: 'Page',
+        username: 'bob.page',
+        password: 'HpF3WNyvESjgQKftfFbPq5ckqsFoeWzEyKR89UoVpVjhlRYJhK',
+        roles: ['admin'],
+        school_dns: [@school.dn.to_s],
+        mfa_enabled: true,
+      )
+
+      @mfa_user.save!
+
+      mfa_server_url = 'http://127.0.0.1:9999/v1/authenticate'
+      mfa_bearer = "Bearer #{CONFIG['mfa_server']['bearer_key']}"
+
+      mfa_success = {
+        'status' => 'success',
+        'messages' => {
+          '1002' => 'Code accepted.'
+        }
+      }.to_json
+
+      mfa_fail = {
+        'status' => 'fail',
+        'messages' => {
+          '2002' => 'Invalid code.'
+        }
+      }.to_json
+
+      # Stub the MFA server requests. The server's not installed in puavo-standalone,
+      # and installing and configuring it would be a nightmare to automate. And actually
+      # setting up the MFA data would be almost impossible. So we'll fake the requests.
+      # Both success and failure responses (including HTTP status codes) were adapted
+      # from the real responses the MFA server returns.
+
+      # This request must succeed
+      stub_request(:post, mfa_server_url)
+        .with(
+          headers: { 'Authorization' => mfa_bearer },
+          body: {
+            'userid' => @mfa_user.uuid,
+            'code' => '987654'
+          }
+        )
+        .to_return(
+          status: 200,
+          headers: { 'X-Request-ID' => '-FAKEFAKE-' },
+          body: mfa_success
+        )
+
+      # All these requests must fail
+      ['123456', '234567', '345678', '456789', '111111'].each do |code|
+        stub_request(:post, mfa_server_url)
+          .with(
+            headers: { 'Authorization' => mfa_bearer },
+            body: {
+              'userid' => @mfa_user.uuid,
+              'code' => code
+            }
+          )
+          .to_return(
+            status: 403,
+            headers: { 'X-Request-ID' => '-FAKEFAKE-' },
+            body: mfa_fail
+          )
+      end
+    end
+
+    def authenticate_user(return_to: 'https://test-client-service.example.com/')
+      post '/v3/sso', {
+        'username' => 'bob.page',
+        'password' => 'HpF3WNyvESjgQKftfFbPq5ckqsFoeWzEyKR89UoVpVjhlRYJhK',
+        'organisation' => 'example.puavo.net',
+        'return_to' => return_to
+      }
+    end
+
+    def go_to_mfa_form
+      # Extract the MFA form URL from the response headers, and fetch the form.
+      # Can't just directly go to /v3/mfa, because we need the session token.
+      mfa_form_path = Addressable::URI.parse(last_response.headers['Location'])
+      assert_equal mfa_form_path.path, '/v3/mfa'
+
+      mfa_form_path.scheme = nil
+      mfa_form_path.host = nil
+
+      get mfa_form_path.to_s
+
+      # The caller needs this
+      mfa_form_path
+    end
+
+    it 'basic successfull MFA login' do
+      authenticate_user()
+
+      # Since the user has MFA enabled and we passed the login form with basic auth,
+      # the response must contain a redirect into the MFA form. Extract its URL
+      # and fetch the form. (Can't just directly go to /v3/mfa, because we need the
+      # session token.)
+      assert_equal last_response.status, 302
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.body, ''
+
+      mfa_form_path = go_to_mfa_form()
+
+      assert_equal last_response.status, 401    # still 401 even though Kerberos is complete by now
+      assert last_response.body.include?('Two-factor authentication has been activated on your account.')
+
+      # Ensure the form's hidden token is the same as in the URL (it's not necessary to validate
+      # this, but let's make sure)
+      assert_equal css('input[name="token"]').first.attributes['value'].value, mfa_form_path.query_values['token']
+
+      # Now we're in the MFA form. "Fill" and post it.
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '987654',
+      }
+
+      # Ensure the check passed. Validate the JWT.
+      assert_equal last_response.status, 302
+      assert_equal last_response.headers.include?('Location'), true
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+      jwt = JWT.decode(redirect.query_values['jwt'], @external_service.puavoServiceSecret)[0]
+      assert_equal 'bob.page', jwt['username']
+      assert_equal 'Bob', jwt['first_name']
+      assert_equal 'Page', jwt['last_name']
+      assert_equal 'admin', jwt['user_type']
+      assert_nil jwt['email']
+      assert_equal 'Example Organisation', jwt['organisation_name']
+      assert_equal 'example.puavo.net', jwt['organisation_domain']
+      assert_equal '/', jwt['external_service_path_prefix']
+      assert_equal @school.puavoId.to_s, jwt['primary_school_id']
+    end
+
+    it 'basic failed MFA login' do
+      authenticate_user()
+
+      assert_equal last_response.status, 302
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.body, ''
+
+      mfa_form_path = go_to_mfa_form()
+
+      assert_equal last_response.status, 401
+      assert last_response.body.include?('Two-factor authentication has been activated on your account.')
+      assert_equal css('input[name="token"]').first.attributes['value'].value, mfa_form_path.query_values['token']
+
+      # Use one of the incorrect codes
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '123456',
+      }
+
+      # Ensure the check failed
+      assert_equal last_response.status, 401
+      assert_equal last_response.headers.include?('Location'), false
+      assert last_response.body.include?('Two-factor authentication has been activated on your account.')
+      assert last_response.body.include?('<div id="mfa_invalid_code">Incorrect code</div>')
+    end
+
+    it 'multiple failed attempts' do
+      authenticate_user()
+      mfa_form_path = go_to_mfa_form()
+
+      # Post the form four times with incorrect codes
+      ['123456', '234567', '345678', '456789'].each do |code|
+        post '/v3/mfa', {
+          'token' => mfa_form_path.query_values['token'],
+          'mfa_code' => code,
+        }
+
+        # Ensure each check fails
+        assert_equal last_response.status, 401
+        assert_equal last_response.headers.include?('Location'), false
+        assert_equal css('input[name="token"]').empty?, false
+        assert_equal css('input#mfa_code').empty?, false
+        assert last_response.body.include?('Two-factor authentication has been activated on your account.')
+        assert last_response.body.include?('<div id="mfa_invalid_code">Incorrect code</div>')
+      end
+
+      # Then the final (fifth) check. This must halt the whole process.
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '111111',
+      }
+
+      assert_equal last_response.status, 401
+      assert_equal last_response.headers.include?('Location'), false
+      assert_equal css('input[name="token"]').empty?, true
+      assert_equal css('input#mfa_code').empty?, true
+      assert_equal last_response.body.include?('Two-factor authentication has been activated on your account.'), false
+      assert last_response.body.include?('Too many failed two-factor login attempts. Login halted. Go to the original login form and try logging in again.')
+    end
+
+    it 'expired MFA session must fail' do
+      authenticate_user()
+      mfa_form_path = go_to_mfa_form()
+
+      # Delete the Redis session entries, making the attempt fail even if the code is correct.
+      # This simulates the user waiting for too long.
+      REDIS_CONNECTION.del("mfa_sso_login:#{@mfa_user.uuid}")
+      REDIS_CONNECTION.del("mfa_sso_login:#{mfa_form_path.query_values['token']}")
+
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '987654',
+      }
+
+      # Ensure the check failed
+      assert_equal last_response.status, 401
+      assert_equal last_response.headers.include?('Location'), false
+      assert_equal last_response.body.include?('Two-factor authentication has been activated on your account.'), false
+      assert last_response.body.include?('Your login attempt has expired. Go to the original login form and try logging in again.')
+    end
+
+    it 'invalid MFA session token' do
+      get '/v3/mfa?token=foobar'
+
+      assert_equal last_response.status, 401
+      assert_equal last_response.headers.include?('Location'), false
+      assert_equal last_response.body.include?('Two-factor authentication has been activated on your account.'), false
+      assert last_response.body.include?('Your login attempt has expired. Go to the original login form and try logging in again.')
+    end
+
+    it 'SSO sessions with MFA work' do
+      # Authenticate the user into a service that uses sessions
+      clear_cookies
+      authenticate_user(return_to: 'https://session_test.example.com')
+      assert_equal last_response.status, 302
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.body, ''
+
+      # Fill in the MFA form
+      mfa_form_path = go_to_mfa_form()
+      assert_equal last_response.status, 401
+      assert last_response.body.include?('Two-factor authentication has been activated on your account.')
+
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '987654',
+      }
+
+      # Ensure the check passed. Validate the JWT.
+      assert_equal last_response.status, 302
+      assert_equal last_response.body, ''
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.headers.include?('Set-Cookie'), true
+      assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), true
+      session_key = last_response.cookies[PUAVO_SSO_SESSION_KEY][0]
+
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+      jwt = JWT.decode(redirect.query_values['jwt'], @external_service2.puavoServiceSecret)[0]
+
+      assert_equal 'bob.page', jwt['username']
+      assert_equal 'Bob', jwt['first_name']
+      assert_equal 'Page', jwt['last_name']
+      assert_equal 'admin', jwt['user_type']
+      assert_nil jwt['email']
+      assert_equal 'Example Organisation', jwt['organisation_name']
+      assert_equal 'example.puavo.net', jwt['organisation_domain']
+      assert_equal '/', jwt['external_service_path_prefix']
+      assert_equal @school.puavoId.to_s, jwt['primary_school_id']
+
+      # The session has been established, so try to use it. There should be no login/MFA forms.
+      clear_cookies
+
+      url = Addressable::URI.parse('/v3/sso')
+      url.query_values = { 'return_to' => 'https://session_test.example.com' }
+      set_cookie "#{PUAVO_SSO_SESSION_KEY}=#{session_key}"
+      get url.to_s, {}, { 'HTTP_HOST' => 'api.puavo.net' }
+
+      assert_equal last_response.status, 302
+      assert_equal last_response.body, ''
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.headers.include?('Set-Cookie'), false
+      assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), false
+
+      # Extract and verify the JWT again
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+      jwt = JWT.decode(redirect.query_values['jwt'], @external_service2.puavoServiceSecret)[0]
+
+      assert_equal 'bob.page', jwt['username']
+      assert_equal 'Bob', jwt['first_name']
+      assert_equal 'Page', jwt['last_name']
+      assert_equal 'admin', jwt['user_type']
+      assert_nil jwt['email']
+      assert_equal 'Example Organisation', jwt['organisation_name']
+      assert_equal 'example.puavo.net', jwt['organisation_domain']
+      assert_equal '/', jwt['external_service_path_prefix']
+      assert_equal @school.puavoId.to_s, jwt['primary_school_id']
+
+      # Try to login to another service with the session, it should fail
+      clear_cookies
+
+      url = Addressable::URI.parse('/v3/sso')
+      url.query_values = { 'return_to' => 'https://session_test2.example.com' }
+      set_cookie "#{PUAVO_SSO_SESSION_KEY}=#{session_key}"
+      get url.to_s, {}, { 'HTTP_HOST' => 'api.puavo.net' }
+
+      assert_equal last_response.status, 401
+      assert_equal last_response.headers.include?('Location'), false
+      assert_equal last_response.headers.include?('Set-Cookie'), false
+      assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), false
+      assert_equal last_response.body.include?('Two-factor authentication has been activated on your account.'), false
+      assert last_response.body.include?('Login to service <span>Another service with SSO sessions</span>')
+    end
+
+    it 'a failed MFA check must not create an SSO session' do
+      # Authenticate
+      clear_cookies
+      authenticate_user(return_to: 'https://session_test.example.com')
+      assert_equal last_response.status, 302
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.body, ''
+
+      # Fill in the MFA form
+      mfa_form_path = go_to_mfa_form()
+      assert_equal last_response.status, 401
+      assert last_response.body.include?('Two-factor authentication has been activated on your account.')
+
+      # Post the form four times with incorrect codes
+      ['123456', '234567', '345678', '456789'].each do |code|
+        post '/v3/mfa', {
+          'token' => mfa_form_path.query_values['token'],
+          'mfa_code' => code,
+        }
+
+        # Ensure each check fails
+        assert_equal last_response.status, 401
+        assert_equal last_response.headers.include?('Location'), false
+        assert last_response.body.include?('<div id="mfa_invalid_code">Incorrect code</div>')
+        assert_equal last_response.headers.include?('Location'), false
+        assert_equal last_response.headers.include?('Set-Cookie'), false
+        assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), false
+      end
+
+      # Then the final check. This must halt the whole process.
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '111111',
+      }
+
+      assert_equal last_response.headers.include?('Location'), false
+      assert_equal last_response.headers.include?('Set-Cookie'), false
+      assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), false
+      assert last_response.body.include?('Too many failed two-factor login attempts. Login halted. Go to the original login form and try logging in again.')
+    end
+
+    it 'MFA with custom fields in the return_to URL' do
+      clear_cookies
+      authenticate_user(return_to: 'https://test-client-service.example.com/?foo=bar&baz=quux')
+
+      # The custom parameters must not be present in the MFA form redirect
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+      assert_equal redirect.query_values.include?('foo'), false
+      assert_equal redirect.query_values.include?('bar'), false
+
+      mfa_form_path = go_to_mfa_form()
+
+      assert_equal last_response.headers.include?('Location'), false
+
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '987654',
+      }
+
+      # No session
+      assert_equal last_response.headers.include?('Set-Cookie'), false
+      assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), false
+
+      # Ensure the custom parameters are in the redirect URL
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+      assert_equal redirect.query_values.include?('foo'), true
+      assert_equal redirect.query_values['foo'], 'bar'
+      assert_equal redirect.query_values.include?('baz'), true
+      assert_equal redirect.query_values['baz'], 'quux'
+
+      # Quick JWT validation
+      jwt = JWT.decode(redirect.query_values['jwt'], @external_service.puavoServiceSecret)[0]
+      assert_equal 'bob.page', jwt['username']
+      assert_equal 'Bob', jwt['first_name']
+      assert_equal 'Page', jwt['last_name']
+    end
+
+    it 'MFA with custom fields in the return_to URL, with SSO sessions' do
+      # Step 1: Acquire a session
+      clear_cookies
+      authenticate_user(return_to: 'https://session_test.example.com/?foo=bar&baz=quux')
+
+      # The custom parameters must not be present in the MFA form redirect
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+      assert_equal redirect.query_values.include?('foo'), false
+      assert_equal redirect.query_values.include?('bar'), false
+
+      mfa_form_path = go_to_mfa_form()
+      assert_equal last_response.headers.include?('Location'), false
+
+      post '/v3/mfa', {
+        'token' => mfa_form_path.query_values['token'],
+        'mfa_code' => '987654',
+      }
+
+      # Must have a session
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.headers.include?('Set-Cookie'), true
+      assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), true
+      session_key = last_response.cookies[PUAVO_SSO_SESSION_KEY][0]
+
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+
+      # Must have the custom parameters in the URL
+      assert_equal redirect.query_values.include?('foo'), true
+      assert_equal redirect.query_values['foo'], 'bar'
+      assert_equal redirect.query_values.include?('baz'), true
+      assert_equal redirect.query_values['baz'], 'quux'
+
+      # Validate the JWT
+      jwt = JWT.decode(redirect.query_values['jwt'], @external_service2.puavoServiceSecret)[0]
+      assert_equal 'bob.page', jwt['username']
+      assert_equal 'Bob', jwt['first_name']
+      assert_equal 'Page', jwt['last_name']
+
+      # Step 2: Use the session. This time use different parameters in the redirect URL.
+      # The original parameters must not be present anymore.
+      clear_cookies
+
+      url = Addressable::URI.parse('/v3/sso')
+      url.query_values = { 'return_to' => 'https://session_test.example.com/?blurf=mangle' }
+      set_cookie "#{PUAVO_SSO_SESSION_KEY}=#{session_key}"
+      get url.to_s, {}, { 'HTTP_HOST' => 'api.puavo.net' }
+
+      # Validate the redirect
+      assert_equal last_response.status, 302
+      assert_equal last_response.body, ''
+      assert_equal last_response.headers.include?('Location'), true
+      assert_equal last_response.headers.include?('Set-Cookie'), false
+      assert_equal last_response.cookies.include?(PUAVO_SSO_SESSION_KEY), false
+
+      redirect = Addressable::URI.parse(last_response.headers['Location'])
+
+      # Must have the custom parameters in the URL
+      assert_equal redirect.query_values.include?('foo'), false
+      assert_equal redirect.query_values.include?('baz'), false
+      assert_equal redirect.query_values.include?('blurf'), true
+      assert_equal redirect.query_values['blurf'], 'mangle'
+
+      # Quick JWT validation
+      jwt = JWT.decode(redirect.query_values['jwt'], @external_service2.puavoServiceSecret)[0]
+      assert_equal 'bob.page', jwt['username']
+      assert_equal 'Bob', jwt['first_name']
+      assert_equal 'Page', jwt['last_name']
+    end
+  end
 end
