ca93c528abbcabe0273ab2db3348f3b3cec66782
Jarmo Pietiläinen <jarmo@opinsys.fi>
2025-05-07 07:40:50 +0300

    Fix indentation

--- rest/test/all.rb
+++ rest/test/all.rb
@@ -11,6 +11,6 @@ module Warning
 end
 
 Dir.glob(File.expand_path(File.dirname(__FILE__)) + "/*_test.rb").each do |p|
-    puts "TEST #{ p }"
-    require_relative p
+  puts "TEST #{ p }"
+  require_relative p
 end
