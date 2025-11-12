181316a668ccaeb7e0963bb3ce233561dbcd9e0a
Jarmo Pietiläinen <jarmo@opinsys.fi>
2025-11-12 12:35:41 +0200

    Indent code
    Commit 50dab1f61cc597d1209cb31d321c04b67d1dd05d accidentally unindented this.

--- rest/resources/oauth2/access_token_request.rb
+++ rest/resources/oauth2/access_token_request.rb
@@ -307,49 +307,48 @@ def build_access_token(request_id,
                        expires_in: 3600,
                        ldap_user_dn: nil,
                        custom_claims: nil)
+  now = Time.now.utc.to_i
 
-now = Time.now.utc.to_i
+  token_claims = {
+    'jti' => SecureRandom.uuid,
+    'iat' => now,
+    'nbf' => now,
+    'exp' => now + expires_in,
+    'iss' => ISSUER,
+    'sub' => subject,
+    'aud' => audience,
+    'scopes' => scopes.join(' ')
+  }
 
-token_claims = {
-  'jti' => SecureRandom.uuid,
-  'iat' => now,
-  'nbf' => now,
-  'exp' => now + expires_in,
-  'iss' => ISSUER,
-  'sub' => subject,
-  'aud' => audience,
-  'scopes' => scopes.join(' ')
-}
+  token_claims['client_id'] = client_id if client_id
 
-token_claims['client_id'] = client_id if client_id
+  token_claims['ldap_user_dn'] = ldap_user_dn if ldap_user_dn
 
-token_claims['ldap_user_dn'] = ldap_user_dn if ldap_user_dn
+  token_claims.merge!(custom_claims) if custom_claims.is_a?(Hash)
 
-token_claims.merge!(custom_claims) if custom_claims.is_a?(Hash)
+  # Load the signing private key. Unlike the public key, this is not kept in memory.
+  begin
+    private_key = OpenSSL::PKey.read(File.open(CONFIG['oauth2']['token_key']['private_file']))
+  rescue StandardError => e
+    # Tested (manually)
+    rlog.error("[#{request_id}] Cannot load the access token signing private key file: #{e}")
+    return { success: false }
+  end
 
-# Load the signing private key. Unlike the public key, this is not kept in memory.
-begin
-  private_key = OpenSSL::PKey.read(File.open(CONFIG['oauth2']['token_key']['private_file']))
-rescue StandardError => e
-  # Tested (manually)
-  rlog.error("[#{request_id}] Cannot load the access token signing private key file: #{e}")
-  return { success: false }
-end
+  # TODO: Support encrypted tokens with JWE? The "jwe" gem is already installed.
+  # TODO: Install the jwt-eddsa gem and use EdDSA signing? Is it compatible with JWT?
 
-# TODO: Support encrypted tokens with JWE? The "jwe" gem is already installed.
-# TODO: Install the jwt-eddsa gem and use EdDSA signing? Is it compatible with JWT?
+  # Sign the token data using the private key. RFC 9068 section 2.1. says the "typ" value
+  # SHOULD be "at+jwt", but the JWT gem does not set it, so let's set it manually.
+  # (I have no idea what I'm doing.)
+  access_token = JWT.encode(token_claims, private_key, 'ES256', { typ: 'at+jwt' })
 
-# Sign the token data using the private key. RFC 9068 section 2.1. says the "typ" value
-# SHOULD be "at+jwt", but the JWT gem does not set it, so let's set it manually.
-# (I have no idea what I'm doing.)
-access_token = JWT.encode(token_claims, private_key, 'ES256', { typ: 'at+jwt' })
-
-{
-  success: true,
-  access_token: access_token,
-  raw_token: token_claims,        # some places, like auditing, needs to see the raw data
-  expires_at: now + expires_in
-}
+  {
+    success: true,
+    access_token: access_token,
+    raw_token: token_claims,        # some places, like auditing, needs to see the raw data
+    expires_at: now + expires_in
+  }
 end
 
 # Returns a base64-encoded "half hash", ie. the first half (16 bytes) of a SHA256 hash of the input data.
