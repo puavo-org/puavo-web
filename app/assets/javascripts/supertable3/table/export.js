e061b3ea2d586c4eccfba027f63da3685fa85ed4
Jarmo Pietiläinen <jarmo@opinsys.fi>
2026-03-04 11:01:31 +0200

    SuperTable: Simplify exporting

--- app/assets/javascripts/supertable3/table/export.js
+++ app/assets/javascripts/supertable3/table/export.js
@@ -4,155 +4,166 @@ import { _tr } from "../../common/utils.js";
 import { getTemplate } from "../../common/dom.js";
 import { getPopupContents } from "../../common/modal_popup.js";
 import { ColumnType, INDEX_FILTERABLE } from "./constants.js";
-import { JAVASCRIPT_TIME_GRANULARITY } from "./utils.js";
+import { JAVASCRIPT_TIME_GRANULARITY, isNullOrUndefined } from "./utils.js";
 
+// These characters will be escaped, even if the outputted value will be quoted
 const ESCAPE = [
     ["\"", "\\\""],
     ["\t", "\\t"],
     ["\n", "\\n"],
 ];
 
-// Exports the data in columns, delimited by a single character
-function storeAsColumns(data, source, columns, timeColumns, mustQuote, separator, onlySelected, output)
+const transformDate = v => new Date(v * JAVASCRIPT_TIME_GRANULARITY).toISOString();
+
+function exportColumnar(data, options, separator)
 {
-    for (const rowIndex of source) {
+    let output = [];
+
+    for (const rowIndex of options.source) {
         const row = data.transformed[rowIndex];
-        let out = [];
+        let rowOut = [];
 
-        if (onlySelected)
-            if (!data.selectedItems.has(row.id[INDEX_FILTERABLE]))
-                continue;
-
-        for (const col of columns) {
-            if (!(col in row) || row[col][INDEX_FILTERABLE] === null || row[col][INDEX_FILTERABLE] === undefined) {
-                out.push(null);
-                continue;
-            }
-
-            if (timeColumns.has(col))
-                out.push(new Date(row[col][INDEX_FILTERABLE] * JAVASCRIPT_TIME_GRANULARITY).toISOString());
-            else if (mustQuote.has(col)) {
+        for (const col of options.columns) {
+            if (col in row) {
                 let value = row[col][INDEX_FILTERABLE];
 
-                if (Array.isArray(value)) {
-                    for (const e of ESCAPE)
-                        for (let v of value)
-                            v = v.replaceAll(e[0], e[1]);
-
-                    value = value.join("\n");
-                } else {
-                    for (const e of ESCAPE)
-                        value = value.replaceAll(e[0], e[1]);
+                if (isNullOrUndefined(value)) {         // output null for missing columns
+                    rowOut.push(null);
+                    continue;
                 }
 
-                out.push(`"${value}"`);
-            } else out.push(row[col][INDEX_FILTERABLE]);
+                if (options.timeColumns.has(col))
+                    rowOut.push(transformDate(value));
+                else if (options.mustQuote.has(col)) {
+                    if (Array.isArray(value)) {
+                        for (const e of ESCAPE)
+                            for (let v of value)
+                                v = v.replaceAll(e[0], e[1]);
+
+                        value = value.join("\n");
+                    } else {
+                        for (const e of ESCAPE)
+                            value = value.replaceAll(e[0], e[1]);
+                    }
+
+                    rowOut.push(`"${value}"`);
+                } else rowOut.push(value);
+            }
         }
 
-        output.push(out.join(separator));
+        output.push(rowOut.join(separator));
     }
+
+    return output;
 }
 
-function _doExport(format, data, allColumns, prefix)
+function exportJSON(data, options)
+{
+    let output = [];
+
+    for (const rowIndex of options.source) {
+        const row = data.transformed[rowIndex];
+        let rowOut = {};
+
+        for (const col of options.columns) {
+            if (col in row) {
+                const value = row[col][INDEX_FILTERABLE];
+
+                if (isNullOrUndefined(value))           // completely omit missing columns
+                    continue;
+
+                if (options.timeColumns.has(col))
+                    rowOut[col] = transformDate(value);
+                else rowOut[col] = value;
+            }
+        }
+
+        output.push(rowOut);
+    }
+
+    return output;
+}
+
+function doExport(table, format)
 {
     try {
-        const onlySelected = modalPopup.getContents().querySelector("input#only-selected-rows").checked,
-              visibleRows = modalPopup.getContents().querySelector("input#only-visible-rows").checked,
-              visibleCols = modalPopup.getContents().querySelector("input#only-visible-cols").checked;
+        const contents = modalPopup.getContents(),
+              onlySelected = contents.querySelector("input#selected_rows").checked,
+              visibleRows = contents.querySelector("input#visible_rows").checked,
+              visibleCols = contents.querySelector("input#visible_cols").checked;
 
-        let source = null;
+        let options = {};
 
-        if (visibleRows)
-            source = data.current;
-        else {
-            // 0, 1, 2, 3, ... N
-            source = Array.from(Array(data.transformed.length).keys());
+        // Which rows to export?
+        if (visibleRows) {
+            // Only the currently visible
+            options.source = table.data.current;
+        } else {
+            // All of them (0, 1, 2, 3, ... N)
+            options.source = Array.from(Array(table.data.transformed.length).keys());
         }
 
-        let output = [],
-            mimetype, extension;
+        if (onlySelected) {
+            // Only export rows that are selected
+            let newSource = [];
 
-        const columns = visibleCols ?
-            allColumns.current :
-            Object.keys(allColumns.definitions);
+            for (const rowIndex of options.source) {
+                const row = table.data.transformed[rowIndex];
 
-        let headers = [...columns];
+                if (onlySelected && table.data.selectedItems.has(row.id[INDEX_FILTERABLE]))
+                    newSource.push(rowIndex);
+            }
 
-        const timeColumns = new Set(),
-              mustQuote = new Set();
-
-        // Optional export alias names
-        for (let i = 0; i < headers.length; i++) {
-            const def = allColumns.definitions[headers[i]];
-
-            if (def.export_name)
-                headers[i] = def.export_name;
-
-            if (def.type == ColumnType.UNIXTIME)
-                timeColumns.add(headers[i]);
-
-            if (def.type == ColumnType.STRING)
-                mustQuote.add(headers[i]);
+            options.source = newSource;
         }
+
+        // Which columns to export?
+        options.columns = visibleCols ? table.columns.current : Object.keys(table.columns.definitions);
+        options.headers = [];
+        options.timeColumns = new Set();
+        options.mustQuote = new Set();
+
+        for (const column of options.columns) {
+            const definition = table.columns.definitions[column];
+
+            options.headers.push(definition.export_name || column);
+
+            if (definition.type == ColumnType.UNIXTIME)
+                options.timeColumns.add(column);
+
+            if (definition.type == ColumnType.STRING)
+                options.mustQuote.add(column);
+        }
+
+        const FORMATS = {
+            "csv":  { ext: "csv", sep: ";", mime: "text/csv", },
+            "tsv":  { ext: "tsv", sep: "\t", mime: "text/tab-separated-values" },
+            "json": { ext: "json", mime: "json" }
+        }
+
+        // Build the output
+        let output = [];
 
         switch (format) {
             case "csv":
-            default:
-                output.push(headers.join(";"));
-                storeAsColumns(data, source, columns, timeColumns, mustQuote, ";", onlySelected, output);
-
-                output = output.join("\n");
-                mimetype = "text/csv";
-                extension = "csv";
-                break;
-
             case "tsv":
-                output.push(headers.join("\t"));
-                storeAsColumns(data, source, columns, timeColumns, mustQuote, "\t", onlySelected, output);
-
+                output.push(options.headers.join(FORMATS[format].sep));
+                output = output.concat(exportColumnar(table.data, options, FORMATS[format].sep));
                 output = output.join("\n");
-                mimetype = "text/tab-separated-values";
-                extension = "tsv";
                 break;
 
-            case "json": {
-                for (const rowIndex of source) {
-                    const row = data.transformed[rowIndex];
-                    let out = {};
-
-                    if (onlySelected)
-                        if (!data.selectedItems.has(row.id[INDEX_FILTERABLE]))
-                            continue;
-
-                    for (let i = 0; i < columns.length; i++) {
-                        const col = columns[i];
-
-                        if (!(col in row) || row[col][INDEX_FILTERABLE] === null || row[col][INDEX_FILTERABLE] === undefined)
-                            continue;
-
-                        if (timeColumns.has(col))
-                            out[col] = new Date(row[col][INDEX_FILTERABLE] * JAVASCRIPT_TIME_GRANULARITY).toISOString();
-                        else out[col] = row[col][INDEX_FILTERABLE];
-                    }
-
-                    output.push(out);
-                }
-
-                output = JSON.stringify(output);
-                mimetype = "application/json";
-                extension = "json";
-
+            case "json":
+                output = JSON.stringify(exportJSON(table.data, options));
                 break;
-            }
         }
 
         // Build a blob object (it must be an array for some reason), then trigger a download.
         // Download code stolen from StackOverflow.
-        const b = new Blob([output], { type: mimetype });
-        let a = window.document.createElement("a");
+        const b = new Blob([output], { type: FORMATS[format].mime });
+        const a = window.document.createElement("a");
 
         a.href = window.URL.createObjectURL(b);
-        a.download = `${prefix}-${I18n.strftime(new Date(), "%Y%m%d-%H%M%S")}.${extension}`;
+        a.download = `${table.settings.csvPrefix}-${I18n.strftime(new Date(), "%Y%m%d-%H%M%S")}.${FORMATS[format].ext}`;
 
         document.body.appendChild(a);
         a.click();
@@ -163,19 +174,18 @@ function _doExport(format, data, allColumns, prefix)
     }
 }
 
-function open(event, data, columns, prefix, enableSelection)
+function open(table, e)
 {
     const template = getTemplate("exportPopup");
 
-    template.querySelector(`button#btnCSV`).addEventListener("click", () => _doExport("csv", data, columns, prefix));
-    template.querySelector(`button#btnTSV`).addEventListener("click", () => _doExport("tsv", data, columns, prefix));
-    template.querySelector(`button#btnJSON`).addEventListener("click", () => _doExport("json", data, columns, prefix));
+    for (const b of template.querySelectorAll("div#exportButtons button"))
+        b.addEventListener("click", e => doExport(table, e.target.dataset.format));
 
-    template.querySelector(`input#only-selected-rows`).disabled = !enableSelection;
+    template.querySelector(`input#selected_rows`).disabled = !table.settings.enableSelection;
 
     if (modalPopup.create()) {
         modalPopup.getContents().appendChild(template);
-        modalPopup.attach(event);
+        modalPopup.attach(e.target);
         modalPopup.display("bottom");
     }
 }
@@ -185,6 +195,6 @@ export function setup(table, frag)
     const button = frag.querySelector("thead div#top button#export");
 
     if (table.settings.enableExport)
-        button.addEventListener("click", e => open(e.target, table.data, table.columns, table.settings.csvPrefix, table.settings.enableSelection));
+        button.addEventListener("click", e => open(table, e));
     else button.remove();
 }
