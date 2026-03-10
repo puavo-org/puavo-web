d35fdbc53ed818bafc4903fa2b8b7837dbf5f808
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-03-10 12:33:18 +0200

    Drop multi-organisation support from "puavo-sync-external-login-info.rb".
    It has never been run in production without an organisation argument,
    and it is a better idea to have a separate process for each
    organisation anyway (more bureacracy, but there have only been
    a few).   And this is a better model anyway in case of an
    organisation specific event handler, which is planned to appear
    here with Univention integration.

--- rest/scripts/puavo-sync-external-login-info.rb
+++ rest/scripts/puavo-sync-external-login-info.rb
@@ -13,15 +13,18 @@ class SyncLog
 end
 
 def log_warn(e, orig_msg)
-  msg ="#{ orig_msg }: #{ e.message }"
-  msg += " / #{ e.backtrace.join(' / ') }" unless e.kind_of?(ExternalLoginError)
+  msg = orig_msg
+  msg += ": #{ e.message }" if e
+  msg += " / #{ e.backtrace.join(' / ') }" \
+    unless e.kind_of?(ExternalLoginError)
   warn(msg)
 end
 
 # necessary for routing ExternalLogin logging to stdout/stderr
 $rest_log = SyncLog.new
 
-only_this_organisation = ARGV[0]
+organisation = ARGV[0]
+raise "usage: $0 organisation_name" unless this_organisation
 
 topdomain = IO.read('/etc/puavo/topdomain').chomp
 
@@ -40,129 +43,118 @@ extlogin_conf = puavo_rest_conf['external_login']
 
 raise 'no external login configuration' unless extlogin_conf.kind_of?(Hash)
 
-all_organisations_ok = true
+org_conf = extlogin_conf[organisation]
+raise 'no external login configuration for "#{ organisation }"' \
+  unless org_conf.kind_of?(Hash)
 
-extlogin_conf.each do |organisation, org_conf|
-  next if only_this_organisation && only_this_organisation != organisation
+begin
+  puts ">>> checking organisation #{ organisation }"
 
-  begin
-    raise 'organisation is not a string' unless organisation.kind_of?(String)
+  org_domain = "#{ organisation }.#{ topdomain }"
 
-    puts ">>> checking organisation #{ organisation }"
+  manage_puavousers = org_conf['manage_puavousers']
+  raise 'no manage puavousers configuration value' if manage_puavousers.nil?
 
-    org_domain = "#{ organisation }.#{ topdomain }"
-
-    manage_puavousers = org_conf['manage_puavousers']
-    raise 'no manage puavousers configuration value' if manage_puavousers.nil?
-
-    unless manage_puavousers then
-      puts %Q{>> skipping organisation "#{ organisation }", users are not} \
-             + ' managed by external logins'
-      next
-    end
-
-    admin_dn = org_conf['admin_dn']
-    raise 'no admin dn' unless admin_dn
-
-    admin_password = org_conf['admin_password']
-    raise 'no admin password' unless admin_password
-
-    LdapModel.setup(
-      :credentials  => { :dn => admin_dn, :password => admin_password },
-      :organisation => PuavoRest::Organisation.by_domain!(org_domain),
-      :rest_root    => 'DUMMY')
-
-    external_login = PuavoRest::ExternalLogin.new
-    login_service = external_login.new_external_service_handler()
-
-    external_users = nil
-    begin
-      external_users = login_service.lookup_all_users()
-    rescue StandardError => e
-      raise 'error looking up all users from external login service: ' \
-              + e.message
-    end
-
-    all_users_ok = true
-
-    puts ">> checking users to remove on [#{ organisation }]"
-
-    PuavoRest::User.all.each do |puavo_user|
-      begin
-        extlogin_id = external_login.extlogin_id(puavo_user)
-        unless extlogin_id then
-          id_field = external_login.puavo_extlogin_id_field
-          puts "> user #{ puavo_user.username } [#{ organisation }] " \
-                 + "has no extlogin id (#{ id_field }), skipping check"
-          next
-        end
-
-        if external_users.has_key?(extlogin_id) then
-          puts "> user #{ puavo_user.username } (#{ extlogin_id })" \
-                 + " [#{ organisation }] exists in external service"
-          next
-        end
-
-        puts "> user #{ puavo_user.username } (#{ extlogin_id })"
-               + " [#{ organisation }] does not exist in external service"
-
-        # User not found in external service, so it must be in Puavo
-        # and we mark it for removal.
-        if puavo_user.mark_for_removal! then
-          puts(%Q{> puavo user "#{ puavo_user.username }" (#{ extlogin_id })} \
-                 + " [#{ organisation }] is marked for removal")
-        end
-
-      rescue StandardError => e
-        log_warn(e, %Q{> ! error in marking user "#{ puavo_user.username }"} \
-                      + " [#{ organisation }] for removal")
-        all_users_ok = false
-      end
-    end
-
-    puts '>> checking updates to users in Puavo'
-
-    external_users.each do |extlogin_id, userinfo|
-      begin
-        username   = userinfo['username']
-        user_entry = userinfo['user_entry']
-
-        puts "> updating Puavo information on #{ username }" \
-               + " (#{ extlogin_id }) [#{ organisation }]"
-
-        login_service.set_userinfo(username, user_entry)
-        userinfo = login_service.get_userinfo(username)
-        user_status = external_login.update_user_info(userinfo, nil, {})
-
-        if user_status != PuavoRest::ExternalLoginStatus::NOCHANGE \
-          && user_status != PuavoRest::ExternalLoginStatus::UPDATED then
-            raise 'user information update to Puavo failed with status' \
-                    + " #{ user_status }"
-        end
-
-      rescue StandardError => e
-        log_warn(e,
-                 %Q{! error checking user "#{ username }" (#{ extlogin_id })} \
-                   + " [#{ organisation }] in external login service")
-        all_users_ok = false
-      end
-    end
-
-    unless all_users_ok then
-      raise 'could not check and update one or more users' \
-              + ' from external login service'
-    end
-
-  rescue StandardError => e
-    log_warn(e, '!! error in updating users from external login service on' \
-                  + %Q{ organisation "#{ organisation }"})
-    all_organisations_ok = false
+  unless manage_puavousers then
+    puts %Q{>> users are not managed by external logins on "#{ organisation }"}
+    exit 0
   end
-end
 
-unless all_organisations_ok then
-  warn '!!! some organisation syncs failed'
-  exit(1)
+  admin_dn = org_conf['admin_dn']
+  raise 'no admin dn' unless admin_dn
+
+  admin_password = org_conf['admin_password']
+  raise 'no admin password' unless admin_password
+
+  LdapModel.setup(
+    :credentials  => { :dn => admin_dn, :password => admin_password },
+    :organisation => PuavoRest::Organisation.by_domain!(org_domain),
+    :rest_root    => 'DUMMY')
+
+  external_login = PuavoRest::ExternalLogin.new
+  login_service = external_login.new_external_service_handler()
+
+  external_users = nil
+  begin
+    external_users = login_service.lookup_all_users()
+  rescue StandardError => e
+    raise 'error looking up all users from external login service: ' \
+            + e.message
+  end
+
+  all_users_ok = true
+
+  puts ">> checking users to remove on [#{ organisation }]"
+
+  PuavoRest::User.all.each do |puavo_user|
+    begin
+      extlogin_id = external_login.extlogin_id(puavo_user)
+      unless extlogin_id then
+        id_field = external_login.puavo_extlogin_id_field
+        puts "> user #{ puavo_user.username } [#{ organisation }] " \
+               + "has no extlogin id (#{ id_field }), skipping check"
+        next
+      end
+
+      if external_users.has_key?(extlogin_id) then
+        puts "> user #{ puavo_user.username } (#{ extlogin_id })" \
+               + " [#{ organisation }] exists in external service"
+        next
+      end
+
+      puts "> user #{ puavo_user.username } (#{ extlogin_id })"
+             + " [#{ organisation }] does not exist in external service"
+
+      # User not found in external service, so it must be in Puavo
+      # and we mark it for removal.
+      if puavo_user.mark_for_removal! then
+        puts(%Q{> puavo user "#{ puavo_user.username }" (#{ extlogin_id })} \
+               + " [#{ organisation }] is marked for removal")
+      end
+
+    rescue StandardError => e
+      log_warn(e, %Q{> ! error in marking user "#{ puavo_user.username }"} \
+                    + " [#{ organisation }] for removal")
+      all_users_ok = false
+    end
+  end
+
+  puts '>> checking updates to users in Puavo'
+
+  external_users.each do |extlogin_id, userinfo|
+    begin
+      username   = userinfo['username']
+      user_entry = userinfo['user_entry']
+
+      puts "> updating Puavo information on #{ username }" \
+             + " (#{ extlogin_id }) [#{ organisation }]"
+
+      login_service.set_userinfo(username, user_entry)
+      userinfo = login_service.get_userinfo(username)
+      user_status = external_login.update_user_info(userinfo, nil, {})
+
+      if user_status != PuavoRest::ExternalLoginStatus::NOCHANGE \
+        && user_status != PuavoRest::ExternalLoginStatus::UPDATED then
+          raise 'user information update to Puavo failed with status' \
+                  + " #{ user_status }"
+      end
+
+    rescue StandardError => e
+      log_warn(e,
+               %Q{! error checking user "#{ username }" (#{ extlogin_id })} \
+                 + " [#{ organisation }] in external login service")
+      all_users_ok = false
+    end
+  end
+
+  unless all_users_ok then
+    log_warn(nil, 'could not check and update one or more users' \
+                    + ' from external login service')
+  end
+
+rescue StandardError => e
+  log_warn(e, '!! error in updating users from external login service on' \
+                + %Q{ organisation "#{ organisation }"})
 end
 
 exit(0)
