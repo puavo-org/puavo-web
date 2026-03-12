b7ab26e783821f5130007da15ce983ab466bb51e
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-03-12 13:26:19 +0200

    Add update_from_external to ExternalLoginService class that should simplify updating Puavo user data obtained from external services.

--- rest/lib/external_login/service.rb
+++ rest/lib/external_login/service.rb
@@ -7,5 +7,17 @@ module PuavoRest
       @rlog           = rlog
       @service_name   = service_name
     end
+
+    def update_from_external(username, external_user_entry)
+      set_userinfo_from_external(username, external_user_entry)
+      userinfo = get_userinfo_for_puavo(username)
+      user_status = @external_login.update_user_info(userinfo, nil, {})
+
+      if user_status != PuavoRest::ExternalLoginStatus::NOCHANGE \
+        && user_status != PuavoRest::ExternalLoginStatus::UPDATED then
+          raise 'user information update to Puavo failed with status' \
+                  + " #{ user_status }"
+      end
+    end
   end
 end
