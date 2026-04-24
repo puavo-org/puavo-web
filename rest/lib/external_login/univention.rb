0d9c38a5080649a674b9399754921bb1eaedcb6d
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2026-04-24 14:26:20 +0300

    Introduce classes for Univention object entries.

--- rest/lib/external_login/univention.rb
+++ rest/lib/external_login/univention.rb
@@ -6,17 +6,6 @@ require_relative './service'
 class UniventionDataError < ExternalLoginDataError; end
 
 module PuavoRest
-  module PrefillEvent
-    def handle
-      super
-      return self
-    end
-
-    def publisher_name
-      'udm-pre-fill'
-    end
-  end
-
   module Univention
     def self.get_conf_string(config, key, errmsg)
       value = config[key]
@@ -27,8 +16,6 @@ module PuavoRest
   end
 
   class ExternalUniventionService < ExternalLoginService
-    attr_reader :provisioning
-
     def initialize(external_login, univention_config, service_name, rlog)
       super(external_login, service_name, rlog)
 
@@ -84,17 +71,15 @@ module PuavoRest
 
     def get_userinfo_for_puavo(username)
       raise 'univention userinfo not set' \
-        unless @username && @univention_userinfo
+        unless @username && @univention_user
 
-      get_attr = lambda do |attr|
-                   get_univention_attribute(@univention_userinfo, attr)
-                 end
+      get_attr = lambda { |attr| @univention_user.get(attr) }
 
       puavo_extlogin_id_field = @external_login.puavo_extlogin_id_field
       userinfo = {
         puavo_extlogin_id_field => get_attr.call(@extlogin_id_field),
-        'first_name'            => get_attr.call('firstname'),
-        'last_name'             => get_attr.call('lastname'),
+        'first_name'            => get_attr.call('givenName'),
+        'last_name'             => get_attr.call('sn'),
         # XXX 'ldap_password_hash'    => get_attr.call('userPasswordHash'),
         'locked'                => get_attr.call('disabled'),
         'username'              => username,
@@ -147,7 +132,7 @@ module PuavoRest
     def get_user_roles()
       user_roles = []
 
-      ucsschool_roles = @univention_userinfo['ucsschoolRole']
+      ucsschool_roles = @univention_user.get('ucsschoolRole')
       unless ucsschool_roles.kind_of?(Array) && !ucsschool_roles.empty? then
         raise UniventionDataError,
               %Q{user "#{ @username }" has no roles in UCS@school}
@@ -180,11 +165,11 @@ module PuavoRest
     def get_user_puavo_school_dns()
       update_puavo_schools_by_id()
 
-      user_univention_school_urls = @univention_userinfo['schools']
+      user_univention_school_urls = @univention_user.get('schools')
       check_if_some_user_school_is_not_known \
         = lambda do
             user_univention_school_urls.any? do |url|
-              !@univention_schools_by_url.has_key?(url)
+              !@connector.schools.has_key?(url)
             end
           end
       if check_if_some_user_school_is_not_known.call() then
@@ -197,8 +182,7 @@ module PuavoRest
 
       user_puavo_schools = []
       @univention_schools_by_url.each do |school_url, univention_school_info|
-        extschool_id = get_univention_attribute(univention_school_info,
-                                                @extschool_id_field)
+        extschool_id = univention_school.get(@extschool_id_field)
         puavo_schools = @puavo_schools_by_id[extschool_id]
         user_puavo_schools += puavo_schools if puavo_schools
       end
@@ -208,17 +192,15 @@ module PuavoRest
 
     def update_school_information_and_report_connections()
       update_puavo_schools_by_id()
-      update_univention_schools_by_url()
 
       @rlog.info('>> reporting school linkages')
 
       current_puavo_schools_by_id = @puavo_schools_by_id.clone
 
-      @univention_schools_by_url.each do |school_url, univention_school_info|
-        extschool_id = get_univention_attribute(univention_school_info,
-                                                @extschool_id_field)
-        msg = %Q{> Univention school "#{ univention_school_info['name'] }"} \
-                + " (#{ extschool_id })"
+      @connector.schools.each do |school_id, univention_school|
+        extschool_id = univention_school.get(@extschool_id_field)
+        school_name = univention_school.get('displayName')
+        msg = %Q{> Univention school "#{ school_name }" (#{ extschool_id })}
         puavo_schools = current_puavo_schools_by_id.delete(extschool_id)
         if puavo_schools then
           school_names = puavo_schools.map { |s| %Q{"#{ s.name }"} }
@@ -240,18 +222,15 @@ module PuavoRest
     end
 
     def lookup_all_users
-      raise 'unimplemented'
-
       update_school_information_and_report_connections()
 
       users = {}
 
-      @provisioning.users.each do |univention_user|
-        extlogin_id = get_univention_attribute(univention_user,
-                                               @extlogin_id_field)
+      @connector.users.each do |user_id, univention_user|
+        extlogin_id = univention_user.get(@extlogin_id_field)
         next unless extlogin_id.kind_of?(String)
 
-        username = univention_user[@external_username_field]
+        username = univention_user.get(@external_username_field)
         next unless username.kind_of?(String)
 
         users[ extlogin_id ] = {
@@ -266,47 +245,14 @@ module PuavoRest
       users
     end
 
-    def get_schools_by_url
-      schools_by_url = {}
-
-      school_list = @provisioning.schools
-      school_list.each do |school|
-        begin
-          raise UniventionDataError,
-                'school does not have a name of type string' \
-            unless get_univention_attribute(school, 'name').kind_of?(String)
-          raise UniventionDataError,
-                'school does not have an url of type string' \
-            unless get_univention_attribute(school, 'url').kind_of?(String)
-          raise UniventionDataError,
-                'school does not have an external id of type string' \
-            unless get_univention_attribute(school, @extschool_id_field) \
-                     .kind_of?(String)
-          schools_by_url[ school['url'] ] = school
-        rescue StandardError => e
-          @rlog.error("error looking up school from Univention: #{ e.message }")
-        end
-      end
-
-      return schools_by_url
-    end
-
-    def get_univention_attribute(univention_object, attribute)
-      univention_object.dig(attribute)
-    end
-
-    def set_userinfo_from_external(username, univention_userinfo)
+    def set_userinfo_from_external(username, univention_user)
       @username = username
-      @univention_userinfo = univention_userinfo
+      @univention_user = univention_user
     end
 
     def update_puavo_schools_by_id()
       return if @puavo_schools_by_id
       @puavo_schools_by_id = get_puavo_schools_by_id()
-    end
-
-    def update_univention_schools_by_url()
-      @univention_schools_by_url = get_schools_by_url()
     end
 
     def update_univentionuserinfo(username)
@@ -328,10 +274,10 @@ module PuavoRest
     end
 
     def read_current
-      @groups       = []
-      @schools      = []
+      @groups       = {}
+      @schools      = {}
       @refresh_done = false
-      @users        = []
+      @users        = {}
 
       loop do
         begin
@@ -339,12 +285,14 @@ module PuavoRest
           raise 'EOF received when reading from puavo-univention' unless json
           event = create_event_object( JSON.parse(json) )
           if event.kind_of?(RefreshDoneEvent) then
-            @rlog.info('refresh done')
+            @rlog.info('>>> ldap refresh from Univention done')
             @refresh_done = true
             break
           end
           entry = event.get_entry(false)
+          sort_entry(entry)
         rescue StandardError => e
+          # XXX should this tolerate some errors such as missing attributes?
           errmsg = "connector reader error: #{ e.message }"
           @rlog.error(errmsg)
           raise errmsg
@@ -354,6 +302,15 @@ module PuavoRest
 
     def handle_events
       raise 'unimplemented'
+    end
+
+    def sort_entry(entry)
+      if entry.kind_of?(UniventionSchool) then
+        @schools[ entry.id ] = entry
+      end
+      if entry.kind_of?(UniventionUser) then
+        @users[ entry.id ] = entry
+      end
     end
 
     def create_event_object(parsed_data)
@@ -383,6 +340,25 @@ module PuavoRest
       @rlog          = rlog
     end
 
+    def check_entry(entry)
+      raise 'no univentionObjectType' \
+        unless entry.has_key?('univentionObjectType')
+      raise 'no objectClass' unless entry.has_key?('objectClass')
+    end
+
+    def entry_class(entry)
+      check_entry(entry)
+
+      return UniventionUser if entry['univentionObjectType'] == 'users/user'
+      return UniventionSchool \
+        if entry['univentionObjectType'] == 'container/ou' \
+             && Array(entry['objectClass']).include?('ucsschoolOrganizationalUnit')
+
+#     @rlog.warn(
+#       "unknown Univention object type: #{ entry['univentionObjectType'] }")
+      UniventionEntry
+    end
+
     def get_entry(expect_update)
       is_update = @data['is_update']
       raise 'is_update is not a boolean value' \
@@ -390,11 +366,10 @@ module PuavoRest
       raise 'update state did not match what we expected' \
         unless is_update == expect_update
 
-      # XXX should determine what type of Entry this is
-
       entry = @data['entry']
       raise 'entry is not a Hash' unless entry.kind_of?(Hash)
-      entry
+
+      entry_class(entry).new(entry, @rlog)
     end
   end
 
@@ -408,5 +383,37 @@ module PuavoRest
     def initialize(data, login_service, rlog)
       super(data, login_service, rlog)
     end
+  end
+
+  class UniventionEntry
+    def initialize(data, rlog)
+      @data = data
+      @rlog = rlog
+      validate
+    end
+
+    def get(key)
+      unless @data.has_key?(key) then
+        msg = "no such key as '#{ key }'"
+        @rlog.warn(msg + " in #{ @data.inspect }")
+        raise msg
+      end
+      @data[key]
+    end
+
+    def id
+      @data['univentionObjectIdentifier']
+    end
+
+    def validate
+      raise 'no univentionObjectIdentifier' \
+        unless @data['univentionObjectIdentifier'].kind_of?(String)
+    end
+  end
+
+  class UniventionSchool < UniventionEntry
+  end
+
+  class UniventionUser < UniventionEntry
   end
 end
