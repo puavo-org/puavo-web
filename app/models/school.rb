a710b29c0f33f153ae024e5fa446958f21a15309
Jarmo Pietiläinen <jarmo@opinsys.fi>
2026-04-27 11:37:40 +0300

    Clean up the school model
    This has been running in my development environment for a week now
    without issues. It has passed 5-6 rounds of full testing. I've
    re-checked the changed lines at least 3 times.

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,7 @@
-require_relative "./puavo_conf_mixin"
-require_relative "./puavo_tag_mixin"
+# frozen_string_literal: true
+
+require_relative 'puavo_conf_mixin'
+require_relative 'puavo_tag_mixin'
 
 class School < BaseGroup
   include Wlan
@@ -11,40 +13,37 @@ class School < BaseGroup
   include Mountpoint
   include Puavo::Locale
 
-  ldap_mapping( :dn_attribute => "puavoId",
-                :prefix => "ou=Groups",
-                :classes => ['top','posixGroup','puavoSchool','sambaGroupMapping'] )
+  ldap_mapping dn_attribute: 'puavoId',
+               prefix: 'ou=Groups',
+               classes: %w[top posixGroup puavoSchool sambaGroupMapping]
 
-  has_many( :members, :class_name => "User",
-            :primary_key => 'dn',
-            :foreign_key => 'puavoSchool' )
-  has_many :user_members, :class_name => "User", :wrap => "member", :primary_key => "dn"
-  has_many( :user_school_admins,
-            :class_name => 'User',
-            :primary_key => 'dn',
-            :foreign_key => 'puavoAdminOfSchool' )
-  has_many :user_member_uids, :class_name => "User", :wrap => "memberUid", :primary_key => "uid"
-
-  has_many( :groups, :class_name => 'Group',
-            :primary_key => 'dn',
-            :foreign_key => 'puavoSchool' )
+  has_many :members, class_name: 'User', primary_key: 'dn', foreign_key: 'puavoSchool'
+  has_many :user_members, class_name: 'User', wrap: 'member', primary_key: 'dn'
+  has_many :user_school_admins, class_name: 'User', primary_key: 'dn', foreign_key: 'puavoAdminOfSchool'
+  has_many :user_member_uids, class_name: 'User', wrap: 'memberUid', primary_key: 'uid'
+  has_many :groups, class_name: 'Group', primary_key: 'dn', foreign_key: 'puavoSchool'
 
   attr_accessor :image
 
   before_save :set_puavo_mountpoint, :set_preferred_language
 
-  validate :validate_school_code, :validate_group_name, :validate_name_prefix, :validate_name, :validate_puavoconf, :validate_wlan_attributes, :validate_image
+  validate :validate_school_code, :validate_group_name, :validate_name_prefix, :validate_name,
+           :validate_puavoconf, :validate_wlan_attributes, :validate_image
 
-  alias_method :v1_as_json, :as_json
+  alias v1_as_json as_json
 
   def self.image_size
-    { :width => 400, :height => 200 }
+    {
+      width: 400,
+      height: 200
+    }
   end
 
   def validate_name
     if self.displayName.to_s.empty?
-      errors.add( :displayName, I18n.t("activeldap.errors.messages.blank",
-                                       :attribute => I18n.t("activeldap.attributes.school.displayName")) )
+      errors.add(:displayName,
+                 I18n.t('activeldap.errors.messages.blank',
+                        attribute: I18n.t('activeldap.attributes.school.displayName')))
     end
   end
 
@@ -56,15 +55,15 @@ class School < BaseGroup
 
   def validate_group_name
     unless self.cn.to_s =~ /^[a-z0-9-]+$/
-      errors.add( :cn, I18n.t("activeldap.errors.messages.school.invalid_characters",
-                              :attribute => I18n.t("activeldap.attributes.school.cn")) )
+      errors.add(:cn, I18n.t('activeldap.errors.messages.school.invalid_characters',
+                             attribute: I18n.t('activeldap.attributes.school.cn')))
     end
   end
 
   def validate_name_prefix
     unless self.puavoNamePrefix.to_s =~ /^[a-z0-9-]*$/
-      errors.add( :puavoNamePrefix, I18n.t("activeldap.errors.messages.school.invalid_characters",
-                                           :attribute => I18n.t("activeldap.attributes.school.puavoNamePrefix")) )
+      errors.add(:puavoNamePrefix, I18n.t('activeldap.errors.messages.school.invalid_characters',
+                                          attribute: I18n.t('activeldap.attributes.school.puavoNamePrefix')))
     end
   end
 
@@ -82,30 +81,28 @@ class School < BaseGroup
 
   def remove_user(user)
     begin
-    self.ldap_modify_operation(:delete, [{ "memberUid" => [user.uid]},
-                                         { "member" => [user.dn.to_s] }])
+      self.ldap_modify_operation(:delete, [{ 'memberUid' => [user.uid] }, { 'member' => [user.dn.to_s] }])
     rescue ActiveLdap::LdapError::NoSuchAttribute
-      logger.warn "Cannot remove nonexistent memberUid=#{ user.uid } or member=#{ user.dn.to_s } from #{ displayName }"
+      logger.warn "Cannot remove nonexistent memberUid=#{user.uid} or member=#{user.dn} from #{displayName}"
     end
 
-    if Array(user.puavoAdminOfSchool).map{ |s| s.to_s }.include?(self.dn.to_s)
+    if Array(user.puavoAdminOfSchool).map(&:to_s).include?(self.dn.to_s)
       begin
-        self.ldap_modify_operation(:delete, [{ "puavoSchoolAdmin" => [user.dn.to_s] }])
+        self.ldap_modify_operation(:delete, [{ 'puavoSchoolAdmin' => [user.dn.to_s] }])
       rescue ActiveLdap::LdapError::NoSuchAttribute
-        logger.warn "Cannot remove nonexistent puavoSchoolAdmin=#{ user.dn.to_s } from #{ displayName }"
+        logger.warn "Cannot remove nonexistent puavoSchoolAdmin=#{user.dn} from #{displayName}"
       end
-
     end
   end
 
   def add_admin(user)
     begin
-      self.ldap_modify_operation( :add, [{"puavoSchoolAdmin" => [user.dn.to_s]}] )
+      self.ldap_modify_operation(:add, [{ 'puavoSchoolAdmin' => [user.dn.to_s] }])
     rescue ActiveLdap::LdapError::TypeOrValueExists
     end
 
     begin
-      user.ldap_modify_operation( :add, [{"puavoAdminOfSchool" => [self.dn.to_s]}] )
+      user.ldap_modify_operation(:add, [{ 'puavoAdminOfSchool' => [self.dn.to_s] }])
     rescue ActiveLdap::LdapError::TypeOrValueExists
     end
 
@@ -124,12 +121,12 @@ class School < BaseGroup
     end
 
     begin
-      self.ldap_modify_operation( :delete, [{"puavoSchoolAdmin" => [user.dn.to_s]}] )
+      self.ldap_modify_operation(:delete, [{ 'puavoSchoolAdmin' => [user.dn.to_s] }])
     rescue ActiveLdap::LdapError::NoSuchAttribute
     end
 
     begin
-      user.ldap_modify_operation( :delete, [{"puavoAdminOfSchool" => [self.dn.to_s]}] )
+      user.ldap_modify_operation(:delete, [{ 'puavoAdminOfSchool' => [self.dn.to_s] }])
     rescue ActiveLdap::LdapError::NoSuchAttribute
     end
   end
@@ -138,7 +135,7 @@ class School < BaseGroup
     if user.organisation_owner?
       self.all.sort
     else
-      self.find(:all, :attribute => "puavoSchoolAdmin", :value => user.dn).sort
+      self.find(:all, attribute: 'puavoSchoolAdmin', value: user.dn).sort
     end
   end
 
@@ -152,29 +149,20 @@ class School < BaseGroup
   #  super(*args)
   #end
 
-  def as_json(*args)
-    return ldap_prettify
+  def as_json(*)
+    ldap_prettify
   end
 
   def printers
-    servers_dn = Server.find(
-      :all,
-      {
-        :attribute => "puavoSchool",
-        :value => self.dn
-      }
-    ).map{ |server| server.dn }
+    servers_dn = Server.find(:all, { attribute: 'puavoSchool', value: self.dn })
+                       .map(&:dn)
 
-    Printer.all.select{ |p| servers_dn.include?(p.puavoServer) }
+    Printer.all.select { |p| servers_dn.include?(p.puavoServer) }
   end
 
   def boot_servers
-    Server.find(:all, {
-      :attribute => "puavoSchool",
-      :value => self.dn.to_s
-    }).select do |s|
-      s.puavoDeviceType == "bootserver"
-    end
+    Server.find(:all, { attribute: 'puavoSchool', value: self.dn.to_s })
+          .select { |s| s.puavoDeviceType == 'bootserver' }
   end
 
   def has_wireless_printer?(printer)
@@ -184,17 +172,18 @@ class School < BaseGroup
 
   def add_wireless_printer(printer)
     printer = self.class.ensure_dn(printer)
-    ldap_modify_operation(:add, [
-      { "puavoWirelessPrinterQueue" => printer }
-    ]) rescue ActiveLdap::LdapError::TypeOrValueExists
+
+    begin
+      ldap_modify_operation(:add, [{ 'puavoWirelessPrinterQueue' => printer }])
+    rescue ActiveLdap::LdapError::TypeOrValueExists
+    end
   end
 
   def remove_wireless_printer(printer)
     printer = self.class.ensure_dn(printer)
-    ldap_modify_operation(:delete, [
-      { "puavoWirelessPrinterQueue" => [printer.to_s] }
-    ]) rescue ActiveLdap::LdapError::NoSuchAttribute
+    begin
+      ldap_modify_operation(:delete, [{ 'puavoWirelessPrinterQueue' => [printer.to_s] }])
+    rescue ActiveLdap::LdapError::NoSuchAttribute
+    end
   end
-
-
 end
