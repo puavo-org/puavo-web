c7e81c0eeea22e8d188105a29cc2eb180eec6c38
Jarmo Pietiläinen <jarmo@opinsys.fi>
2026-06-12 08:20:13 +0300

    Rubocop the groups rename helper
    Rubocop does not like the way the new number is formatted,
    but I'm not going to touch it.

--- app/helpers/rename_groups_helper.rb
+++ app/helpers/rename_groups_helper.rb
@@ -1,10 +1,12 @@
-module RenameGroupsHelper
+# frozen_string_literal: true
 
+module RenameGroupsHelper
   def increase_numeric_value_of_string(value)
     match_data = value.match(/\d+/)
-    return value + "1" if match_data.nil?
+    return "#{value}1" if match_data.nil?
+
     number_length = match_data[0].length
     number = match_data[0].to_i + 1
-    return value.sub(/\d+/, ("%0#{number_length}d" % number))
+    value.sub(/\d+/, ("%0#{number_length}d" % number))
   end
 end
