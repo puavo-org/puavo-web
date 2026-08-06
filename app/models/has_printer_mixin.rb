4d6fb55997022a0b8902b3304c102e9980d75fd7
Jarmo Pietiläinen <jarmo@opinsys.fi>
2026-08-06 08:26:22 +0300

    Rubocop printer models stuff

--- app/models/has_printer_mixin.rb
+++ app/models/has_printer_mixin.rb
@@ -1,5 +1,6 @@
-module HasPrinterMixin
+# frozen_string_literal: true
 
+module HasPrinterMixin
   def has_printer?(printer)
     printer = self.class.ensure_dn(printer)
     Array(self.puavoPrinterQueue).include?(printer)
@@ -7,16 +8,19 @@ module HasPrinterMixin
 
   def add_printer(printer)
     printer = self.class.ensure_dn(printer)
-    ldap_modify_operation(:add, [
-      { "puavoPrinterQueue" => printer }
-    ]) rescue ActiveLdap::LdapError::TypeOrValueExists
+
+    begin
+      ldap_modify_operation(:add, [{ 'puavoPrinterQueue' => printer }])
+    rescue ActiveLdap::LdapError::TypeOrValueExists
+    end
   end
 
   def remove_printer(printer)
     printer = self.class.ensure_dn(printer)
-    ldap_modify_operation(:delete, [
-      { "puavoPrinterQueue" => [printer.to_s] }
-    ]) rescue ActiveLdap::LdapError::NoSuchAttribute
-  end
 
+    begin
+      ldap_modify_operation(:delete, [{ 'puavoPrinterQueue' => [printer.to_s] }])
+    rescue ActiveLdap::LdapError::NoSuchAttribute
+    end
+  end
 end
--- app/models/printer.rb
+++ app/models/printer.rb
@@ -1,28 +1,28 @@
+# frozen_string_literal: true
+
 class Printer < LdapBase
-  ldap_mapping( :dn_attribute => "puavoId",
-                :prefix => "ou=Printers",
-                :classes => ['top', 'cupsPrinter', 'puavoPrinterQueue'] )
+  ldap_mapping dn_attribute: 'puavoId',
+               prefix: 'ou=Printers',
+               classes: %w[top cupsPrinter puavoPrinterQueue]
 
   before_validation :set_puavo_id, :set_empty_location
 
   def validate
-    Printer.find( :all,
-                  :attribute => 'printerDescription',
-                  :value => self.printerDescription ).each do |printer|
+    Printer.find(:all, attribute: 'printerDescription', value: self.printerDescription).each do |printer|
       if self.puavoId != printer.puavoId && self.puavoServer == printer.puavoServer
-        errors.add( :printerDescription,
-                    I18n.t("activeldap.errors.messages.uniqueness",
-                           :attribute => I18n.t("activeldap.attributes.printer.printerDescription") ) )
+        errors.add(:printerDescription,
+                   I18n.t('activeldap.errors.messages.uniqueness',
+                          attribute: I18n.t('activeldap.attributes.printer.printerDescription')))
       end
     end
   end
-  
+
   def set_puavo_id
     self.puavoId = IdPool.next_puavo_id if self.puavoId.nil?
   end
 
   def id
-    self.puavoId.to_s unless self.puavoId.nil?
+    self.puavoId&.to_s
   end
 
   def server
@@ -30,8 +30,6 @@ class Printer < LdapBase
   end
 
   def set_empty_location
-    if self.printerLocation.to_s.empty?
-      self.printerLocation = "-"
-    end
+    self.printerLocation = '-' if self.printerLocation.to_s.empty?
   end
 end
