3ce3f666c208ec85c243713263cbf2a1f0c9c99a
Jarmo Pietiläinen <jarmo@opinsys.fi>
2026-08-07 12:19:32 +0300

    Rubocop the Kerberos model

--- app/models/kerberos.rb
+++ app/models/kerberos.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 class Kerberos < LdapBase
-  ldap_mapping(:dn_attribute => 'krbPrincipalName',
-               :prefix       => 'ou=Kerberos Realms',
-               :classes      => [ 'krbPrincipal' ])
+  ldap_mapping dn_attribute: 'krbPrincipalName',
+               prefix: 'ou=Kerberos Realms',
+               classes: %w[krbPrincipal]
 
   def self.all_auth_times_by_uid
     auth_times_by_uid = {}
 
     self.all.each do |k|
-      unless k.krbPrincipalName then
-        warn("could not find kerberos principal in #{ k.dn }")
+      unless k.krbPrincipalName
+        warn("could not find kerberos principal in #{k.dn}")
         next
       end
 
       match = k.krbPrincipalName.match(/^(.*)@/)
-      unless match then
-        logger.warn("kerberos principal is in bad format in dn=#{ k.dn }")
+
+      unless match
+        logger.warn("kerberos principal is in bad format in dn=#{k.dn}")
         next
       end
 
