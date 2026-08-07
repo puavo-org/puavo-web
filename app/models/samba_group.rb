1eeeab45371ec6a86aa4e590707f443a6c1a6101
Jarmo Pietiläinen <jarmo@opinsys.fi>
2026-08-07 12:37:11 +0300

    Rubocop the Samba group model

--- app/models/samba_group.rb
+++ app/models/samba_group.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class SambaGroup < LdapBase
-  ldap_mapping( :dn_attribute => "cn",
-                :prefix => "ou=Groups",
-                :classes => ["top","posixGroup", "sambaGroupMapping"] )
+  ldap_mapping dn_attribute: 'cn',
+               prefix: 'ou=Groups',
+               classes: %w[top posixGroup sambaGroupMapping]
 
   def self.add_uid_to_memberUid(cn, uid)
     object = self.find(cn)
@@ -16,20 +18,16 @@ class SambaGroup < LdapBase
   end
 
   def add_uid_to_memberUid(uid)
-    begin
-      unless Array(self.memberUid).include?(uid)
-        self.ldap_modify_operation( :add, [{"memberUid" => [uid]}] )
-      end
-    rescue ActiveLdap::LdapError::TypeOrValueExists
-      # Just move on; LDAP isn't a relational DB, this can happen and it'll get fixed soon
+    unless Array(self.memberUid).include?(uid)
+      self.ldap_modify_operation(:add, [{ 'memberUid' => [uid] }])
     end
+  rescue ActiveLdap::LdapError::TypeOrValueExists
+    # Just move on; LDAP isn't a relational DB, this can happen and it'll get fixed soon
   end
 
   def delete_uid_from_memberUid(uid)
-    begin
-      self.ldap_modify_operation( :delete, [{"memberUid" => [uid]}] )
-    rescue ActiveLdap::LdapError::NoSuchAttribute
-      logger.warn "Cannot remove nonexistent memberUid=#{ uid } from SambaGroup #{ dn }"
-    end
+    self.ldap_modify_operation(:delete, [{ 'memberUid' => [uid] }])
+  rescue ActiveLdap::LdapError::NoSuchAttribute
+    logger.warn "Cannot remove nonexistent memberUid=#{uid} from SambaGroup #{dn}"
   end
 end
