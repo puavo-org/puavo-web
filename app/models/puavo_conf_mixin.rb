a2b6bbf0aa9aa6d41311cf53e18678f3b49ad04b
Jarmo Pietiläinen <jarmo@opinsys.fi>
2026-08-07 12:32:49 +0300

    Rubocop the puavoconf mixin

--- app/models/puavo_conf_mixin.rb
+++ app/models/puavo_conf_mixin.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module PuavoConfMixin
   def validate_puavoconf
     puavoconf_string = self.puavoConf.to_s
@@ -5,31 +7,25 @@ module PuavoConfMixin
 
     begin
       puavoconf_data = JSON.parse(puavoconf_string)
-    rescue JSON::ParserError => e
-      errors.add(:puavoConf,
-                 I18n.t('activeldap.errors.messages.puavoconf.not_json'))
+    rescue JSON::ParserError
+      errors.add(:puavoConf, I18n.t('activeldap.errors.messages.puavoconf.not_json'))
       return
-    rescue StandardError => e
-      errors.add(:puavoConf,
-                 I18n.t('activeldap.errors.messages.puavoconf.unknown_json_error'))
+    rescue StandardError
+      errors.add(:puavoConf, I18n.t('activeldap.errors.messages.puavoconf.unknown_json_error'))
       return
     end
 
-    unless puavoconf_data.kind_of?(Hash) then
-      errors.add(:puavoConf,
-                 I18n.t('activeldap.errors.messages.puavoconf.not_a_hash'))
+    unless puavoconf_data.is_a?(Hash)
+      errors.add(:puavoConf, I18n.t('activeldap.errors.messages.puavoconf.not_a_hash'))
       return
     end
 
-    types_ok = puavoconf_data.all? do |k,v|
-                 k.kind_of?(String) \
-                   && (v.kind_of?(String) || v.kind_of?(Integer) \
-                         || v == false || v == true)
-               end
-    unless types_ok then
-      errors.add(:puavoConf,
-                 I18n.t('activeldap.errors.messages.puavoconf.unsupported_value_types'))
-      return
+    types_ok = puavoconf_data.all? do |k, v|
+      k.is_a?(String) && (v.is_a?(String) || v.is_a?(Integer) || v == false || v == true)
     end
+
+    return if types_ok
+
+    errors.add(:puavoConf, I18n.t('activeldap.errors.messages.puavoconf.unsupported_value_types'))
   end
 end
